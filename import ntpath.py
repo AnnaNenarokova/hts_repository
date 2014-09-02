ac234541116fc91eb708ac65c1d433b92bdee059
AnnaNenarokova <a.nenarokova@gmail.com>
2014-09-03 00:18:40 +0400

    fixed unfixed

--- import ntpath.py
+++
@@ -1,8 +0,0 @@
-import ntpath
-
-def get_file_path(path):
-    head, tail = ntpath.split(path)
-    dir_path = [head, tail]
-    return dir_path
-
-print get_file_path('/home/anna/HTS_programming/HTS_spacers/CTG_CCGTCC_L001_1.fastq')
