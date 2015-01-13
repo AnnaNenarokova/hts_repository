ae0146515192a87bf9882be906c80c3217e7112a
Anna <anna@anna-Lenovo-G510>
2015-01-13 21:38:26 +0300

    bash script for fastqc

---
+++ bash_s/fastqc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dir='/home/anna/bioinformatics/htses/katya/'
+cd $dir
+mkdir fastqc_reports
+for f in *.fastq
+do fastqc $f &
+done
+wait
+mv *.zip fastqc_reports
+tar -cvf fastqc_reports.tar fastqc_reports
