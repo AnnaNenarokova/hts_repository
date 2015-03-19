1aaecca8d135bc2be55644351f2a004be0d5669d
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-20 02:27:29 +0300

    ni

--- wheat/pbs/R1_blast.sh
+++ wheat/pbs/R1_blast.sh
@@ -5,4 +5,4 @@
 adapter='/mnt/lustre/nenarokova/wheat/universal_adapter.fasta'
 outfile='/mnt/lustre/nenarokova/wheat/R1_blast_out.csv'
 blast_db='/mnt/lustre/nenarokova/wheat/R1_not_bsc_bl_db/R1_not_bsc_1.db'
-blastn -query $adapter -db $blast_db -out $outfile -outfmt 10 -task blastn-short num_threads 24
+blastn -query $adapter -db $blast_db -out $outfile -outfmt 10 -task blastn-short -num_threads 24
