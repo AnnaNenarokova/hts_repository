fd9c31a11c27b43fbb24b92860460caa629bbe72
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-23 20:03:42 +0300

    ni

--- wheat/pbs/R1_blast.sh
+++ wheat/pbs/R1_blast.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=5
+#PBS -l nodes=1:ppn=10
 #PBS -l walltime=100:00:00
 #PBS -l mem=10gb
 adapter='/mnt/lustre/nenarokova/ngs/wheat/right_barcodes.fasta'
 outfile='/mnt/lustre/nenarokova/wheat/R1_not_bc_right_bsc.csv'
-blast_db='/mnt/lustre/nenarokova/wheat/R1_not_bsc_bl_db/R1_not_bsc_1.db'
-blastn -query $adapter -db $blast_db -out $outfile -outfmt 10 -task blastn-short -num_threads 24
+blast_db='/mnt/lustre/nenarokova/wheat/R1_not_bsc_bl_db/R1_not_bsc_1.db.00'
+blastn -query $adapter -db $blast_db -out $outfile -outfmt 10 -num_threads 10
