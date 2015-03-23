9e3d919bbf108c748f6bce50d7302e3760b1db08
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-23 18:34:55 +0300

    ni

---
+++ wheat/pbs/L_not_bcs_blast.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=24
+#PBS -l walltime=100:00:00
+#PBS -l mem=48gb
+adapter='/mnt/lustre/nenarokova/ngs/wheat/right_barcodes.fasta'
+outfile='/mnt/lustre/nenarokova/wheat/L00000210.BC1D3RACXX.5/L00000210.BC1D3RACXX.5_1/not_bsc/not_bsc_1_blast_right_bsc.csv'
+blast_db='/mnt/lustre/nenarokova/wheat/L00000210.BC1D3RACXX.5/L00000210.BC1D3RACXX.5_1/not_bsc/not_bsc_1/db_folder/not_bsc_1.db'
+blastn -query $adapter -db $blast_db -out $outfile -outfmt 10 -task blastn-short -num_threads 24
