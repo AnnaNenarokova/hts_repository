01cd137de9de1dc1daa525ff054217102a02dccc
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-26 21:41:11 +0300

    ni

--- wheat/pbs/bowtie2_index.sh
+++ wheat/pbs/bowtie2_index.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=5
+#PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
-#PBS -l mem=20gb
-
 cd /home/nenarokova/wheat/wheat_bowtie2_index/
 bowtie2-build /mnt/lustre/nenarokova/wheat/wheat_scaffolds.fasta wheat
