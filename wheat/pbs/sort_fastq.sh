ba4b1181e98ff93aa5e6c8a15f8f15a160fedb03
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-28 02:08:54 +0300

    ni

--- wheat/pbs/sort_fastq.sh
+++ wheat/pbs/sort_fastq.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=24
 #PBS -l walltime=100:00:00
-#PBS -l mem=48gb
-
-cd /home/nenarokova/wheat/R1
-numbers=( 1 2 )
-name1='0sec_ACAGTG_L001_R'
-name2='_001.fastq'
-for n in "${numbers[@]}"
-	do 
-		f=$name1$n$name2
-		fastqc $f &
-		echo 'fiffi'
-done
-wait
+cd /home/nenarokova/wheat/pbs/
+python sort_fastq_m.py
