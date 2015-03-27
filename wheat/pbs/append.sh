875b92a37d47379b320a4d5e92ae23d6c940cacd
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-27 16:21:25 +0300

    ni

--- wheat/pbs/append.sh
+++ wheat/pbs/append.sh
@@ -2,9 +2,9 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
 
-dir='/home/nenarokova/wheat/R1'
+dir='/home/nenarokova/wheat/R1/'
 cd $dir
-mkdir sum_fastq
+mkdir sum_fastq_re
 for d in *L003*/
 do 
 	cd $d
