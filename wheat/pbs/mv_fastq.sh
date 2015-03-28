28135ac4f83b995a7ff4f51ea552f953f0220274
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-29 00:47:29 +0300

    ni

--- wheat/pbs/mv_fastq.sh
+++ wheat/pbs/mv_fastq.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
-cd /home/nenarokova/wheat/L00000210.BC1D3RACXX.5/
-mv *.fastq /mnt/results/nenarokova/wheat/L/
+mv /mnt/lustre/nenarokova/wheat/wheat.tar.gz /mnt/results/nenarokova/wheat/R1_2/
 
