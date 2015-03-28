124742edb6537ea5c29b9cca1d43803423e30cef
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-29 00:39:57 +0300

    ni

---
+++ wheat/pbs/mv_fastq.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=100:00:00
+cd /home/nenarokova/wheat/L00000210.BC1D3RACXX.5/
+mv *.fastq /mnt/results/nenarokova/wheat/L/
+
---
+++ wheat/pbs/mv_fastq2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=100:00:00
+cd /home/nenarokova/wheat/R1_2/R2/
+mv *.fastq /mnt/results/nenarokova/wheat/R1_2/R2/
