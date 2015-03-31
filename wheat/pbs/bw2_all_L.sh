7095c436a659397da5898efab50378a7644f0c18
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-31 15:25:00 +0300

    ni

--- wheat/pbs/bw2_all_L.sh
+++ wheat/pbs/bw2_all_L.sh
@@ -4,7 +4,7 @@
 head_folder='/home/nenarokova/wheat/L00000210.BC1D3RACXX.5/L00000210.BC1D3RACXX.5_1/sorted/'
 bt2_base='/mnt/lustre/nenarokova/wheat/wheat_bowtie2_index/wheat'
 cd $head_folder
-folder=`ls -1 | tail -n $SGE_TASK_ID | head -1`
+folder=`ls -1 | tail -n $PBS_ARRAY_INDEX | head -1`
 cd $folder
 cd trim_out
 bowtie2 --local --very-fast-local -p 1 --reorder -a -x $bt2_base, -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam
