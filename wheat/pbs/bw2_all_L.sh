6deecff8eacc0500b20e75e3a1031e10e0f4df57
AnnaNenarokova <a.nenarokova@gmail.com>
2015-03-31 14:19:32 +0300

    ni

--- wheat/pbs/bw2_all_L.sh
+++ wheat/pbs/bw2_all_L.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=1
 #PBS -l walltime=1000:00:00
-qsub -t 1-93
 head_folder='/home/nenarokova/wheat/L00000210.BC1D3RACXX.5/L00000210.BC1D3RACXX.5_1/sorted/'
 bt2_base='/mnt/lustre/nenarokova/wheat/wheat_bowtie2_index/wheat'
 cd $head_folder
