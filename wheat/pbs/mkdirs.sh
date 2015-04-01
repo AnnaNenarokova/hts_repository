1b7bf2242950a009a69c77f8514c2c5292d7e1f5
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-02 02:43:46 +0300

    ni

--- wheat/pbs/mkdirs.sh
+++ wheat/pbs/mkdirs.sh
@@ -2,7 +2,9 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
 
-dir='/home/nenarokova/wheat/R1_2/sum_fastq_re/'
+# dir='/home/nenarokova/wheat/R1_2/sum_fastq_re/'
+dir='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/'
+
 cd $dir
 
 for f in *_1.fastq
