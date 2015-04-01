e18b5225e56e2367e739dae58885e1ecfa98352e
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-02 02:35:40 +0300

    ni

--- wheat/pbs/trim.sh
+++ wheat/pbs/trim.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
-fastq_dir='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted/'
+# fastq_dir='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted/'
+fastq_dir='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/'
 cd $fastq_dir
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 mkdir trim_out
 trimc_dir='/home/nenarokova/Trimmomatic-0.33/'
 trimc='trimmomatic-0.33.jar'
-adapters_folder='adapters/TruSeq2-PE.fa'
-# adapters_folder='adapters/all_trim.fa'
+# adapters_folder='adapters/TruSeq2-PE.fa'
+adapters_folder='adapters/all_trim.fa'
 adapters=$trimc_dir$adapters_folder
 file_fw=$folder'_1.fastq'
 file_rv=$folder'_2.fastq'
