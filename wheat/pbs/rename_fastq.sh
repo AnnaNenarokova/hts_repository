de15f88f0a2baff81eba66ddf6dc14475c44308d
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-02 16:52:27 +0300

    ni

--- wheat/pbs/rename_fastq.sh
+++ wheat/pbs/rename_fastq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dir='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted'
-# dir='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/sorted'
+# dir='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted'
+dir='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/sorted'
 
 cd $dir
 
