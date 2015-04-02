ffed32803d6f82e7252ac9ca2847bd82f581bada
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-02 03:42:22 +0300

    ni

--- wheat/pbs/bw2_all_L.sh
+++ wheat/pbs/bw2_all_L.sh
@@ -2,7 +2,7 @@
 #PBS -l walltime=1000:00:00
 #PBS -l mem=24Gb
 #PBS -l nodes=1:ppn=12
-head_folder='/home/nenarokova/wheat/L00000210.BC1D3RACXX.5/L00000210.BC1D3RACXX.5_1/sorted/'
+head_folder='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/sorted/'
 bt2_base='/mnt/lustre/nenarokova/wheat/wheat_bowtie2_index/wheat'
 cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
