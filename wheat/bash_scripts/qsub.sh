3fa637a7712c34184ac30cba6fc0c9f1da74bf3a
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-06 23:45:50 +0300

    ni

--- wheat/bash_scripts/qsub.sh
+++ wheat/bash_scripts/qsub.sh
@@ -3,11 +3,15 @@
 for i in {3..9}
 do
 	node='node0'$i
-    qsub -t $i-$i -l nodes=$node bw2_all_L.sh
+	k=$(($i + 30))
+	echo $k
+    # qsub -t $i-$i -l nodes=$node bw2_all_L.sh
 done 
 
-for i in {10..32}
+for i in {10..19}
 do
 	node='node'$i
-    qsub -t $i-$i -l nodes=$node bw2_all_L.sh
+	k=$(($i + 30))
+	echo $k
+    # qsub -t $i-$i -l nodes=$node bw2_all_L.sh
 done 
