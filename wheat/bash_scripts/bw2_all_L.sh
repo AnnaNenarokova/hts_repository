a1ffd227bdb09e9c3f0ab4bbf2ce19ee86080aa4
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-13 19:28:58 +0300

    ni

--- wheat/bash_scripts/bw2_all_L.sh
+++ wheat/bash_scripts/bw2_all_L.sh
@@ -9,4 +9,4 @@ cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --very-fast -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam
+bowtie2 --very-fast-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam
