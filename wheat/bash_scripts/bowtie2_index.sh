5bec4a7de22abc7a75049ff03c4abafed317586c
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-14 01:38:58 +0300

    ni

--- wheat/bash_scripts/bowtie2_index.sh
+++ wheat/bash_scripts/bowtie2_index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
-cd /home/nenarokova/wheat/wheat_bw2_base_v2.2/
+cd /home/nenarokova/wheat/bw2_base_v.2.2_all/
 bowtie2-build /mnt/lustre/nenarokova/wheat/Taestivum_296_v2.2.transcript.fa wheat
