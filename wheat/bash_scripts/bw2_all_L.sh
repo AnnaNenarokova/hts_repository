927248e329c7c8a424cd87f89e49e4d196314183
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-13 18:45:30 +0300

    ni

--- wheat/bash_scripts/bw2_all_L.sh
+++ wheat/bash_scripts/bw2_all_L.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
-#PBS -l mem=48Gb
-#PBS -l nodes=1:ppn=24
+#PBS -l mem=15Gb
+#PBS -l nodes=1:ppn=1
 
 head_folder='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/sorted/'
 bt2_base='/home/nenarokova/wheat/wheat_bw2_base_v2.2/wheat'
@@ -9,4 +9,4 @@ cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --local --very-fast -p 24 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam
+bowtie2 --very-fast -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam
--- wheat/bash_scripts/bw2_all_R.sh
+++ wheat/bash_scripts/bw2_all_R.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=1000:00:00
-#PBS -l mem=24Gb
-#PBS -l nodes=1:ppn=12
+#PBS -l mem=48Gb
+#PBS -l nodes=1:ppn=24
 
 head_folder='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted/'
 bt2_base='/mnt/lustre/nenarokova/wheat/wheat_bowtie2_index/wheat'
@@ -9,4 +9,4 @@ cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --local --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam
+bowtie2 --very-fast -p 24 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam
