aea4b404ba06b9f65f88afd829a92344332fbeb2
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-15 16:07:42 +0300

    ni

---
+++ wheat/bash_scripts/bw2_all_R_assembly.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -l walltime=100:00:00
+#PBS -l mem=2Gb
+#PBS -l nodes=1:ppn=1
+
+head_folder='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted/1/'
+bt2_base='/home/nenarokova/wheat/bw2_base_v.2.2_all/wheat'
+cd $head_folder
+folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
+cd $folder
+cd trim_out
+bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam > ../bowtie2_logs
