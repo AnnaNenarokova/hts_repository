549093fb34864758f7eef4804a9fd133d7b8560b
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-15 14:13:46 +0300

    ni

--- wheat/bash_scripts/bw2_all_L.sh
+++ wheat/bash_scripts/bw2_all_L.sh
@@ -3,10 +3,10 @@
 #PBS -l mem=15Gb
 #PBS -l nodes=1:ppn=1
 
-head_folder='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/sorted/'
-bt2_base='/home/nenarokova/wheat/bw2_base_v.2.2_all/wheat'
+head_folder='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/sorted/2/'
+bt2_base='/home/nenarokova/wheat/bw2_base_v2.2_transcripts/wheat'
 cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment.sam
+bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment_transcripts.sam > ../bowtie2_logs_transcripts
