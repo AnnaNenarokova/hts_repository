c80a75f95d301c1faf8022d23fc0c695744603cd
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-15 18:21:20 +0300

    ni

--- wheat/bash_scripts/bw2_all_L.sh
+++ wheat/bash_scripts/bw2_all_L.sh
@@ -4,9 +4,9 @@
 #PBS -l nodes=1:ppn=1
 
 head_folder='/mnt/results/nenarokova/wheat/L/L00000210.BC1D3RACXX.5_1/sorted/'
-bt2_base='/home/nenarokova/wheat/bw2_base_v2.2_transcripts/wheat'
+bt2_base='/home/nenarokova/wheat/wheat_bowtie2_index/wheat'
 cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment_transcripts.sam > ../bowtie2_logs_transcripts
+bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment_new.sam > ../bowtie2_logs_new
