3d51b218013cdb146e90cb364b0d86c637fce7e1
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-15 16:21:14 +0300

    ni

--- wheat/bash_scripts/bw2_all_R_transcripts.sh
+++ wheat/bash_scripts/bw2_all_R_transcripts.sh
@@ -3,7 +3,7 @@
 #PBS -l mem=2Gb
 #PBS -l nodes=1:ppn=1
 
-head_folder='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted/1/'
+head_folder='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted/'
 bt2_base='/home/nenarokova/wheat/bw2_base_v2.2_transcripts/wheat'
 cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
