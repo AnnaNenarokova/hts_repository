5751609c625792b9dcc567493e02a1859433ec6e
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-17 02:20:10 +0300

    ni

--- wheat/bash_scripts/bw2_all_R_transcripts.sh
+++ wheat/bash_scripts/bw2_all_R_transcripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
 #PBS -l mem=2Gb
-#PBS -l nodes=1:ppn=12
+#PBS -l nodes=1:ppn=1
 
 head_folder='/home/nenarokova/wheat/R1_2/sum_fastq_re/sorted/'
 bt2_base='/home/nenarokova/wheat/bw2_base_v2.2_transcripts/wheat'
@@ -9,4 +9,4 @@ cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --very-sensitive-local -p 12  --ma 1 --mp 0,0 --rdg 0,0 --rfg 0,0 --score-min L,0,0.85 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment_transcripts_strict.sam
+bowtie2 --very-sensitive-local -p 1  --ma 1 --mp 0,0 --rdg 0,0 --rfg 0,0 --score-min L,0,0.85 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../wheat_alignment_transcripts_strict.sam
