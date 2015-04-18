723c83bef9c43db5f0882c7c9ef33ed510be4269
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-18 05:40:44 +0300

    ni

--- wheat/bash_scripts/mv_fastq.sh
+++ wheat/bash_scripts/mv_fastq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
-mv /mnt/lustre/nenarokova/wheat/R1_2/R1/sum_fastq /mnt/results/nenarokova/wheat/R1_2/R1/
+mv /home/nenarokova/wheat/R1_2 /mnt/results/nenarokova/wheat/R1_2/
 
