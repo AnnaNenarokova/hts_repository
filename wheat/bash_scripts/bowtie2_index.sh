67d23389a65bcc5f96d58b07039a2b77de0ff5b2
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-20 23:16:00 +0300

    ni

--- wheat/bash_scripts/bowtie2_index.sh
+++ wheat/bash_scripts/bowtie2_index.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
-mkdir /home/nenarokova/wheat/bw2_bases/CIN14/
+cd /home/nenarokova/wheat/
+mkdir bw2_bases
+cd bw2_bases
+mkdir CIN14/
 cd /home/nenarokova/wheat/bw2_bases/CIN14/
 bowtie2-build /home/nenarokova/ngs/wheat/nbs_lrr_genes/CIN14.fasta wheat
