9173c1ce8e5ec7068839a20b62113b2a6edbcff9
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-20 23:23:57 +0300

    ni

--- wheat/bash_scripts/bowtie2_index.sh
+++ wheat/bash_scripts/bowtie2_index.sh
@@ -2,7 +2,7 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
 cd /home/nenarokova/wheat/bw2_bases/
-dir='S11A11'
+dir='ScRGA-6RL1'
 mkdir $dir
 cd $dir
-bowtie2-build /home/nenarokova/ngs/wheat/nbs_lrr_genes/S11A11.fasta wheat
+bowtie2-build /home/nenarokova/ngs/wheat/nbs_lrr_genes/ScRGA-6RL1.fasta wheat
