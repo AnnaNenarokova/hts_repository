edb0bec4951d0fb2f6517be01967f02a416074d9
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-21 13:53:33 +0300

    ni

--- wheat/bash_scripts/bw2_all.sh
+++ wheat/bash_scripts/bw2_all.sh
@@ -3,10 +3,10 @@
 #PBS -l mem=2Gb
 #PBS -l nodes=1:ppn=1
 
-head_folder='/mnt/results/nenarokova/wheat/R/sum_fastq_re/sorted/'
-bt2_base='/home/nenarokova/wheat/bw2_bases/ScRGA-6RL2/wheat'
+head_folder='/mnt/results/nenarokova/wheat/L/sum_fastq_re/sorted/'
+bt2_base='/home/nenarokova/wheat/bw2_bases/ScRGA-6RL3/wheat'
 cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../ScRGA-6RL2.sam
+bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../ScRGA-6RL3.sam
