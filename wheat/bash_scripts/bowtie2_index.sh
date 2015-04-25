aa3067e85bc440bac8cc02b6bd3d9df722f7d5e3
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-25 16:05:13 +0300

    ni

--- wheat/bash_scripts/bowtie2_index.sh
+++ wheat/bash_scripts/bowtie2_index.sh
@@ -5,4 +5,4 @@ cd /home/nenarokova/wheat/bw2_bases/
 dir='NBS_LRR_wheat_genes'
 mkdir $dir
 cd $dir
-bowtie2-build /home/nenarokova/ngs/wheat/NBS_LRR_wheat_genes.fasta NBS_LRR_wheat_genes
+bowtie2-build /home/nenarokova/wheat/NBS_LRR_wheat_genes.fasta NBS_LRR_wheat_genes
