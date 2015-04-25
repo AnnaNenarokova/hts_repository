d9195bb8310f40ed9b0748adf190d895506bffa3
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-26 00:07:24 +0300

    ni

--- wheat/bash_scripts/blast.sh
+++ wheat/bash_scripts/blast.sh
@@ -3,8 +3,8 @@
 #PBS -l walltime=100:00:00
 #PBS -l mem=48gb
 blast_db='/mnt/lustre/nenarokova/wheat/new_assembly_blastdb/wheat_scaffolds.db'
-# fasta='/mnt/lustre/nenarokova/wheat/NBS_LRR_all_plants.fasta'
-fasta='/home/anna/bioinformatics/ngs/wheat/Cocos_nucifera_RGA.fasta'
-outfile='/home/nenarokova/wheat/NBS_LRR_new_assembly_blreport'
+fasta='/mnt/lustre/nenarokova/wheat/NBS_LRR_all_plants.fasta'
+# fasta='/home/anna/bioinformatics/ngs/wheat/Cocos_nucifera_RGA.fasta'
+outfile='/home/nenarokova/wheat/NBS_LRR_new_assembly_blreport2'
 blastn -query $fasta -db $blast_db -out $outfile -outfmt 10 -num_threads 24
 	
