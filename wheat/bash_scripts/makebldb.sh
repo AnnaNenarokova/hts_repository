b50a977645f252c58d8cb943e88d1e8315e46ea7
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-25 22:22:06 +0300

    ni

--- wheat/bash_scripts/makebldb.sh
+++ wheat/bash_scripts/makebldb.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=24
-#PBS -l walltime=100:00:00
-#PBS -l mem=48gb
-makeblastdb -in /home/anna/bioinformatics/htses/ERR015599/not_bsc_1/not_bsc_1.fasta -parse_seqids -dbtype nucl -out /home/anna/bioinformatics/htses/ERR015599/not_bsc_1/blast_db/not_bsc_1.db
+makeblastdb -in /mnt/lustre/nenarokova/wheat/wheat_scaffolds.fasta -parse_seqids -dbtype nucl -out /home/nenarokova/wheat/new_assembly_blastdb/wheat_scaffolds.db
