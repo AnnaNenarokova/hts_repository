9292fd094025555797f6a11ba7bb1d425eb7621d
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-25 22:59:19 +0300

    ni

--- wheat/bash_scripts/blast.sh
+++ wheat/bash_scripts/blast.sh
@@ -2,5 +2,8 @@
 #PBS -l nodes=1:ppn=24
 #PBS -l walltime=100:00:00
 #PBS -l mem=48gb
-cd /home/nenarokova/wheat
-blastn -query ./wheat_adapter.fasta -subject ./R1.fasta -out ./not_bcs_1_report -outfmt 10
+blast_db='/mnt/lustre/nenarokova/wheat/new_assembly_blastdb/wheat_scaffolds.db'
+fasta='/mnt/lustre/nenarokova/wheat/NBS_LRR_all_plants.fasta'
+outfile='/home/nenarokova/wheat/NBS_LRR_new_assembly_blreport'
+blastn -query $fasta -db $blast_db -out $outfile -outfmt 10 -num_threads 24
+	
