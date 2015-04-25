9d445151afc477e9718f63fa914b650154a1f230
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-25 16:44:58 +0300

    ni

--- wheat/bash_scripts/bw2_all.sh
+++ wheat/bash_scripts/bw2_all.sh
@@ -3,10 +3,10 @@
 #PBS -l mem=2Gb
 #PBS -l nodes=1:ppn=1
 
-head_folder='/mnt/results/nenarokova/wheat/R/sum_fastq_re/sorted/'
-bt2_base='/home/nenarokova/wheat/bw2_bases/CIN14/wheat'
+head_folder='/mnt/results/nenarokova/wheat/L/sum_fastq_re/sorted/'
+bt2_base='/home/nenarokova/wheat/NBS_LRR_wheat_genes/NBS_LRR_wheat_genes'
 cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../CIN14.sam
+bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../NBS_LRR_wheat_genes.sam
