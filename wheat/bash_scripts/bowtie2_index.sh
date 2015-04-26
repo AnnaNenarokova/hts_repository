a2108b99d37197c92bf584908a03a7462f8be37c
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-26 18:38:56 +0300

    ni

--- wheat/bash_scripts/bowtie2_index.sh
+++ wheat/bash_scripts/bowtie2_index.sh
@@ -2,7 +2,7 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
 cd /home/nenarokova/wheat/bw2_bases/
-dir='NBS_LRR_wheat_genes'
+dir='nbs_lrr_new_assembly'
 mkdir $dir
 cd $dir
-bowtie2-build /home/nenarokova/wheat/NBS_LRR_all_plants.fasta NBS_LRR_all_plants
+bowtie2-build /mnt/lustre/nenarokova/wheat/nbs_lrr_new_assembly.fasta nbs_lrr_new_assembly
