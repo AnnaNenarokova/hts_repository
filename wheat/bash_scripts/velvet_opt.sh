025c4c10632edee3ff7f3120d0a9b638f5db357b
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-26 14:29:33 +0300

    ni

--- wheat/bash_scripts/velvet_opt.sh
+++ wheat/bash_scripts/velvet_opt.sh
@@ -2,6 +2,9 @@
 #PBS -l nodes=1:ppn=24
 #PBS -l walltime=100:00:00
 #PBS -l mem=48gb
-cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/sorted/A01/trim_out
-
-/home/nenarokova/VelvetOptimiser-2.2.5/VelvetOptimiser.pl -s 33 -e 63 -t 24 -f '-fastq unpaired_out_fw.fastq -fastq unpaired_out_rv.fastq -shortPaired -fastq paired_out_fw.fastq -fastq paired_out_rv.fastq' -o '-min_contig_lgth 200 -cov_cutoff 10'
+cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/sorted/
+for d in */
+	do
+		cd $dir
+		cd /trim_out
+		/home/nenarokova/VelvetOptimiser-2.2.5/VelvetOptimiser.pl -s 33 -e 63 -t 24 -f '-fastq unpaired_out_fw.fastq -fastq unpaired_out_rv.fastq -shortPaired -fastq paired_out_fw.fastq -fastq paired_out_rv.fastq' -o '-min_contig_lgth 200 -cov_cutoff 10'
