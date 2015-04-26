a6cf8f242f01fa189f15cd661d2c264c4bbd4fc2
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-26 18:54:50 +0300

    ni

--- wheat/bash_scripts/blast_parcer.py
+++ wheat/bash_scripts/blast_parcer.py
@@ -2,20 +2,23 @@
 import csv
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
+global ONLY_ONE_HIT; ONLY_ONE_HIT = False
 
 handle_file = '/home/anna/bioinformatics/wheat/NBS_LRR_new_assembly_blreport.csv'
 handle_file = open(handle_file)
 handle_csv = csv.reader(handle_file, delimiter=',')
-sorted_csv = sorted( handle_csv, key = lambda x: ( x[0], -int(x[3]), -float(x[2]) ) ) 
 
-results = []
-cur_seq = None
-for row in sorted_csv:
-	if row[0] != cur_seq: 
-		cur_seq = row[0]
-		results.append(row)
-		# print row
-handle_file.close()
+if ONLY_ONE_HIT:
+	sorted_csv = sorted( handle_csv, key = lambda x: ( x[0], -int(x[3]), -float(x[2]) ) ) 
+
+	results = []
+	cur_seq = None
+	for row in sorted_csv:
+		if row[0] != cur_seq: 
+			cur_seq = row[0]
+			results.append(row)
+	handle_file.close()
+else: results = handle_csv
 
 fasta_file = '/home/anna/bioinformatics/wheat/wheat_scaffolds.fasta'
 result_seqs = []
@@ -27,7 +30,7 @@ for seq_record in SeqIO.parse(fasta_file, "fasta"):
 			if start > end: start, end = end, start
 			seq = seq_record.seq[start:end]
 			result_seqs.append(SeqRecord(seq=seq, id=seq_record.id + ' ' + row[0], description=''))
-			results.remove(row)
+			if ONLY_ONE_HIT: results.remove(row)
 
 out_file = '/home/anna/bioinformatics/wheat/nbs_lrr_new_assembly.fasta'	
 SeqIO.write(result_seqs, out_file, "fasta")
