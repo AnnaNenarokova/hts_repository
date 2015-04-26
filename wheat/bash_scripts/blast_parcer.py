b046621765ccc6a2d7daef314b0ac9453e485870
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-26 16:24:07 +0300

    ni

--- wheat/bash_scripts/blast_parcer.py
+++ wheat/bash_scripts/blast_parcer.py
@@ -8,12 +8,24 @@ handle_csv = csv.reader(handle_file, delimiter=',')
 sorted_csv = sorted( handle_csv, key = lambda x: ( x[0], -int(x[3]), -float(x[2]) ) ) 
 
 results = []
-
 cur_seq = None
-print len(sorted_csv)
 for row in sorted_csv:
 	if row[0] != cur_seq: 
 		cur_seq = row[0]
 		results.append(row)
+		# print row
+handle_file.close()
 
-for 
+fasta_file = '/home/anna/bioinformatics/wheat/wheat_scaffolds.fasta'
+result_seqs = []
+for seq_record in SeqIO.parse(fasta_file, "fasta"):
+	for row in results:
+		if seq_record.id == row[1]:
+			start = int(row[8])
+			end = int(row[9])
+			if start > end: start, end = end, start
+			seq = seq_record.seq[start:end]
+			result_seqs.append(seq)
+			results.remove(row)
+out_file = '/home/anna/bioinformatics/wheat/nbs_lrr_new_assembly.fasta'	
+SeqIO.write(out_file, result_seqs, "fasta")
