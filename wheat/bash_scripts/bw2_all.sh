eb322e600a084937e65ff6373b25f3eaa328be81
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-27 05:26:26 +0300

    ni

--- wheat/bash_scripts/bw2_all.sh
+++ wheat/bash_scripts/bw2_all.sh
@@ -3,10 +3,10 @@
 #PBS -l mem=2Gb
 #PBS -l nodes=1:ppn=1
 
-head_folder='/mnt/results/nenarokova/wheat/R/sum_fastq_re/sorted/'
-bt2_base='/mnt/lustre/nenarokova/wheat/bw2_bases/bw2_base_new_assembly/wheat'
+head_folder='/mnt/results/nenarokova/wheat/L/sum_fastq_re/sorted/'
+bt2_base='/mnt/lustre/nenarokova/wheat/bw2_bases/old_assembly_transcripts/transcripts2'
 cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../new_assembly.sam
+bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../transcripts2.sam
