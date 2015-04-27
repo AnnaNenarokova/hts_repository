7e658c5ad076dcd6212053233e0b6e73a4460e9a
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-27 04:23:29 +0300

    ni

--- wheat/bash_scripts/bowtie2_index.sh
+++ wheat/bash_scripts/bowtie2_index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
 cd /home/nenarokova/wheat/bw2_bases/
-dir='nbs_lrr_new_assembly'
+dir='old_assembly_transcripts'
 mkdir $dir
 cd $dir
-bowtie2-build /mnt/lustre/nenarokova/wheat/new_assembly/sum_nbs_lrr.fasta sum_nbs_lrr
+bowtie2-build /mnt/lustre/nenarokova/wheat/Taestivum_296_v2.2.transcript.fa transcripts2
