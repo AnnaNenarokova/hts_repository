ebe99fe0e7f9dcf497cb6d64076977ba23b43dc9
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 16:58:51 +0300

    ni

--- wheat/bash_scripts/bw2_all.sh
+++ wheat/bash_scripts/bw2_all.sh
@@ -3,7 +3,7 @@
 #PBS -l mem=2Gb
 #PBS -l nodes=1:ppn=1
 
-head_folder='/mnt/results/nenarokova/wheat/R/sum_fastq_re/sorted/'
+head_folder='/mnt/results/nenarokova/wheat/L/sum_fastq_re/sorted/'
 bt2_base='/mnt/lustre/nenarokova/wheat/bw2_bases/new_assembly_nbs_lrr_ids/new_assembly_nbs_lrr_ids'
 cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
