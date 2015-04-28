1d93b6df2922b4b325a3a7d8e0c2cdb6ae121ccc
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-29 02:33:59 +0300

    ni

--- wheat/bash_scripts/call_snp.sh
+++ wheat/bash_scripts/call_snp.sh
@@ -7,6 +7,6 @@ var_vcf='../vcf/'${f%%.*}'.flt.bcf'
 echo $f
 echo $var_bcf
 echo $var_vcf
-samtools mpileup -uf $ref $f > $var_bcf
+samtools mpileup -uf $ref -d 100000 $f > $var_bcf
 # bcftools view -bvcg > $var_bcf  
 # bcftools view $var_bcf | vcfutils.pl varFilter -D 1000000 > $var_vcf 
