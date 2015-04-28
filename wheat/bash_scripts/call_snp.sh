a9e32a0a0f356446095e89c47984de32b681ac2b
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-29 00:14:34 +0300

    ni

--- wheat/bash_scripts/call_snp.sh
+++ wheat/bash_scripts/call_snp.sh
@@ -2,8 +2,8 @@
 cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/merged_alignments/mapq_5_alignments/
 f=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 ref='/mnt/lustre/nenarokova/wheat/new_assembly/nbs_lrr_genes/nbs_lrr_new_assembly.fasta'
-var_bcf='../vcf/'${f:0:-4}'.raw.bcf'
-var_vcf='../vcf/'${f:0:-4}'.flt.bcf'
+# var_bcf='../vcf/'${f:0:-4}'.raw.bcf'
+# var_vcf='../vcf/'${f:0:-4}'.flt.bcf'
 echo $f
 echo $var_bcf
 echo $var_vcf
