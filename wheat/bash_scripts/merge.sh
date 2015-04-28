d68c97806f84ffdb022149563812e990423964e2
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 19:19:52 +0300

    ni

--- wheat/bash_scripts/merge.sh
+++ wheat/bash_scripts/merge.sh
@@ -4,17 +4,14 @@ mkdir merged_alignments
 letters=( A B C D E F G H )
 for l in letters
 do
-	for n in {1..5}
+	for n in {1..11}
 	do 
 		out_bam='./merged_alignments/'$l$n'.bam'
-		in1_bam='./'$l$n'new_assembly_nbs_lrr_ids.bam'
-		in2_bam='./'$l'0'$n'new_assembly_nbs_lrr_ids.bam'
+		in_bam1='./'$l$n'new_assembly_nbs_lrr_ids.bam'
+		in_bam2='./'$l'0'$n'new_assembly_nbs_lrr_ids.bam'
 		echo $out_bam
 		echo $in_bam1
 		echo $in_bam2
-		ls $out_bam
-		ls $in_bam1
-		ls $in_bam2
 		# samtools merge $out_bam $in_bam1 $in_bam2
 	done
 done
