dfe6859fdf3db3f7829d645d851aa2ea280cccf1
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 23:16:49 +0300

    ni

--- wheat/bash_scripts/sort_mapq.sh
+++ wheat/bash_scripts/sort_mapq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /mnt/results/nenarokova/wheat/R/sum_fastq_re/merged_alignments/full_alignments
+cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/merged_alignments/full_alignments
 f=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 q_alignment='../mapq_50_alignments/'$f
 echo $q50_alignment
