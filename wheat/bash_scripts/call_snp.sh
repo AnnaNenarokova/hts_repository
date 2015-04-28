1ae2c9cae866b5374303d601481cee637fb86406
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 20:53:43 +0300

    ni

--- wheat/bash_scripts/call_snp.sh
+++ wheat/bash_scripts/call_snp.sh
@@ -7,4 +7,4 @@ ref='/mnt/lustre/nenarokova/wheat/new_assembly/nbs_lrr_genes/nbs_lrr_new_assembl
 var_bcf=${q50_alignment:0:-4}'.raw.bcf'
 var_vcf=${q50_alignment:0:-4}'.flt.bcf'
 samtools mpileup -uf $ref $q50_alignment | bcftools view -bvcg - > $var_bcf  
-bcftools view $var_bcf | vcfutils.pl varFilter -D100 > $var_vcf 
+bcftools view $var_bcf | vcfutils.pl varFilter -D1000000 > $var_vcf 
