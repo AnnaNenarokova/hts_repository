a96d4cd8a5fbdbfc3dd4988351ccf96053d6c322
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 19:35:51 +0300

    ni

--- wheat/bash_scripts/merge.sh
+++ wheat/bash_scripts/merge.sh
@@ -3,12 +3,9 @@ cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/sorted
 mkdir merged_alignments
 for l in A B C D E F G H
 do
-	for n in {1..11}
-	do 
-		out_bam='./merged_alignments/'$l$n'.bam'
-		in_bam1='./'$l$n'/new_assembly_nbs_lrr_ids.bam'
-		in_bam2='./'$l'0'$n'/new_assembly_nbs_lrr_ids.bam'
-		echo $out_bam
-		samtools merge $out_bam $in_bam1 $in_bam2
-	done
+	out_bam='./merged_alignments/'$l$PBS_ARRAYID'.bam'
+	in_bam1='./'$l$PBS_ARRAYID'/new_assembly_nbs_lrr_ids.bam'
+	in_bam2='./'$l'0'$PBS_ARRAYID'/new_assembly_nbs_lrr_ids.bam'
+	echo $out_bam
+	samtools merge $out_bam $in_bam1 $in_bam
 done
