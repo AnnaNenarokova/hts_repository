ab1c8959021830b543092ecc15bca94b4e590ed7
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 19:53:13 +0300

    ni

--- wheat/bash_scripts/merge.sh
+++ wheat/bash_scripts/merge.sh
@@ -7,5 +7,5 @@ do
 	in_bam1='./'$l$PBS_ARRAYID'/new_assembly_nbs_lrr_ids.bam'
 	in_bam2='./'$l'0'$PBS_ARRAYID'/new_assembly_nbs_lrr_ids.bam'
 	echo $out_bam
-	echo samtools merge $out_bam $in_bam1 $in_bam
+	echo samtools merge $out_bam $in_bam1 $in_bam2
 done
