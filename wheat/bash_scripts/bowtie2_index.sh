9cb14ad05bb9b782826bdd44a665d5819a349b4a
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 16:03:58 +0300

    ni

--- wheat/bash_scripts/bowtie2_index.sh
+++ wheat/bash_scripts/bowtie2_index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
 cd /home/nenarokova/wheat/bw2_bases/
-dir='sum_new_assembly_nbs_lrr'
+dir='new_assembly_nbs_lrr_ids'
 mkdir $dir
 cd $dir
-bowtie2-build /mnt/lustre/nenarokova/wheat/new_assembly/nbs_lrr_genes/sum_nbs.fasta sum_new_assembly_nbs_lrr
+bowtie2-build /mnt/lustre/nenarokova/wheat/new_assembly/nbs_lrr_genes/nbs_lrr_new_assembly.fasta new_assembly_nbs_lrr_ids
