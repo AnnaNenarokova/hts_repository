d50ced9c847e4848d84a48e3f6a7d66514543135
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 20:55:16 +0300

    ni

--- wheat/bash_scripts/call_snp.sh
+++ wheat/bash_scripts/call_snp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/sorted/merged_alignments/full_alignments
+cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/merged_alignments/full_alignments
 f=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 q50_alignment='../mapq_50_alignments/'$f
 samtools view -q 50 $f > $q50_alignment
