b469db67dae89255496b1146ec55fd39ebc1f9c7
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 19:24:07 +0300

    ni

--- wheat/bash_scripts/merge.sh
+++ wheat/bash_scripts/merge.sh
@@ -6,8 +6,8 @@ do
 	for n in {1..11}
 	do 
 		out_bam='./merged_alignments/'$l$n'.bam'
-		in_bam1='./'$l$n'new_assembly_nbs_lrr_ids.bam'
-		in_bam2='./'$l'0'$n'new_assembly_nbs_lrr_ids.bam'
+		in_bam1='./'$l$n'/new_assembly_nbs_lrr_ids.bam'
+		in_bam2='./'$l'0'$n'/new_assembly_nbs_lrr_ids.bam'
 		echo $out_bam
 		echo $in_bam1
 		echo $in_bam2
