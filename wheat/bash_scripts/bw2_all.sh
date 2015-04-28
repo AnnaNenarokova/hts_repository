946f26acd8a7c1fc138d9fa89ec4849d4db1c0b5
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 16:19:34 +0300

    ni

--- wheat/bash_scripts/bw2_all.sh
+++ wheat/bash_scripts/bw2_all.sh
@@ -9,4 +9,8 @@ cd $head_folder
 folder=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 cd $folder
 cd trim_out
-bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S ../new_assembly_nbs_lrr_ids.sam
+alignment_name='new_assembly_nbs_lrr_ids'
+alignment='../'$alignment_name'.sam'
+bowtie2 --very-sensitive-local -p 1 --reorder -x $bt2_base -1 paired_out_fw.fastq -2 paired_out_rv.fastq -U unpaired_out_fw.fastq,unpaired_out_rv.fastq -S $alignment
+cd ..
+samtools view -bS $alignment_name'.sam' > $alignment_name'.bam' 
