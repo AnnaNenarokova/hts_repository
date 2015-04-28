e912ccf5087e831bb03b42a2d32407ef70a1b882
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-28 23:33:09 +0300

    ni

--- wheat/bash_scripts/call_snp.sh
+++ wheat/bash_scripts/call_snp.sh
@@ -4,5 +4,5 @@ f=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 ref='/mnt/lustre/nenarokova/wheat/new_assembly/nbs_lrr_genes/nbs_lrr_new_assembly.fasta'
 var_bcf=${f:0:-4}'.raw.bcf'
 var_vcf=${f:0:-4}'.flt.bcf'
-samtools mpileup -uf $ref $q50_alignment | bcftools view -bvcg - > $var_bcf  
+samtools mpileup -uf $ref $f | bcftools view -bvcg - > $var_bcf  
 bcftools view $var_bcf | vcfutils.pl varFilter -D1000000 > $var_vcf 
