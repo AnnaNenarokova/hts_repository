11146f5349226fedaf3ace8fbf3d29e7591a2581
AnnaNenarokova <a.nenarokova@gmail.com>
2015-04-30 01:30:55 +0300

    ni

--- wheat/bash_scripts/call_snp.sh
+++ wheat/bash_scripts/call_snp.sh
@@ -3,10 +3,12 @@ cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/merged_alignments/mapq_5_alignme
 f=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 ref='/mnt/lustre/nenarokova/wheat/new_assembly/nbs_lrr_genes/nbs_lrr_new_assembly.fasta'
 var_bcf='../vcf/'${f%%.*}'.raw.bcf'
+var_bcf='../vcf/'${f%%.*}'22.raw.bcf'
 var_vcf='../vcf/'${f%%.*}'.flt.vcf'
 echo $f
 echo $var_bcf
+echo $var_bcf
 echo $var_vcf
 samtools mpileup -uf $ref $f -d 1000000 > $var_bcf
-bcftools view -bvcg $var_bcf > $var_bcf  
-# bcftools view $var_bcf | vcfutils.pl varFilter -D 1000000 > $var_vcf 
+bcftools view -bvcg $var_bcf > $var_bcf2  
+# bcftools view $var_bcf2 | vcfutils.pl varFilter -D 1000000 > $var_vcf 
