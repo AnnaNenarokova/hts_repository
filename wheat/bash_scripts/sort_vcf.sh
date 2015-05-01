7c89a8c2f2500c9747c4a87b9130b12e2c2e224b
AnnaNenarokova <a.nenarokova@gmail.com>
2015-05-02 02:45:07 +0300

    ni

--- wheat/bash_scripts/sort_vcf.sh
+++ wheat/bash_scripts/sort_vcf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=100:00:00
-cd /mnt/results/nenarokova/wheat/R/sum_fastq_re/merged_alignments/vcf/vcf/
+cd /mnt/results/nenarokova/wheat/L/sum_fastq_re/merged_alignments/vcf/
 f=`ls -1 *.flt.vcf | tail -n $PBS_ARRAYID | head -1`
 /home/nenarokova/ngs/wheat/sort_vcf.py $f
