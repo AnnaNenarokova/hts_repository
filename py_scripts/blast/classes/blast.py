4f6ffbd8be214e47820da00049bb014e6c0a20d5
AnnaNenarokova <a.nenarokova@gmail.com>
2015-11-01 14:54:53 +0100

    ni

--- py_scripts/blast/classes/blast.py
+++ py_scripts/blast/classes/blast.py
@@ -5,34 +5,21 @@ sys.path.insert(0, "/home/anna/bioinformatics/ngs/py_scripts/")
 from common_helpers.make_outdir import file_from_path, name_outdir, make_outdir
 from seq_helpers.convert import fastq_fasta
 
-global FORMATS; 
-FORMATS = { 
-	'pairwise': '0', 
-	'query_anchored_identities': '1',
-	'query_anchored_no_identities': '2',
-	'flat_query_anchored_identities': '3',
-	'flat_query_anchored_no_identities': '4',
-	'xml': '5', 
-	'tabular': '6',
-	'tabular_comment_lines': '7',
-	'text_asn': '8',
-	'binary_asn': '9',
-	'comma_values': '10',
-	'blast_archive': '11'
-}
-
 class Blast(object):
-	def __init__(self, ref=False, ref_type='fasta', db_type=False, db=False, query=False, outdir=False):
-		self.ref = ref
+	def __init__(self, ref_path=False, ref_type='fasta', db_type=False, db_path=False, query_path=False, outdir=False, threads=False):
+		self.ref_path = ref_path
 		self.ref_type = ref_type
 		if self.ref_type=='fastq': self.ref = fastq_fasta(self.ref)
 		self.db_type = db_type
-		self.db = db
-		self.query = query
+		self.db_path = db_path
+		self.query_path = query_path
 		self.outdir = outdir
 		return None
 
 	def makeblastdb(self):
+		if not ref: 
+			print "Error: No reference"
+			return 1
 		if not self.outdir:
 			self.outdir = make_outdir(self.ref)
 		db_folder = self.outdir + 'db_folder/'
@@ -41,8 +28,33 @@ class Blast(object):
 		call(makeblastdb)
 		return db_path
 
-	def blast(self, fmt=6):
-		if not self.db: self.db = makeblastdb()
-		
-		return None
+	formats = { 
+	'pairwise': {'№':'0', 'ext':''}, 
+	'query_anchored_identities': {'№':'1', 'ext':''},
+	'query_anchored_no_identities': {'№':'2', 'ext':''},
+	'flat_query_anchored_identities': {'№':'3', 'ext':''},
+	'flat_query_anchored_no_identities': {'№':'4', 'ext':''},
+	'xml': {'№':'5', 'ext':'.xml'}, 
+	'tabular': {'№':'6', 'ext':'.csv'},
+	'tabular_comment_lines': {'№':'7', 'ext':'.csv'},
+	'text_asn': {'№':'8', 'ext':''},
+	'binary_asn': {'№':'9', 'ext':''},
+	'comma_values': {'№':'10', 'ext':'.csv'},
+	'blast_archive': {'№':'11', 'ext':''}
+	}
+
+	def blast(self, bltype='blastn', outfmt='comma_values', blastn_short=False, evalue=10, threads=8, outfile=False):
+		if not self.query_path:
+			print "Error: No query"
+			return 1
+		if not self.db_path: self.db_path = makeblastdb()
+		else:
+			if not outfile: self.outdir = make_outdir(self.ref)
+
+		query_name = file_from_path(self.query_path, endcut=6)
+		if not outfile: outfile = outdir + str(query_name + "_bl_report" + formats[outfmt]['ext'])
+
+		blast_call = [bltype, '-query', self.query, '-db', self.db_path, '-out', outfile, '-outfmt', formats['outfmt'], '-num_threads', str(threads)]
+		if blastn_short: blast_call.extend ['-task', 'blastn-short']
+		return outfile
 
--- py_scripts/common_helpers/make_outdir.py
+++ py_scripts/common_helpers/make_outdir.py
@@ -7,13 +7,13 @@ def file_from_path(path, folder=False, endcut=0):
     if folder: return head
     else: return tail[0:-endcut]
 
-def name_outdir(f, workdir=False, endcut=6):
-	name = file_from_path(f, endcut=endcut)
-	if workdir: outdir = workdir + name + '/'
-	else: outdir = file_from_path(f, folder=True) + name + '/'
+def name_outdir(f_path, workdir=False, endcut=6):
+	name = file_from_path(f_path, endcut=endcut)
+	if workdir: outdir = workdir + '/' + name + '/'
+	else: outdir = file_from_path(f_path, folder=True) + '/' +name + '/'
 	return outdir
 
-def make_outdir(f, workdir=False, endcut=6):
-	outdir = name_outdir(f, workdir, endcut=endcut)
+def make_outdir(f_path, workdir=False, endcut=6):
+	outdir = name_outdir(f_path, workdir, endcut=endcut)
 	if not exists(outdir): makedirs(outdir)
 	return outdir
