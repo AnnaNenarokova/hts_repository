95051c372bfcaefb5d5af0495e0c6dde8c5fca41
AnnaNenarokova <a.nenarokova@gmail.com>
2015-11-03 22:24:30 +0100

    ni

--- py_scripts/blast/classes/blast_parser.py
+++ py_scripts/blast/classes/blast_parser.py
@@ -5,14 +5,10 @@ sys.path.insert(0, "/home/anna/bioinformatics/ngs/py_scripts/")
 from common_helpers.make_outdir import file_from_path, make_outdir
 
 class BlastParser(object):
-	def __init__(self, bl_report_path, bl_report_type):
-		self.bl_report_path = bl_report_path
-		self.bl_report_type = bl_report_type
-
 	def parse_blast_csv(self):
 		handle_file = open(self.bl_report_path)
 		handle_csv = csv.reader(handle_file, delimiter=',')
-		results = []
+		self.hits = []
 		for row in handle_csv:
 			hit = {
 			'query_id': str(row[0]),
@@ -28,20 +24,34 @@ class BlastParser(object):
 			'e-value': float(row[10]),
 			'bit_score': float(row[11]),
 					}
-			results.append(hit)
-		return results
-	
+			self.hits.append(hit)
+		return self.hits
+
+	def __init__(self, bl_report_path, bl_report_type):
+		self.bl_report_path = bl_report_path
+		self.bl_report_type = bl_report_type
+		self.parse_blast_csv()
+		return None
+
 	def sort_blast_csv(self):
 		hits = self.parse_blast_csv()
 		sorted_hits = sorted( hits, key = lambda hit: ( hit['query_id'], -hit['alignment_length'], hit['e-value']) ) 
-		return sorted_hits
+		self.hits = sorted_hits
+		return self.hits
 
-	def write_blast_csv(self, hits, outfile_path, header=False):
+	def count_hits(self):
+		queries = []
+		for hit in self.hits: queries.append(hit['query_id'])
+		return [len(queries), len(set(queries))]
+
+	def write_blast_csv(self, outfile_path, hits=False, header=False):
 		with open(outfile_path, 'wb') as outfile:
 			csv_writer = csv.writer(outfile, delimiter=',')
 			if header:
 				csv_writer.writerow(['query id', 'subject id', '% identity', 'alignment length', 
 				 	'mismatches','gap opens', 'q start','q end','s start', 's end', 'e-value', 'bit score'])
+			
+			if not hits: hits = self.hits
 			for hit in hits:
 				row = [
 					hit['query_id'], hit['subject_id'], hit['%_identity'], hit['alignment_length'],
@@ -52,11 +62,49 @@ class BlastParser(object):
 			outfile.close()
 		return outfile_path
 
+	def extract_credible_hits(self, max_hits=2):
+		self.sort_blast_csv()
+		print self.count_hits()
+		query = False
+		results = []
+		for hit in self.hits:
+			if not query: 
+				query = hit['query_id']
+				results.append(hit)
+				i = 1
+			else:
+				if hit['query_id'] == query:
+					i += 1
+					if i <= max_hits:
+						results.append(hit)
+				else:
+					i = 1
+					query = hit['query_id']
+					results.append(hit)
+		outfile_path = file_from_path(self.bl_report_path, folder=True) + 'credible_hits.csv'
+		self.write_blast_csv(outfile_path=outfile_path, hits=results, header=True)
+		return outfile_path
 
-# blreport = '/home/anna/bioinformatics/euglena/223_mitogenes/223_mitogenes_b2go/m_genes_bl_report.csv'
-blreport = '/home/anna/bioinformatics/euglena/Tripanosoma_Verner/Tr_proteins/blast_db/bl_reports/Tr_prote/223_mitogenes_b2go_bl_report.csv'
+	def extract_unique_hits(self):
+		self.sort_blast_csv()
+		print self.count_hits()
+		query = False
+		results = []
+		for hit in self.hits:
+			if not query: 
+				query = hit['query_id']
+				result = hit
+				i = 1
+			else:
+				if hit['query_id'] == query:
+					i += 1
+				else:
+					if i == 1: results.append(result)
+					i = 1
+					query = hit['query_id']
+					result = hit
 
-out_path = '/home/anna/bioinformatics/euglena/out.csv' 
-new_bp = BlastParser(blreport, 'csv')
-sorted_hits = new_bp.sort_blast_csv()
-new_bp.write_blast_csv(hits=sorted_hits, outfile_path=out_path, header=True)
+		outfile_path = file_from_path(self.bl_report_path, folder=True) + 'unique_hits.csv'
+		print len(results), 'unique hits'
+		self.write_blast_csv(outfile_path=outfile_path, hits=results, header=True)
+		return outfile_path
--- py_scripts/blast/use_blast.py
+++ py_scripts/blast/use_blast.py
@@ -2,19 +2,21 @@
 import sys
 sys.path.insert(0, "/home/anna/bioinformatics/ngs/py_scripts/")
 from blast.classes.blast import Blast
+from blast.classes.blast_parser import BlastParser
+
 # query_path = '/home/anna/bioinformatics/euglena/Tripanosoma_Verner/Tr_proteins.fasta'
-# query_path = '/home/anna/bioinformatics/euglena/mDNA/Euglena_mito_genes/rnsR_Path_24504_1.fa'
-query_path = '/home/anna/bioinformatics/euglena/Tripanosoma_Verner/ATP_synthase_proteins.fasta'
-# query_path = '/home/anna/bioinformatics/euglena/223_mitogenes/223_mitogenes_b2go.fasta'
+query_path = '/home/anna/bioinformatics/euglena/223_mitogenes/223_mitogenes_b2go.fasta'
+
 db_path ='/home/anna/bioinformatics/euglena/Tripanosoma_Verner/Tr_proteins/blast_db/Tr_proteins.db'
 # db_path = '/home/anna/bioinformatics/euglena/E_gracilis_transcriptome_final.PROTEINS/blast_db/E_gracilis_transcriptome_final.PROTEINS.db'
-# db_path ='/home/anna/bioinformatics/euglena/E_gracilis_transcriptome_cds/db_folder/E_gracilis_transcriptome_cds.db'
-# db_path ='/home/anna/bioinformatics/euglena/Euglena_gracilis_genome_V1/db_folder/Euglena_gracilis_genome_V1.db'
 
 # new_blast = Blast(ref_path=ref_path, query_path=query_path, db_type='prot')
-new_blast = Blast(db_path=db_path, query_path=query_path, db_type='prot')
-new_blast.blast(bl_type='blastp', evalue=10, outfmt='tabular_comment_lines')
 
 # new_blast = Blast(ref_path=ref_path, query_path=query_path, db_type='nucl')
 # new_blast = Blast(db_path=db_path, query_path=query_path, db_type='nucl')
 # new_blast.blast(bl_type='blastn', evalue=10, outfmt='comma_values')
+
+# new_blast = Blast(db_path=db_path, query_path=query_path, db_type='prot')
+# bl_report = new_blast.blast(bl_type='blastp', evalue=0.1, outfmt='comma_values')
+bl_report = '/home/anna/bioinformatics/euglena/E_gracilis_transcriptome_final.PROTEINS/blast_db/bl_reports/E_gracilis_transcriptome_final.PROTE/Tr_proteins_bl_report.csv'
+BlastParser(bl_report, 'csv').extract_unique_hits()
