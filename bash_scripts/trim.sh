9b7d9d460e4d55adc4e49d4f8c76547c9ba225e7
AnnaNenarokova <a.nenarokova@gmail.com>
2015-11-18 11:16:27 +0100

    ni

--- bash_scripts/trim.sh
+++ bash_scripts/trim.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-trimc_dir='/home/anna/bioinformatics/bioprograms/Trimmomatic-0.33/'
-fastq_dir='/home/anna/bioinformatics/htses/katya/'
-trimc='trimmomatic-0.33.jar'
+trimc_dir='/home/anna_nenarokova/Trimmomatic-0.35/'
+fastq_dir='/home/anna_nenarokova/euglena/1_ELIS_reads/Sample_1-light/'
+trimc='trimmomatic-0.35.jar'
 cd $fastq_dir
 mkdir trim_out
-adapters_folder='adapters/all_trim.fa'
-adapters=$trimc_dir$adapters_folder
-file_fw='0sec_ACAGTG_L001_R1.fastq'
-file_rv='0sec_ACAGTG_L001_R2.fastq'
+adapters= 'adapters/TruSeq3-PE.fa'
+file_fw='L004_light_Elis_R1.fq'
+file_rv='L004_light_Elis_R2.fq'
 trim='java -jar ' 
 trim+=$trimc_dir$trimc
 illumina_clip='ILLUMINACLIP:'
 illumina_clip+=$adapters
 illumina_clip+=':2:30:10'
-$trim PE -phred33 $file_fw $file_rv trim_out/paired_out_fw trim_out/unpaired_out_fw trim_out/paired_out_rv trim_out/unpaired_out_rv $illumina_clip LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:30
+$trim PE -phred33 $file_fw $file_rv trim_out/paired_out_fw trim_out/unpaired_out_fw trim_out/paired_out_rv trim_out/unpaired_out_rv $illumina_clip LEADING:3 TRAILING:3 SLIDINGWINDOW:5:15 MINLEN:30 > trimming.log
