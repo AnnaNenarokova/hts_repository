5ba63c36437d0fd174a24332f2abbc79a78f42e9
AnnaNenarokova <a.nenarokova@gmail.com>
2016-05-16 14:21:25 +0200

    ni

--- bash_scripts/trim.sh
+++ bash_scripts/trim.sh
@@ -6,13 +6,14 @@ trimc_dir='/home/nenarokova/tools/Trimmomatic-0.36/'
 
 trim='java -jar '$trimc_dir'trimmomatic-0.36.jar'
 
-illumina_clip='ILLUMINACLIP:'$adapters':2:30:10'
 p_out_fw='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_paired_out_fw.fastq'
 u_out_fw='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_unpaired_out_fw.fastq'
 p_out_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_paired_out_rv.fastq'
 u_out_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_unpaired_out_rv.fastq'
 
+log = '/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_trimming.log
+
 file_fw='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/raw_reads/19109_8#2_1.fastq'
 file_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/raw_reads/19109_8#2_2.fastq'
 
-$trim PE -phred33 $file_fw $file_rv $p_out_fw $u_out_fw $p_out_rv $u_out_rv $illumina_clip LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
+$trim PE -phred33 $file_fw $file_rv $p_out_fw $u_out_fw $p_out_rv $u_out_rv LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
