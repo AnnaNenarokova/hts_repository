50c3a1db44a1b7f6e37eed529195d50ccdfe8256
AnnaNenarokova <a.nenarokova@gmail.com>
2016-05-16 14:21:54 +0200

    ni

--- bash_scripts/trim.sh
+++ bash_scripts/trim.sh
@@ -11,7 +11,7 @@ u_out_fw='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads
 p_out_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_paired_out_rv.fastq'
 u_out_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_unpaired_out_rv.fastq'
 
-log = '/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_trimming.log
+log = '/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_trimming.log'
 
 file_fw='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/raw_reads/19109_8#2_1.fastq'
 file_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/raw_reads/19109_8#2_2.fastq'
