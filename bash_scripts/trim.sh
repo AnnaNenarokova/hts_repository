f14d173aee6429027b87063883cba4749b4d6913
AnnaNenarokova <a.nenarokova@gmail.com>
2016-05-16 14:08:21 +0200

    ni

--- bash_scripts/trim.sh
+++ bash_scripts/trim.sh
@@ -12,7 +12,7 @@ u_out_fw='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads
 p_out_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_paired_out_rv.fastq'
 u_out_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/trimmed_reads/19109_8#2_unpaired_out_rv.fastq'
 
-file_fw='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/raw_reads19109_8#2_1.fastq'
+file_fw='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/raw_reads/19109_8#2_1.fastq'
 file_rv='/media/4TB1/kinetoplastids_hinxton/species/cer3/illumina/raw_reads/19109_8#2_2.fastq'
 
 $trim PE -phred33 $file_fw $file_rv $p_out_fw $u_out_fw $p_out_rv $u_out_rv $illumina_clip LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
