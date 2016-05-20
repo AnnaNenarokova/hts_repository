2dbbeab5fa37b0bbb86e496fa793194fbc14420c
AnnaNenarokova <a.nenarokova@gmail.com>
2016-05-20 19:00:39 +0200

    ni

--- rb_scripts/trim_all_tp.rb
+++ rb_scripts/trim_all_tp.rb
@@ -40,6 +40,7 @@ def run_in_pool(processes, threads)
     processes.each do |exec|
         pool.process do
             puts "executing"
+            `#{exec} 2> #{output_log}`
             puts ' ____________________________ '
         end
     end
