0665de72cab70328330a377478ebb434b1844626
AnnaNenarokova <a.nenarokova@gmail.com>
2016-05-31 17:22:55 +0200

    ni

--- bash_scripts/bowtie2.sh
+++ bash_scripts/bowtie2.sh
@@ -7,4 +7,4 @@ folder='/home/nenarokova/contaminants/trimmed_reads'
 bt2_base='/home/nenarokova/contaminants/genomes/leptomonas'
 cd $folder
 alignment='/home/nenarokova/contaminants/genomes/leptomonas.sam'
-bowtie2 --very-sensitive -p 30 --reorder -x $bt2_base -1 Leptomonas_seymouri_ad_q20_l50_paired_out_fw.fastq -2 Leptomonas_seymouri_ad_q20_l50_paired_out_rv.fastq -U Leptomonas_seymouri_ad_q20_l50_unpaired_out_fw.fastq,Leptomonas_seymouri_ad_q20_l50_unpaired_out_rv.fastq -S $alignment
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 30 --reorder -x $bt2_base -1 Leptomonas_seymouri_ad_q20_l50_paired_out_fw.fastq -2 Leptomonas_seymouri_ad_q20_l50_paired_out_rv.fastq -U Leptomonas_seymouri_ad_q20_l50_unpaired_out_fw.fastq,Leptomonas_seymouri_ad_q20_l50_unpaired_out_rv.fastq -S $alignment
