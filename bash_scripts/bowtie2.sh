db1be2606e8e31653fac29905ae0ff0b03e4f345
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-04 17:41:24 +0200

    ni

--- bash_scripts/bowtie2.sh
+++ bash_scripts/bowtie2.sh
@@ -7,4 +7,4 @@ folder='/home/nenarokova/contaminants/trimmed_reads'
 bt2_base='/home/nenarokova/contaminants/genomes/leptomonas'
 cd $folder
 alignment='/home/nenarokova/contaminants/genomes/leptomonas_test.sam'
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --fast -p 60 -x $bt2_base -1 H10-3_ACAGTG_L007_001_ad_q20_l50_paired_out_fw.fastq -2 H10-3_ACAGTG_L007_001_ad_q20_l50_paired_out_rv.fastq -U H10-3_ACAGTG_L007_001_ad_q20_l50_unpaired_out_fw.fastq,H10-3_ACAGTG_L007_001_ad_q20_l50_unpaired_out_rv.fastq -S $alignment
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --sensitive -p 60 -x $bt2_base -1 H10-3_ACAGTG_L007_001_ad_q20_l50_paired_out_fw.fastq -2 H10-3_ACAGTG_L007_001_ad_q20_l50_paired_out_rv.fastq -U H10-3_ACAGTG_L007_001_ad_q20_l50_unpaired_out_fw.fastq,H10-3_ACAGTG_L007_001_ad_q20_l50_unpaired_out_rv.fastq -S $alignment
