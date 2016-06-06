ba033189577729156e32a765969fc42dad949dee
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-06 14:41:14 +0200

    ni

--- bash_scripts/bowtie2_build.sh
+++ bash_scripts/bowtie2_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
-#PBS -l nodes=1:ppn=60
+#PBS -l nodes=1:ppn=30
 #PBS -d.
 cd /home/nenarokova/contaminants/genomes
 bw2_dir='/home/nenarokova/tools/bowtie2-2.2.9/'
-$bw2_dir'bowtie2-build' -t 60 Leptomonas_seymouri.fa seymouri
+$bw2_dir'bowtie2-build' --threads 30 Leptomonas_seymouri.fa seymouri
