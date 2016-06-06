f88bd94baf844ac905eeb9992be64f90c6c67461
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-06 14:37:01 +0200

    ni

--- bash_scripts/bowtie2_build.sh
+++ bash_scripts/bowtie2_build.sh
@@ -3,5 +3,5 @@
 #PBS -l nodes=1:ppn=60
 #PBS -d.
 cd /home/nenarokova/contaminants/genomes
-bw2_dir = '/home/nenarokova/tools/bowtie2-2.2.9/'
+bw2_dir='/home/nenarokova/tools/bowtie2-2.2.9/'
 $bw2_dir'bowtie2-build' -t 60 seymouri Leptomonas_seymouri.fa
