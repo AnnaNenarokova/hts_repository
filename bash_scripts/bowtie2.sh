a391243e5ad222d2ba891db8d2123384d5a87cbf
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-06 11:54:09 +0200

    ni

--- bash_scripts/bowtie2.sh
+++ bash_scripts/bowtie2.sh
@@ -7,4 +7,4 @@ folder='/home/nenarokova/contaminants/trimmed_reads'
 bt2_base='/home/nenarokova/contaminants/genomes/leptomonas'
 cd $folder
 alignment='/home/nenarokova/contaminants/genomes/leptomonas_test.sam'
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-fast -p 60 -x $bt2_base -1 B08_TAGCTT_L001_ad_q20_l50_paired_out_fw.fastq -2 B08_TAGCTT_L001_ad_q20_l50_paired_out_rv.fastq -U B08_TAGCTT_L001_ad_q20_l50_unpaired_out_fw.fastq,B08_TAGCTT_L001_ad_q20_l50_unpaired_out_rv.fastq -S $alignment 2> /home/nenarokova/bw2_stats/b08_h10.txt
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 60 -x $bt2_base -1 B08_TAGCTT_L001_ad_q20_l50_paired_out_fw.fastq -2 B08_TAGCTT_L001_ad_q20_l50_paired_out_rv.fastq -U B08_TAGCTT_L001_ad_q20_l50_unpaired_out_fw.fastq,B08_TAGCTT_L001_ad_q20_l50_unpaired_out_rv.fastq -S $alignment 2> /home/nenarokova/bw2_stats/b08_h10_vsens.txt
