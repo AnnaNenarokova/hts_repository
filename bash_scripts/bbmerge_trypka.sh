83fec88d68f34d771d3578329c63c452026e7ed7
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-09 16:55:22 +0200

    ni

--- bash_scripts/bbmerge_trypka.sh
+++ bash_scripts/bbmerge_trypka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-folder='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/trimmed_reads'
+folder='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/trimmed_reads/'
 fw=$folder'18021_1#6_paired_out_fw_ad_q20_l50.fastq'
 rv=$folder'18021_1#6_paired_out_rv_ad_q20_l50.fastq'
 merged='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18021_1#6_merged.fq'
