b95925fc70b7ccc531cf6d99a9747d765ea10295
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-09 17:11:19 +0200

    ni

--- bash_scripts/bbmerge_trypka.sh
+++ bash_scripts/bbmerge_trypka.sh
@@ -6,4 +6,4 @@ rv=$folder'18021_1#6_2.fastq'
 merged='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18021_1#6_merged.fq'
 unmerged_fw='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18021_1#6_unmerged_fw.fq'
 unmerged_rv='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18021_1#6_unmerged_rv.fq'
-/home/nenarokova/tools/bbmap/bbmerge.sh in1=$fw in2=$rv out=$merged outu1=$unmerged_fw outu2=$unmerged_rv strict=t qtrim2=t tbo=t usejni=t
+/home/nenarokova/tools/bbmap/bbmerge.sh in1=$fw in2=$rv out=$merged outu1=$unmerged_fw outu2=$unmerged_rv strict=t qtrim2=t usejni=t
