4071f787667649da600bc43afb7836f33b80c408
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-20 17:48:57 +0200

    ni

--- bash_scripts/trim_bbduk_trypka.sh
+++ bash_scripts/trim_bbduk_trypka.sh
@@ -6,7 +6,7 @@ rv=$folder'18021_1_7_unmerged_rv.fq'
 trimmed_fw='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18021_1_7_unmerged_trimmed_fw.fq'
 trimmed_rv='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18021_1_7_unmerged_trimmed_rv.fq'
 
-bbduk.sh in=$fw in=$rv out1=$trimmed_fw out2=$trimmed_rv qtrim=rl trimq=20
+/home/nenarokova/tools/bbmap/bbduk.sh in=$fw in=$rv out1=$trimmed_fw out2=$trimmed_rv qtrim=rl trimq=20
 
 
 fw=$folder'18098_1_7_unmerged_fw.fq'
@@ -14,4 +14,4 @@ rv=$folder'18098_1_7_unmerged_rv.fq'
 trimmed_fw='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18098_1_7_unmerged_trimmed_fw.fq'
 trimmed_rv='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18098_1_7_unmerged_trimmed_rv.fq'
 
-bbduk.sh in=$fw in=$rv out1=$trimmed_fw out2=$trimmed_rv qtrim=rl trimq=20
+/home/nenarokova/tools/bbmap/bbduk.sh in=$fw in=$rv out1=$trimmed_fw out2=$trimmed_rv qtrim=rl trimq=20
