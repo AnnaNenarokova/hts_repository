b5d0baa233d7b2ae3f96d0a6af52e7bc8e5322c6
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-20 17:47:11 +0200

    ni

---
+++ bash_scripts/fastqc_many.sh
@@ -0,0 +1,4 @@
+for f in *
+do
+    /home/nenarokova/tools/FastQC/fastqc $f &
+done
---
+++ bash_scripts/trim_bbduk_trypka.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+folder='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/reads'
+
+fw=$folder'18021_1_7_unmerged_fw.fq'
+rv=$folder'18021_1_7_unmerged_rv.fq'
+trimmed_fw='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18021_1_7_unmerged_trimmed_fw.fq'
+trimmed_rv='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18021_1_7_unmerged_trimmed_rv.fq'
+
+bbduk.sh in=$fw in=$rv out1=$trimmed_fw out2=$trimmed_rv qtrim=rl trimq=20
+
+
+fw=$folder'18098_1_7_unmerged_fw.fq'
+rv=$folder'18098_1_7_unmerged_rv.fq'
+trimmed_fw='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18098_1_7_unmerged_trimmed_fw.fq'
+trimmed_rv='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/18098_1_7_unmerged_trimmed_rv.fq'
+
+bbduk.sh in=$fw in=$rv out1=$trimmed_fw out2=$trimmed_rv qtrim=rl trimq=20
