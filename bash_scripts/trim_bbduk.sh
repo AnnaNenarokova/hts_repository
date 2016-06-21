974a34575404b69f27d2b96d9ca7320fc9334982
AnnaNenarokova <a.nenarokova@gmail.com>
2016-06-21 11:31:06 +0200

    ni

--- bash_scripts/trim_bbduk.sh
+++ bash_scripts/trim_bbduk.sh
@@ -7,7 +7,7 @@ rv=$folder'18021_1_7_unmerged_rv.fq'
 trimmed_fw='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/reads/18021_1_7_unmerged_trimmed_fw.fq'
 trimmed_rv='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/reads/18021_1_7_unmerged_trimmed_rv.fq'
 
-/home/nenarokova/tools/bbmap/bbduk.sh in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv minlen=50 qtrim=rl trimq=20 ktrim=r k=25 mink=11 ref=adapters.fa hdist=1
+/home/nenarokova/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv minlen=50 qtrim=rl trimq=20 ktrim=r k=25 mink=11 ref=adapters.fa hdist=1
 
 
 fw=$folder'18098_1_7_unmerged_fw.fq'
@@ -15,4 +15,4 @@ rv=$folder'18098_1_7_unmerged_rv.fq'
 trimmed_fw='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/reads/18098_1_7_unmerged_trimmed_fw.fq'
 trimmed_rv='/media/4TB1/kinetoplastids_hinxton/illumina/miseq/merged_reads/reads/18098_1_7_unmerged_trimmed_rv.fq'
 
-/home/nenarokova/tools/bbmap/bbduk.sh in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv minlen=50 qtrim=rl trimq=20 ktrim=r k=25 mink=11 ref=adapters.fa hdist=1
+/home/nenarokova/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv minlen=50 qtrim=rl trimq=20 ktrim=r k=25 mink=11 ref=adapters.fa hdist=1
