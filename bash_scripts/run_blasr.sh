df90518c4ac3125326bdd30adf7f583322a7161d
AnnaNenarokova <a.nenarokova@gmail.com>
2016-08-02 16:17:09 +0200

    run_blast.sh added

--- bash_scripts/run_blasr.sh
+++ bash_scripts/run_blasr.sh
@@ -5,7 +5,7 @@ ref='/home/nenarokova/kinetoplastids/contaminants/genomes/pandoraea_apista_AU216
 query='/home/nenarokova/kinetoplastids/pacbio/raw_reads/e262/m151013_144051_00127_c100885902550000001823202004021681_s1_p0.1.bax.h5'
 alignment='/home/nenarokova/kinetoplastids/pacbio/m151013_144051_00127_c100885902550000001823202004021681_s1_p0_1_test.aln'
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/nenarokova/tools/blasr_install/blasr/libcpp/hdf
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/nenarokova/tools/blasr_install/blasr/
 
 blasr='/home/nenarokova/tools/blasr_install/blasr/blasr'
 $blasr $query $ref -out $alignment -nproc 60 -m 0
