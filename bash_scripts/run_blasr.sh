02a951d167a4a7cc15862b9683fa4a5867d4393c
AnnaNenarokova <a.nenarokova@gmail.com>
2016-08-03 11:30:18 +0200

    run_blasr fixed

--- bash_scripts/run_blasr.sh
+++ bash_scripts/run_blasr.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=60
-ref='/home/nenarokova/kinetoplastids/contaminants/genomes/pandoraea_apista_AU2161.fasta'
-query='/home/nenarokova/kinetoplastids/pacbio/raw_reads/e262/m151013_144051_00127_c100885902550000001823202004021681_s1_p0.1.bax.h5'
-alignment='/home/nenarokova/kinetoplastids/pacbio/m151013_144051_00127_c100885902550000001823202004021681_s1_p0_1_test.aln'
+
 libcpp='/home/nenarokova/tools/blasr_install/blasr/libcpp'
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$libcpp/hdf:$libcpp/alignment:$libcpp/pbdata:/home/nenarokova/tools/blasr_install/hdf5/hdf5-1.8.16-linux-centos6-x86_64-gcc447-shared/lib
-
 blasr='/home/nenarokova/tools/blasr_install/blasr/blasr'
-$blasr $query $ref --out $alignment --nproc 60 -m 0
+
+ref='/home/nenarokova/kinetoplastids/contaminants/genomes/pandoraea_apista_AU2161.fasta'
+folder='/home/nenarokova/kinetoplastids/pacbio/raw_reads/e262/'
+
+for f in $folder*.bax.h5
+do
+  $blasr $f $ref --nproc 60 -m 0
+done
