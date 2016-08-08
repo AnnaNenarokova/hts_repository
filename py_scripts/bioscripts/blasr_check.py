80c18dc4db332b0eb2d633d61d049cc2834c038d
AnnaNenarokova <a.nenarokova@gmail.com>
2016-08-08 09:54:28 +0200

    ni

--- py_scripts/bioscripts/blasr_check.py
+++ py_scripts/bioscripts/blasr_check.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
-lpyr_alignment = '/home/anna/Dropbox/phd/bioinformatics/genomes/hinxton/novymonas/Lpyr_aligned_PacBio_reads.txt'
-pand_alignment = '/home/anna/Dropbox/phd/bioinformatics/genomes/hinxton/novymonas/p_apista_blasr_only_reads'
-movie_prefix = None
+lpyr_alignment = '/home/nenarokova/kinetoplastids/illumina/assembly/E262_contigs_pacbio_blasr.out'
+pand_alignment = '/home/nenarokova/kinetoplastids/contaminants/p_apista_blasr_only_reads'
+
 pand = []
 for line in open(pand_alignment):
     pand.append(line.rstrip())
 
 for i, line in enumerate(open(lpyr_alignment)):
-    if i % 10000 == 0:
-        print 'line', i
-
-    alignment_id = line.split()[0]
-    if alignment_id in pand:
-        print alignment_id
+    if line[:16] == '         Query: '
+        alignment_id = line[17:].rstrip()
+        if alignment_id in pand:
+            print alignment_id
