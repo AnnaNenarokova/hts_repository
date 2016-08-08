47d6b8b28de385186d0ae75ad5879fb028494aaf
AnnaNenarokova <a.nenarokova@gmail.com>
2016-08-08 09:59:29 +0200

    ni

--- py_scripts/bioscripts/blasr_check.py
+++ py_scripts/bioscripts/blasr_check.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-lpyr_alignment = '/home/nenarokova/kinetoplastids/illumina/assembly/E262_contigs_pacbio_blasr.out'
+e262_alignment = '/home/nenarokova/kinetoplastids/illumina/assembly/E262_contigs_pacbio_blasr.out'
 pand_alignment = '/home/nenarokova/kinetoplastids/contaminants/p_apista_blasr_only_reads'
 
 pand = []
 for line in open(pand_alignment):
     pand.append(line.rstrip())
 
-for i, line in enumerate(open(lpyr_alignment)):
+for i, line in enumerate(open(e262_alignment)):
     if line[:16] == '         Query: '
         alignment_id = line[17:].rstrip()
         if alignment_id in pand:
