1a2292a5c88b82817c4d10f24f64c8c67cd52744
AnnaNenarokova <a.nenarokova@gmail.com>
2016-10-05 17:00:49 +0200

    ni

--- bash_scripts/bowtie2_pbs_krtecek.sh
+++ bash_scripts/bowtie2_pbs_krtecek.sh
@@ -7,8 +7,8 @@ report_folder='/home/nenarokova/genomes/kinetoplastids/pacbio/assembly/bw2_repor
 read_folder='/home/nenarokova/contaminants/trimmed_reads/'
 alignment_folder='/home/nenarokova/genomes/kinetoplastids/pacbio/assembly/bw2_mapping/alignments/'
 
-fw_paired='18021_1_4_paired_out_fw_ad_q20_l50.fastq.gz'
-rv_paired='18021_1_4_paired_out_rv_ad_q20_l50.fastq.gz'
+fw_paired=$read_folder'18021_1_4_paired_out_fw_ad_q20_l50.fastq.gz'
+rv_paired=$read_folder'18021_1_4_paired_out_rv_ad_q20_l50.fastq.gz'
 fw_unpaired=$read_folder'18021_1_4_unpaired_out_fw_ad_q20_l50.fastq.gz'
 rv_unpaired=$read_folder'18021_1_4_unpaired_out_rv_ad_q20_l50.fastq.gz'
 alignment=$alignment_folder'18021_1_4_alignment.sam'
