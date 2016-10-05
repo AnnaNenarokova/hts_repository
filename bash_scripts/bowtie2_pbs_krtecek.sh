8e000882b26617947c7973bd3b0c7cd6f06bd933
AnnaNenarokova <a.nenarokova@gmail.com>
2016-10-05 16:59:48 +0200

    ni

--- bash_scripts/bowtie2_pbs_krtecek.sh
+++ bash_scripts/bowtie2_pbs_krtecek.sh
@@ -3,7 +3,7 @@
 #PBS -l nodes=1:ppn=60
 
 bt2_base='/home/nenarokova/contaminants/genomes/'
-reports_folder='/home/nenarokova/genomes/kinetoplastids/pacbio/assembly/bw2_reports/'
+report_folder='/home/nenarokova/genomes/kinetoplastids/pacbio/assembly/bw2_reports/'
 read_folder='/home/nenarokova/contaminants/trimmed_reads/'
 alignment_folder='/home/nenarokova/genomes/kinetoplastids/pacbio/assembly/bw2_mapping/alignments/'
 
@@ -12,5 +12,6 @@ rv_paired='18021_1_4_paired_out_rv_ad_q20_l50.fastq.gz'
 fw_unpaired=$read_folder'18021_1_4_unpaired_out_fw_ad_q20_l50.fastq.gz'
 rv_unpaired=$read_folder'18021_1_4_unpaired_out_rv_ad_q20_l50.fastq.gz'
 alignment=$alignment_folder'18021_1_4_alignment.sam'
-report=$reports_folder'18021_1_4_mapping_report.txt'
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 60 -x $bt2_base -1 $fw_paired -2 $rv_paired -U $fw_unpaired,$rv_unpaired -S $alignment 2> $report
+report=$report_folder'18021_1_4_mapping_report.txt'
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 60 -x $bt2_base -1 $fw_paired -2 $rv_paired -U $fw_unpaired,$rv_unpaired -S $alignment
+# 2> $report
