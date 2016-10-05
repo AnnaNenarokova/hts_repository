69ddaf37436bbda28e96faf5dd0e56ca32f55606
AnnaNenarokova <a.nenarokova@gmail.com>
2016-10-05 16:52:50 +0200

    ni

--- bash_scripts/bowtie2_pbs_krtecek.sh
+++ bash_scripts/bowtie2_pbs_krtecek.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
-#PBS -l nodes=1:ppn=30
+#PBS -l nodes=1:ppn=60
 
-folder='/home/nenarokova/contaminants/trimmed_reads'
-cd $folder
-alignment='/home/nenarokova/contaminants/genomes/test.sam'
+bt2_base='/home/nenarokova/contaminants/genomes/'
+bw2_reports='/home/nenarokova/genomes/kinetoplastids/pacbio/assembly/bw2_reports/'
+read_folder='/home/nenarokova/contaminants/trimmed_reads/'
+alignment_folder='/home/nenarokova/genomes/kinetoplastids/pacbio/assembly/bw2_mapping/alignments/'
 
-bt2_base='/home/nenarokova/contaminants/genomes/TriTrypDB-9_0_CfasciculataCfCl_Genome'
-
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 30 -x $bt2_base -1 c_thermophila_dna_ad_q20_l50_paired_out_fw.fastq -2 c_thermophila_dna_ad_q20_l50_paired_out_rv.fastq -U c_thermophila_dna_ad_q20_l50_unpaired_out_fw.fastq,c_thermophila_dna_ad_q20_l50_unpaired_out_rv.fastq -S $alignment 2> /home/nenarokova/contaminants/bw2_stats/thermophila_fasciculata_vsens.txt
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 30 -x $bt2_base -1 H10-3_ACAGTG_L007_001_ad_q20_l50_paired_out_fw.fastq -2 H10-3_ACAGTG_L007_001_ad_q20_l50_paired_out_rv.fastq -U H10-3_ACAGTG_L007_001_ad_q20_l50_unpaired_out_fw.fastq,H10-3_ACAGTG_L007_001_ad_q20_l50_unpaired_out_rv.fastq -S $alignment 2> /home/nenarokova/contaminants/bw2_stats/h10_fasciculata_vsens.txt
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 30 -x $bt2_base -1 Leptomonas_seymouri_ad_q20_l50_paired_out_fw.fastq -2 Leptomonas_seymouri_ad_q20_l50_paired_out_rv.fastq -U Leptomonas_seymouri_ad_q20_l50_unpaired_out_fw.fastq,Leptomonas_seymouri_ad_q20_l50_unpaired_out_rv.fastq -S $alignment 2> /home/nenarokova/contaminants/bw2_stats/seymouri_fasciculata_vsens.txt
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 30 -x $bt2_base -1 B08_TAGCTT_L001_ad_q20_l50_paired_out_fw.fastq -2 B08_TAGCTT_L001_ad_q20_l50_paired_out_rv.fastq -U B08_TAGCTT_L001_ad_q20_l50_unpaired_out_fw.fastq,B08_TAGCTT_L001_ad_q20_l50_unpaired_out_rv.fastq -S $alignment 2> /home/nenarokova/contaminants/bw2_stats/b08_fasciculata_vsens.txt
+fw_paired='18021_1_4_paired_out_fw_ad_q20_l50.fastq.gz'
+rv_paired='18021_1_4_paired_out_rv_ad_q20_l50.fastq.gz'
+fw_unpaired=$read_folder'18021_1_4_unpaired_out_fw_ad_q20_l50.fastq.gz'
+rv_unpaired=$read_folder'18021_1_4_unpaired_out_rv_ad_q20_l50.fastq.gz'
+alignment=$alignment_folder'18021_1_4_alignment.sam'
+report=$reads_folder'18021_1_4_mapping_report.txt'
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 60 -x $bt2_base -1 $fw_paired -2 $rv_paired -U $fw_unpaired,$rv_unpaired -S $alignment 2> $report
