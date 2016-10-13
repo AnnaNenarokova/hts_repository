e05000c9d4a80696e07a0aaca08891fa7f7219ab
AnnaNenarokova <a.nenarokova@gmail.com>
2016-10-13 17:03:25 +0200

    ni

--- py_scripts/blast/use_blast.py
+++ py_scripts/blast/use_blast.py
@@ -33,17 +33,17 @@ def add_header(blast_csv_path, custom_outfmt):
     write_list_of_lists(blast_hits, blast_csv_path, header=header)
     return blast_csv_path
 
-query_path = '/home/nenarokova/genomes/Trypanoplasma_borreli/miniasm/reads.fasta'
+query_path = '/home/nenarokova/trypanoplasma/miniasm_contigs_sorted.fasta'
 custom_outfmt = 'qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send'
 subj_pathes = [
-'/home/nenarokova/genomes/kinetoplastids/contaminants/SSU_NCBI_141_Votep_Hinxton.fasta',
+'/home/pasha/CLC_Databases/nt',
 ]
 
 for subj_path in subj_pathes:
-    new_blast = Blast(query_path=query_path, subj_path=subj_path, db_type='nucl', threads=20)
+    new_blast = Blast(query_path=query_path, subj_path=subj_path, db_type='nucl', threads=32)
     blast_csv_path = new_blast.blast(
                                      bl_type='blastn',
-                                     evalue=0.01,
+                                     evalue=0.00001,
                                      outfmt='comma_values',
                                      custom_outfmt=custom_outfmt,
                                      word_size=7
