021c496291804677e73b7f32d0d7151c3d5ad966
AnnaNenarokova <a.nenarokova@gmail.com>
2016-10-14 13:30:23 +0200

    ni

--- bash_scripts/run_blasr.sh
+++ bash_scripts/run_blasr.sh
@@ -8,9 +8,13 @@ blasr='/home/nenarokova/tools/blasr_install/blasr/blasr'
 
 ref='/home/nenarokova/genomes/Trypanoplasma_borreli/miniasm/contigs.fasta'
 folder='/home/nenarokova/genomes/Trypanoplasma_borreli/TCS_Michael_Giolai_TGAC/Raw_reads/all_subreads/'
-out='/home/nenarokova/genomes/Trypanoplasma_borreli/miniasm/read_mapping.sam'
+out='/home/nenarokova/genomes/Trypanoplasma_borreli/miniasm/read_mapping.bam'
 
-for f in $folder*.fastq
-do
-  $blasr $f $ref --nproc 60 -sam >> $out
-done
+# for f in $folder*.fastq
+# do
+#   $blasr $f $ref --nproc 60 -sam >> $out
+# done
+
+f1="/home/nenarokova/genomes/Trypanoplasma_borreli/TCS_Michael_Giolai_TGAC/Raw_reads/D06_1/Analysis_Results/m151008_074519_42165_c100914232550000001823208104301613_s1_p0.bas.h5"
+f2="/home/nenarokova/genomes/Trypanoplasma_borreli/TCS_Michael_Giolai_TGAC/Raw_reads/E06_1/Analysis_Results/m151008_120454_42165_c100914232550000001823208104301614_s1_p0.bas.h5"
+$blasr $f1 $2 $ref --nproc 60 -bam -clipping soft --out $out
