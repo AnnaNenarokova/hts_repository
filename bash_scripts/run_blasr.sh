0f959c0f9660d73d03eaf2976a9702d1b391a2a7
AnnaNenarokova <a.nenarokova@gmail.com>
2016-10-26 14:41:26 +0200

    ni

--- bash_scripts/run_blasr.sh
+++ bash_scripts/run_blasr.sh
@@ -10,5 +10,5 @@ blasr='/home/nenarokova/tools/blasr/bin/blasr'
 ref='/home/nenarokova/genomes/Trypanoplasma_borreli/miniasm/contigs.fasta'
 out='/home/nenarokova/genomes/Trypanoplasma_borreli/miniasm/read_mapping.bam'
 
-f1="/home/nenarokova/genomes/Trypanoplasma_borreli/TCS_Michael_Giolai_TGAC/Raw_reads/D06_1/Analysis_Results/m151008_074519_42165_c100914232550000001823208104301613_s1_p0.bas.h5"
+f1='/home/nenarokova/genomes/trypanoplasma/TCS_Michael_Giolai_TGAC/Raw_reads/all_subreads/m151008_074519_42165_c100914232550000001823208104301613_s1_p0.1.subreads.fastq'
 $blasr $f1 $ref --nproc 60 --clipping soft --bam $out
