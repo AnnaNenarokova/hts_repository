8d9b9ec869ea92bdd275713e43e3cb16716d46ae
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-02 13:06:33 +0100

    ni

---
+++ bash_scripts/run_pilon_trypka.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/nenarokova/tools/pilon/target/scala-2.11
+genome="/home/nenarokova/genomes/Trypanoplasma_borreli/miniasm/contigs.fasta"
+bam="/media/4TB3/trypanoplasma/pacbio_read_mapping.bam"
+outdir="/media/4TB3/trypanoplasma/pilon_out"
+output="trypanoplasma"
+
+java -Xmx16G -jar pilon_2.11-1.20-one-jar.jar --genome $genome --output $output --outdir $outdir --bam $bam --threads --verbose
