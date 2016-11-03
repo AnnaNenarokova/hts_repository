2a80172760c1d40afe0f72f5832351311d79f03e
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-03 18:03:06 +0100

    ni

--- bash_scripts/run_pilon_trypka.sh
+++ bash_scripts/run_pilon_trypka.sh
@@ -2,6 +2,7 @@
 cd /home/nenarokova/tools/pilon/target/scala-2.11
 genome="/media/4TB3/trypanoplasma/miniasm/contigs.fasta"
 bam="/media/4TB3/trypanoplasma/mapping/pacbio_reads_sorted.bam"
-outdir="/media/4TB3/trypanoplasma/pilon_out"
+outdir="/media/4TB3/trypanoplasma/pilon_out/"
 output="trypanoplasma"
-java -Xmx16G -jar pilon_2.11-1.20-one-jar.jar --genome $genome --output $output --outdir $outdir --bam $bam --threads 32 --verbose
+report="/media/4TB3/trypanoplasma/pilon_out/pilon_out.txt"
+java -Xmx16G -jar pilon_2.11-1.20-one-jar.jar --genome $genome --output $output --outdir $outdir --bam $bam --threads 32 &>$report
--- bash_scripts/sam_to_bam.sh
+++ bash_scripts/sam_to_bam.sh
@@ -3,9 +3,10 @@ source /home/smrtanalysis/current/etc/setup.sh
 
 samfile="/media/4TB3/trypanoplasma/mapping/pacbio_reads.sam"
 bamfile="/media/4TB3/trypanoplasma/mapping/pacbio_reads_unsorted.bam"
-sorted="/media/4TB3/trypanoplasma/mapping/pacbio_reads_sorted.bam"
-samtools view -bS $samfile > $bamfile @ 32
-samtools sort $bamfile $sorted @ 32
-samtools index $sorted @ 32
+sorted="/media/4TB3/trypanoplasma/mapping/pacbio_reads_sorted"
+sorted_file=$sorted".bam"
+samtools view -bS $samfile > $bamfile -@ 32
+samtools sort $bamfile $sorted -@ 32
+samtools index $sorted_file -@ 32
 
-chmod 644 $sorted
+chmod 644 $sorted_file
