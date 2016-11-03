bddeab95abe41832d29eab1fc436753a889daf2e
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-03 10:58:13 +0100

    ni

--- bash_scripts/bowtie2_e262_krtecek.sh
+++ bash_scripts/bowtie2_e262_krtecek.sh
@@ -6,8 +6,11 @@ bt2_base='/home/nenarokova/genomes/hinxton_species/contaminants/genomes/leptomon
 
 fw_paired="/home/nenarokova/genomes/novymonas/raw_illumina/E262_1.fastq.gz"
 rv_paired="/home/nenarokova/genomes/novymonas/raw_illumina/E262_2.fastq.gz"
+clean_reads_paired="/home/nenarokova/genomes/novymonas/mapping/l_pyrrhocoris/clean_reads_paired.fq.gz"
+clean_reads_unpaired="/home/nenarokova/genomes/novymonas/mapping/l_pyrrhocoris/clean_reads_unpaired.fq.gz"
+
 
 alignment="/home/nenarokova/genomes/novymonas/mapping/l_pyrrhocoris/l_pyrrhocoris.sam"
 report="/home/nenarokova/genomes/novymonas/mapping/l_pyrrhocoris/l_pyrrhocoris_bw2_stats.txt"
 
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-fast -p 60 -x $bt2_base -1 $fw_paired -2 $rv_paired  -S $alignment 2> $report
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-fast -p 60 -x $bt2_base -1 $fw_paired -2 $rv_paired --un-gz $clean_reads_unpaired --un-conc-gz $clean_reads_paired -S $alignment 2> $report
