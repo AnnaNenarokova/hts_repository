7c8465236ffb0c68431962096527c167f5f7692f
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-03 08:48:43 +0100

    ni

--- bash_scripts/sam_to_bam.sh
+++ bash_scripts/sam_to_bam.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+source /home/smrtanalysis/current/etc/setup.sh
 
 samfile="/media/4TB3/trypanoplasma/mapping/pacbio_reads.sam"
 bamfile="/media/4TB3/trypanoplasma/mapping/pacbio_reads_unsorted.bam"
 sorted="/media/4TB3/trypanoplasma/mapping/pacbio_reads_sorted.bam"
-samtools view -bS $samfile > $bamfile
-samtools sort $bamfile $sorted
-samtools index $sorted
+samtools view -bS $samfile > $bamfile @ 32
+samtools sort $bamfile $sorted @ 32
+samtools index $sorted @ 32
 
+chmod 644 $sorted
