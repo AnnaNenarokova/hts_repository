84906716cb2a84b58c7283f630181be3f171c292
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-04 14:35:37 +0100

    ni

--- bash_scripts/fastqc_many_krtecek.sh
+++ bash_scripts/fastqc_many_krtecek.sh
@@ -3,6 +3,6 @@
 dir1="/home/nenarokova/genomes/novymonas/raw_illumina/WT_MiSeq_trimmed/with_endosym_trimmed/"
 dir2="/home/nenarokova/genomes/novymonas/raw_illumina/WT_MiSeq_trimmed/without_endosym_trimmed/"
 
-cd $dir1
+cd $dir2
 f=`ls -1 | tail -n $PBS_ARRAYID | head -1`
 /home/nenarokova/tools/FastQC/fastqc $f
