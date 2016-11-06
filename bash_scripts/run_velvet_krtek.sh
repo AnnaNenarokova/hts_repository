18bd06eba55d1fa385526ca71526f19091f32d39
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-06 16:13:33 +0100

    ni

---
+++ bash_scripts/run_velvet_krtek.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -l walltime=100:00:00
+#PBS -l nodes=1:ppn=32
+
+reads1="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_trimmed/with_endosym_trimmed/wt_S2_L001_trimmed_1P.fq"
+reads2="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_trimmed/with_endosym_trimmed/wt_S2_L001_trimmed_2P.fq"
+reads3="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_trimmed/with_endosym_trimmed/wt_S2_L001_trimmed_1U.fq"
+reads4="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_trimmed/with_endosym_trimmed/wt_S2_L001_trimmed_2U.fq"
+
+/home/nenarokova/tools/VelvetOptimiser-2.2.5/VelvetOptimiser.pl -s 27 -e 151 -f '-shortPaired -fasta -separate $reads1 $reads2 -short -fastq $reads3 -short -fastq $reads4' -t 32
