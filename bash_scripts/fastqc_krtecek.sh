9631069b8b101c71392e382413f07ed16f70eadf
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-08 16:10:11 +0100

    ni

--- bash_scripts/fastqc_krtecek.sh
+++ bash_scripts/fastqc_krtecek.sh
@@ -2,7 +2,16 @@
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=30
 
-f="/home/nenarokova/genomes/novymonas/raw_illumina/WT_MiSeq_trimmed/with_endosym_trimmed/wt_S2_L001_trimmed_1P.fq"
+cd /home/nenarokova/genomes/novymonas/raw_illumina/miseq_raw
 
-/home/nenarokova/tools/FastQC/fastqc $f --threads 30
+f1="azi_S1_L001_R1_001.fastq.gz"
+f2="azi_S1_L001_R2_001.fastq.gz"
+f3="wt_S2_L001_R1_001.fastq.gz"
+f4="wt_S2_L001_R2_001.fastq.gz"
+
+/home/nenarokova/tools/FastQC/fastqc $f1 &
+/home/nenarokova/tools/FastQC/fastqc $f2 &
+/home/nenarokova/tools/FastQC/fastqc $f3 &
+/home/nenarokova/tools/FastQC/fastqc $f4 &
+
 
--- rb_scripts/count_each_element_in_list.rb
+++ rb_scripts/count_each_element_in_list.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-f_path = '/home/anna/Dropbox/phd/bioinformatics/genomes/euglena/data/ko.txt'
+f_path = '/home/anna/Dropbox/phd/bioinformatics/genomes/trypanosomatids/novymonas/blast_reports/E262_endosymbiont_gen/count.txt'
 l = IO.readlines(f_path)
 puts l.uniq.length
-# l.uniq.each { |e| puts "#{e} - #{l.count(e)}" }
+l.uniq.each { |e| puts "#{e} - #{l.count(e)}" }
