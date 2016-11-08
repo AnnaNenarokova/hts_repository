e82eef5a240925643b6f0c352b6b53131af40a5d
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-08 16:22:40 +0100

    ni

--- bash_scripts/bbmerge2.sh
+++ bash_scripts/bbmerge2.sh
@@ -4,11 +4,11 @@
 
 dir_raw='/home/nenarokova/genomes/novymonas/raw_illumina/miseq_raw/'
 
-fw=$dir_raw'azi_S1_L001_R1_001.fastq.gz'
-rv=$dir_raw'azi_S1_L001_R2_001.fastq.gz'
+fw=$dir_raw'wt_S2_L001_R1_001.fastq.gz'
+rv=$dir_raw'wt_S2_L001_R2_001.fastq.gz'
 
 dir_merged="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_merged/"
-merged=$dir_merged'azi_S1_L001_merged.fa'
-unmerged_fw=$dir_merged'azi_S1_L001_unmerged_1.fa'
-unmerged_rv=$dir_merged'azi_S1_L001_unmerged_2.fa'
+merged=$dir_merged'wt_S2_L001_merged.fa'
+unmerged_fw=$dir_merged'wt_S2_L001_unmerged_1.fa'
+unmerged_rv=$dir_merged'wt_S2_L001_unmerged_2.fa'
 /home/nenarokova/tools/bbmap/bbmerge.sh t=64 in1=$fw in2=$rv out=$merged outu1=$unmerged_fw outu2=$unmerged_rv strict=t qtrim2=t usejni=t
--- bash_scripts/fastqc_krtecek.sh
+++ bash_scripts/fastqc_krtecek.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
-#PBS -l nodes=1:ppn=4
+#PBS -l nodes=1:ppn=1
 
-cd /home/nenarokova/genomes/novymonas/raw_illumina/miseq_raw
+f=""
 
-f1="azi_S1_L001_R1_001.fastq.gz"
-f2="azi_S1_L001_R2_001.fastq.gz"
-f3="wt_S2_L001_R1_001.fastq.gz"
-f4="wt_S2_L001_R2_001.fastq.gz"
 
-/home/nenarokova/tools/FastQC/fastqc $f1 &
-/home/nenarokova/tools/FastQC/fastqc $f2 &
-/home/nenarokova/tools/FastQC/fastqc $f3 &
-/home/nenarokova/tools/FastQC/fastqc $f4
+/home/nenarokova/tools/FastQC/fastqc $f
+
 
 
