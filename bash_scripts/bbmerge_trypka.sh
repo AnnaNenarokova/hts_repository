7c107d9901c5cf6624211532823faf15aa1204f1
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-09 13:08:59 +0100

    ni

--- bash_scripts/bbmerge_trypka.sh
+++ bash_scripts/bbmerge_trypka.sh
@@ -2,12 +2,12 @@
 
 dir_raw='/media/4TB1/novymonas/raw_reads/'
 
-fw=$dir_raw'azi_S1_L001_R1_001.fastq.gz'
-rv=$dir_raw'azi_S1_L001_R2_001.fastq.gz'
+fw=$dir_raw'wt_S2_L001_R1_001.fastq.gz'
+rv=$dir_raw'wt_S2_L001_R1_001.fastq.gz'
 
 dir_merged="/media/4TB1/novymonas/merged_reads/"
-merged=$dir_merged'azi_S1_L001_merged.fa'
-unmerged_fw=$dir_merged'azi_S1_L001_unmerged_1.fa'
-unmerged_rv=$dir_merged'azi_S1_L001_unmerged_2.fa'
-report=$dir_merged'azi_S1_L001_report.txt'
+merged=$dir_merged'wt_S2_L001_R1_001.fastq.gz'
+unmerged_fw=$dir_merged'wt_S2_L001_R1_001.fastq.gz'
+unmerged_rv=$dir_merged'wt_S2_L001_R1_001.fastq.gz'
+report=$dir_merged'wt_S2_L001_R1_001.fastq.gz'
 /home/nenarokova/tools/bbmap/bbmerge.sh t=32 in1=$fw in2=$rv out=$merged outu1=$unmerged_fw outu2=$unmerged_rv strict=t qtrim2=t usejni=t -tbo 2> $report
