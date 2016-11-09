eca331ff971b92a53031a66b3932621ecb599234
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-09 13:27:08 +0100

    ni

--- bash_scripts/bbmerge_trypka.sh
+++ bash_scripts/bbmerge_trypka.sh
@@ -7,8 +7,8 @@ rv=$dir_raw'azi_S1_L001_R2_001.fastq.gz'
 
 dir_merged='/media/4TB1/novymonas/merged_reads/'
 name='azi_S1'
-merged=$dir_merged$name'_merged.fa'
-unmerged_fw=$dir_merged$name'_unmerged_1.fa'
-unmerged_rv=$dir_merged$name'_unmerged_2.fa'
+merged=$dir_merged$name'_merged.fq'
+unmerged_fw=$dir_merged$name'_unmerged_1.fq'
+unmerged_rv=$dir_merged$name'_unmerged_2.fq'
 report=$dir_merged$name'.txt'
 /home/nenarokova/tools/bbmap/bbmerge.sh t=32 in1=$fw in2=$rv out=$merged outu1=$unmerged_fw outu2=$unmerged_rv strict=t qtrim2=t usejni=t -tbo 2> $report
