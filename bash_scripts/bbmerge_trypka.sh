deee4364080efef423372a44c0a513f56e7bc03e
AnnaNenarokova <a.nenarokova@gmail.com>
2016-11-09 13:15:42 +0100

    ni

--- bash_scripts/bbmerge_trypka.sh
+++ bash_scripts/bbmerge_trypka.sh
@@ -6,8 +6,8 @@ fw=$dir_raw'wt_S2_L001_R1_001.fastq.gz'
 rv=$dir_raw'wt_S2_L001_R2_001.fastq.gz'
 
 dir_merged="/media/4TB1/novymonas/merged_reads/"
-merged=$dir_merged'wt_S2_L001.fastq.gz'
-unmerged_fw=$dir_merged'wt_S2_L001_R1_001.fa'
-unmerged_rv=$dir_merged'wt_S2_L001_R2_001.fa'
+merged=$dir_merged'wt_S2_L001_merged'
+unmerged_fw=$dir_merged'wt_S2_L001_unmerged_1.fa'
+unmerged_rv=$dir_merged'wt_S2_L001_unmerged_2.fa'
 report=$dir_merged'wt_S2_L001.txt'
 /home/nenarokova/tools/bbmap/bbmerge.sh t=32 in1=$fw in2=$rv out=$merged outu1=$unmerged_fw outu2=$unmerged_rv strict=t qtrim2=t usejni=t -tbo 2> $report
