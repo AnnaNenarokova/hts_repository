8706ac4c9d3d5629b0e72ab13f76c47da0486446
anna <a.nenarokova@gmail.com>
2016-11-18 12:19:01 +0100

    ni

--- bash_scripts/bowtie2_build.sh
+++ bash_scripts/bowtie2_build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=60
-#PBS -d.
-cd /home/nenarokova/genomes/kinetoplastids/pacbio/assembly
 bw2_dir='/home/nenarokova/tools/bowtie2-2.2.9/'
-$bw2_dir'bowtie2-build' --threads 60 Mbr04_Wallacemonas_polished_assembly.fasta wallacemonas
-$bw2_dir'bowtie2-build' --threads 60 PNG-M02_Aambiguus_polished_assembly.fasta angomonas
+cd /home/nenarokova/genomes/novymonas/assembly/wt_all_spades/
+$bw2_dir'bowtie2-build' --threads 60 contigs.fasta wt_novymonas
