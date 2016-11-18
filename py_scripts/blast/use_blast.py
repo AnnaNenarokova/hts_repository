9e368e12d67352f13f9d44b1f3df587ac6cf890c
anna <a.nenarokova@gmail.com>
2016-11-18 14:40:28 +0100

    ni

--- py_scripts/blast/use_blast.py
+++ py_scripts/blast/use_blast.py
@@ -33,7 +33,7 @@ def add_header(blast_csv_path, custom_outfmt):
     write_list_of_lists(blast_hits, blast_csv_path, header=header)
     return blast_csv_path
 
-query_path = '/media/4TB1/novymonas/e262_hiseq_contigs.fa'
+query_path = '/media/4TB1/novymonas/spades_contigs.fasta'
 custom_outfmt = 'qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send'
 subj_pathes = [
 "/media/4TB1/novymonas/pandoraea_all.fasta"
