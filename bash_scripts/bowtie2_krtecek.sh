01f1da2c1b426baf47b533aa94c226c7067b61d3
anna <a.nenarokova@gmail.com>
2016-11-21 19:51:36 +0100

    ni

--- bash_scripts/bowtie2_krtecek.sh
+++ bash_scripts/bowtie2_krtecek.sh
@@ -2,10 +2,9 @@
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=60
 
-bw2_dir='/home/nenarokova/tools/bowtie2-2.2.9/'
-base_name='/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10'
-$bw2_dir'bowtie2-build' --threads 60 $base_name'.fa' $base_name
-
+# bw2_dir='/home/nenarokova/tools/bowtie2-2.2.9/'
+# base_name='/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10'
+# $bw2_dir'bowtie2-build' --threads 60 $base_name'.fa' $base_name
 
 s1="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_merged/wt_S2_L001_merged_trimmed.fq"
 p1_1="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_merged/wt_S2_L001_unmerged_trimmed_1.fq"
@@ -22,12 +21,12 @@ unmapped_paired=$base_name"unmapped_paired.fa.gz"
 
 /home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-fast -p 60 -x $bt2_base -1 $p1_1,$p2_1 -2 $p1_2,$p2_2 -U $s1 $--un-gz $unmapped_unpaired --un-conc-gz $unmapped_paired -S $alignment 2> $report
 
-samfile=$alignment
-bamfile=$base_name"_unsorted.bam"
-sorted=$base_name"_sorted"
-sorted_file=$sorted".bam"
+# samfile=$alignment
+# bamfile=$base_name"_unsorted.bam"
+# sorted=$base_name"_sorted"
+# sorted_file=$sorted".bam"
 
-samtools view -bS $samfile > $bamfile -@ 60
-samtools sort $bamfile $sorted -@ 60
-samtools index $sorted_file
+# samtools view -bS $samfile > $bamfile -@ 60
+# samtools sort $bamfile $sorted -@ 60
+# samtools index $sorted_file
 
