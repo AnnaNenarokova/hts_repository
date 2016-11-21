b9976010b7efc80c2dc185b6e8e4e8d6f327dee4
anna <a.nenarokova@gmail.com>
2016-11-21 16:43:07 +0100

    ni

--- bash_scripts/bowtie2_build.sh
+++ bash_scripts/bowtie2_build.sh
@@ -2,5 +2,5 @@
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=60
 bw2_dir='/home/nenarokova/tools/bowtie2-2.2.9/'
-cd /home/nenarokova/genomes/novymonas/assembly/wt_all_spades/
-$bw2_dir'bowtie2-build' --threads 60 contigs.fasta wt_novymonas
+cd /home/nenarokova/genomes/novymonas/assembly/azi_spades/
+$bw2_dir'bowtie2-build' --threads 60 azi_scaffolds_cov_more_10.fa azi_scaffolds_cov_more_10
--- py_scripts/bioscripts/filter_by_cov.py
+++ py_scripts/bioscripts/filter_by_cov.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 from Bio import SeqIO
 
-min_cov=3
+min_cov=10
 inpath = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/novymonas/azi_scaffolds.fa'
 results = []
 
-for record in SeqIO.parse(fasta, "fasta"):
+for record in SeqIO.parse(inpath, "fasta"):
     if float(record.id.split("_")[-1]) > min_cov:
 		results.append(record)
 
-outpath = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/novymonas/azi_scaffolds_cov>3.fa'
+outpath = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/novymonas/azi_scaffolds_cov>10.fa'
 SeqIO.write(results, outpath, "fasta")
