17e5ae1a1da50ce2ddc46f9beb02290694c142d4
anna <a.nenarokova@gmail.com>
2016-11-21 19:47:42 +0100

    ni

--- bash_scripts/bowtie2_krtecek_wt.sh
+++ bash_scripts/bowtie2_krtecek_wt.sh
@@ -3,9 +3,10 @@
 #PBS -l nodes=1:ppn=60
 
 bw2_dir='/home/nenarokova/tools/bowtie2-2.2.9/'
-base_name='/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10'
-$bw2_dir'bowtie2-build' --threads 60 $base_name'.fa' $base_name
+cd /home/nenarokova/genomes/novymonas/assembly/wt_all_spades/
+$bw2_dir'bowtie2-build' --threads 60 contigs.fasta wt_novymonas
 
+bt2_base='/home/nenarokova/genomes/novymonas/assembly/wt_all_spades/wt_novymonas'
 
 s1="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_merged/wt_S2_L001_merged_trimmed.fq"
 p1_1="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_merged/wt_S2_L001_unmerged_trimmed_1.fq"
@@ -13,14 +14,13 @@ p1_2="/home/nenarokova/genomes/novymonas/raw_illumina/miseq_merged/wt_S2_L001_un
 p2_1="/home/nenarokova/genomes/novymonas/raw_illumina/old_hiseq_trimmed/E262_1_trimmed.fastq"
 p2_1="/home/nenarokova/genomes/novymonas/raw_illumina/old_hiseq_trimmed/E262_2_trimmed.fastq"
 
+unmapped_reads="/home/nenarokova/genomes/novymonas/assembly/mapping/wt_unmapped_reads.gz"
+
+base_name="/home/nenarokova/genomes/novymonas/assembly/mapping/wt_all"
 alignment=$base_name".sam"
 report=$base_name".txt"
 
-
-unmapped_unpaired=$base_name"unmapped_unpaired.fa.gz"
-unmapped_paired=$base_name"unmapped_paired.fa.gz"
-
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-fast -p 60 -x $bt2_base -1 $p1_1,$p2_1 -2 $p1_2,$p2_2 -U $s1 $--un-gz $unmapped_unpaired --un-conc-gz $unmapped_paired -S $alignment 2> $report
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-fast -p 60 -x $bt2_base -1 $fw_paired -2 $rv_paired --un-gz $clean_reads_unpaired --un-conc-gz $clean_reads_paired -S $alignment 2> $report
 
 samfile=$alignment
 bamfile=$base_name"_unsorted.bam"
