cb71a609718684f3c494bfa14cfb3176d0c292cc
Anna Nenarokova <a.nenarokova@gmail.com>
2016-11-23 14:49:16 +0100

    ni

--- bash_scripts/run_spades_pand.sh
+++ bash_scripts/run_spades_pand.sh
@@ -2,9 +2,9 @@
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=60
 
-se1="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_unpaired.fa.gz"
-pe1_1="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_paired.fa.1.gz"
-pe1_2="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_paired.fa.2.gz"
+se1="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_unpaired.fq"
+pe1_1="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_paired.fq.1.gz"
+pe1_2="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_paired.fq.2.gz"
 
 outdir="/home/nenarokova/genomes/novymonas/assembly/pandoraea/"
 
