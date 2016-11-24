0e8e887b0304307d9292218796bce228de6bcc8c
Anna Nenarokova <a.nenarokova@gmail.com>
2016-11-24 19:40:10 +0100

    ni

--- bash_scripts/bowtie2_krtecek_rna.sh
+++ bash_scripts/bowtie2_krtecek_rna.sh
@@ -13,12 +13,12 @@ report=$base_name".txt"
 
 /home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 60 -x $base_name -1 $p1_1 -2 $p1_2 --un-gz $unmapped_unpaired -S $alignment 2> $report
 
-samfile=$alignment
-bamfile=$base_name"_unsorted.bam"
-sorted=$base_name"_sorted"
-sorted_file=$sorted".bam"
+# samfile=$alignment
+# bamfile=$base_name"_unsorted.bam"
+# sorted=$base_name"_sorted"
+# sorted_file=$sorted".bam"
 
-samtools view -bS $samfile > $bamfile -@ 60
-samtools sort $bamfile $sorted -@ 60
-samtools index $sorted_file
+# samtools view -bS $samfile > $bamfile -@ 60
+# samtools sort $bamfile $sorted -@ 60
+# samtools index $sorted_file
 
