1a54e1666920a60001fe3bf63f1c271799793c11
Anna Nenarokova <a.nenarokova@gmail.com>
2016-11-25 14:33:56 +0100

    ni

--- bash_scripts/sam_to_bam.sh
+++ bash_scripts/sam_to_bam.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source /home/smrtanalysis/current/etc/setup.sh
 
-samfile="/media/4TB1/novymonas/mapping/pacbio_reads.sam"
-bamfile="/media/4TB1/novymonas/mapping/pacbio_reads_unsorted.bam"
-sorted="/media/4TB1/novymonas/mapping/pacbio_reads_sorted"
+samfile="/media/4TB3/trypanoplasma/mapping/pacbio_reads_quiver_consensus3.sam"
+bamfile="/media/4TB3/trypanoplasma/mapping/pacbio_reads_quiver_consensus3_unsorted.bam"
+sorted="/media/4TB3/trypanoplasma/mapping/pacbio_reads_quiver_consensus3_sorted"
 sorted_file=$sorted".bam"
-samtools view -bS $samfile > $bamfile -@ 20
-samtools sort $bamfile $sorted -@ 20
+samtools view -bS $samfile > $bamfile -@ 32
+samtools sort $bamfile $sorted -@ 32
 samtools index $sorted_file
 
 chmod 644 $sorted_file
