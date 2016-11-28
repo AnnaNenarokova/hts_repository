ae575dc03ecea4e5e8f7cb01a3f53fa03d1a223f
Anna Nenarokova <a.nenarokova@gmail.com>
2016-11-28 12:34:58 +0100

    ni

--- bash_scripts/blasr_in_smrt.sh
+++ bash_scripts/blasr_in_smrt.sh
@@ -3,9 +3,19 @@
 
 source /home/smrtanalysis/current/etc/setup.sh
 
-input="/media/4TB1/novymonas/mapping/input.fofn"
-genome="/media/4TB1/novymonas/e262_hiseq_contigs.fa"
+input="/media/4TB3/trypanoplasma/mapping/input.fofn"
+genome="/media/4TB3/trypanoplasma/mapping/pacbio_consensus_quiver3.fastq"
 
-out="/media/4TB1/novymonas/mapping/pacbio_reads.sam"
-unaligned="media/4TB1/novymonas/mapping/unaligned_pacbio_reads_last.fq"
-blasr $input $genome -sam -out $out -nproc 32 #-clipping soft #-unaligned $unaligned
+out="/media/4TB3/trypanoplasma/mapping/pacbio_consensus_quiver3.sam"
+unaligned="media/4TB1/novymonas/mapping/pacbio_consensus_quiver3_unaligned.fq"
+
+blasr $input $genome -sam -out $out -nproc 32 -clipping soft -unaligned $unaligned
+
+samfile="/media/4TB3/trypanoplasma/mapping/pacbio_consensus_quiver3.sam"
+bamfile="/media/4TB3/trypanoplasma/mapping/pacbio_reads_consensus3_unsorted.bam"
+sorted="/media/4TB3/trypanoplasma/mapping/pacbio_reads_consensus3_sorted"
+sorted_file=$sorted".bam"
+mapped_file=$sorted"_mapped.bam"
+samtools view -bS -@ 32 $samfile > $bamfile
+samtools sort $bamfile $sorted -@ 32
+samtools index $sorted_file
