2542e97fb10342a9c521c2d76695456aed0c0e91
Anna Nenarokova <a.nenarokova@gmail.com>
2016-11-28 11:57:00 +0100

    ni

--- bash_scripts/bowtie2_krtecek_rna.sh
+++ bash_scripts/bowtie2_krtecek_rna.sh
@@ -3,8 +3,8 @@
 # PBS -l walltime=10000:00:00
 bw2_dir='/home/nenarokova/tools/bowtie2-2.2.9/'
 base_name='/home/nenarokova/genomes/novymonas/rnaseq/mapping/pandorea'
-ref="/home/nenarokova/genomes/novymonas/assembly/pandoraea/pandorea_final.fa"
-# $bw2_dir'bowtie2-build' --threads 60 $ref $base_name
+ref="/home/nenarokova/genomes/novymonas/assembly/pandoraea/pandoraea_final.fasta"
+$bw2_dir'bowtie2-build' --threads 60 $ref $base_name
 
 p1_1="/home/nenarokova/genomes/novymonas/rnaseq/trimmed/rna_wt1_trimmed_1.fq"
 p1_2="/home/nenarokova/genomes/novymonas/rnaseq/trimmed/rna_wt1_trimmed_2.fq"
@@ -12,7 +12,7 @@ p1_2="/home/nenarokova/genomes/novymonas/rnaseq/trimmed/rna_wt1_trimmed_2.fq"
 alignment=$base_name".sam"
 report=$base_name".txt"
 
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 60 -x $base_name -1 $p1_1 -2 $p1_2 -S $alignment 2> $report
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive --no-unal -p 60 -x $base_name -1 $p1_1 -2 $p1_2 -S $alignment 2> $report
 
 samfile=$alignment
 bamfile=$base_name"_unsorted.bam"
--- py_scripts/bioscripts/get_records.py
+++ py_scripts/bioscripts/get_records.py
@@ -2,22 +2,21 @@
 from Bio import SeqIO
 
 l = [
-"NODE_1_length_844906_cov_871.097",
-"NODE_62_length_108845_cov_889.51",
-"NODE_65_length_108046_cov_859.19",
-"NODE_95_length_88224_cov_845.745",
-"NODE_700_length_5920_cov_1556.22",
-"NODE_1304_length_1318_cov_1864.36",
-"NODE_1110_length_1849_cov_675.416"
+"NODE_2_length_844906_cov_870.704",
+"NODE_822_length_1318_cov_1863.76",
+"NODE_346_length_5920_cov_1555.3",
+"NODE_121_length_88224_cov_845.318",
+"NODE_106_length_108046_cov_858.69",
+"NODE_104_length_108845_cov_889.343"
 ]
-fasta = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/novymonas/pandoraea_scaffolds.fasta'
+
+fasta = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/novymonas/wt_scaffolds.fa'
 results = []
 
 for record in SeqIO.parse(fasta, "fasta"):
-    for id in l:
-        if id == record.id:
-            results.append(record)
+    if record.id in l:
+        results.append(record)
 
-outpath = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/novymonas/pandorea_final.fa'
+outpath = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/novymonas/pandoraea_final.fasta'
 
 SeqIO.write(results, outpath, "fasta")
