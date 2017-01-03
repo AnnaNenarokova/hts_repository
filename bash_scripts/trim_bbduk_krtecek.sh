f588c8f94ffb5fc98d63d53f110129dbde5ee5d3
Anna Nenarokova <a.nenarokova@gmail.com>
2017-01-03 23:56:39 +0100

    ni

--- bash_scripts/trim_bbduk_krtecek.sh
+++ bash_scripts/trim_bbduk_krtecek.sh
@@ -10,7 +10,7 @@ trimmed_fw=$trimdir$name'_trimmed_1.fq'
 trimmed_rv=$trimdir$name'_trimmed_2.fq'
 report=$trimdir$name"_report.txt"
 adapters='/home/nenarokova/tools/bbmap/resources/adapters.fa'
-/home/nenarokova/tools/bbmap/bbduk.sh in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=64 qtrim=rl trimq=20 2> $report
+/home/nenarokova/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20 2> $report
 
 fw="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_triat_1.fastq.gz"
 rv="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_triat_2.fastq.gz"
@@ -20,4 +20,4 @@ trimmed_fw=$trimdir$name'_trimmed_1.fq'
 trimmed_rv=$trimdir$name'_trimmed_2.fq'
 report=$trimdir$name"_report.txt"
 adapters='/home/nenarokova/tools/bbmap/resources/adapters.fa'
-/home/nenarokova/tools/bbmap/bbduk.sh in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=64 qtrim=rl trimq=20 2> $report
+/home/nenarokova/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20 2> $report
