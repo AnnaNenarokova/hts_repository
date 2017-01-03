674b15a977f981b5d4d809b6d76b657924f7292a
Anna Nenarokova <a.nenarokova@gmail.com>
2017-01-03 23:28:36 +0100

    ni

--- bash_scripts/trim_bbduk_krtecek.sh
+++ bash_scripts/trim_bbduk_krtecek.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#PBS -l walltime=100:00:00
+#PBS -l nodes=1:ppn=60
+
 fw="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_spP57_1.fastq.gz"
 rv="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_spP57_2.fastq.gz"
 trimdir='/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/'
@@ -7,7 +10,7 @@ trimmed_fw=$trimdir$name'_trimmed_1.fq'
 trimmed_rv=$trimdir$name'_trimmed_2.fq'
 report=$trimdir$name"_report.txt"
 adapters='/home/nenarokova/tools/bbmap/resources/adapters.fa'
-/home/nenarokova/tools/bbmap/bbduk.sh in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=20 qtrim=rl trimq=20 2> $report
+/home/nenarokova/tools/bbmap/bbduk.sh in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=64 qtrim=rl trimq=20 2> $report
 
 fw="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_triat_1.fastq.gz"
 rv="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_triat_2.fastq.gz"
@@ -17,4 +20,4 @@ trimmed_fw=$trimdir$name'_trimmed_1.fq'
 trimmed_rv=$trimdir$name'_trimmed_2.fq'
 report=$trimdir$name"_report.txt"
 adapters='/home/nenarokova/tools/bbmap/resources/adapters.fa'
-/home/nenarokova/tools/bbmap/bbduk.sh in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=20 qtrim=rl trimq=20 2> $report
+/home/nenarokova/tools/bbmap/bbduk.sh in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=64 qtrim=rl trimq=20 2> $report
