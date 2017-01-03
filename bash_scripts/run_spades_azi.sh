33a04afb700c0d7a350a8f629aecfbe18a8257d4
Anna Nenarokova <a.nenarokova@gmail.com>
2017-01-04 00:55:29 +0100

    ni

--- bash_scripts/run_spades_azi.sh
+++ bash_scripts/run_spades_azi.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
-#PBS -l nodes=1:ppn=60
+#PBS -l nodes=1:ppn=64
 
-se1="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_unpaired.fq"
-pe1_1="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_paired.fq.1.gz"
-pe1_2="/home/nenarokova/genomes/novymonas/assembly/azi_spades/azi_scaffolds_cov_more_10_unmapped_paired.fq.2.gz"
+pe1_1="/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/p57_trimmed_1.fq"
+pe1_2="/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/p57_trimmed_2.fq"
 
-outdir="/home/nenarokova/genomes/novymonas/assembly/azi_spades/"
+outdir="/home/nenarokova/genomes/blasto/blastocrithidia/genome/assembly/p57/"
 
 report=$outdir"spades_report.txt"
 
-/home/nenarokova/tools/SPAdes-3.9.0-Linux/bin/spades.py --s1 $se1 --pe1-1 $pe1_1 --pe1-2 $pe1_2 --careful -t 60 -o $outdir 2> $report
+/home/nenarokova/tools/SPAdes-3.9.0-Linux/bin/spades.py --pe1-1 $pe1_1 --pe1-2 $pe1_2 --careful -t 64 -o $outdir
--- bash_scripts/trim_bbduk_krtecek.sh
+++ bash_scripts/trim_bbduk_krtecek.sh
@@ -6,8 +6,8 @@ fw="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_spP57_1.f
 rv="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_spP57_2.fastq.gz"
 trimdir='/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/'
 name='p57'
-trimmed_fw=$trimdir$name'_trimmed_1.fq'
-trimmed_rv=$trimdir$name'_trimmed_2.fq'
+trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
+trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
 report=$trimdir$name"_report.txt"
 adapters='/home/nenarokova/tools/bbmap/resources/adapters.fa'
 /home/nenarokova/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
@@ -16,8 +16,8 @@ fw="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_triat_1.f
 rv="/home/nenarokova/genomes/blasto/blastocrithidia/genome/raw/Blastoc_triat_2.fastq.gz"
 trimdir='/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/'
 name='triat'
-trimmed_fw=$trimdir$name'_trimmed_1.fq'
-trimmed_rv=$trimdir$name'_trimmed_2.fq'
+trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
+trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
 report=$trimdir$name"_report.txt"
 adapters='/home/nenarokova/tools/bbmap/resources/adapters.fa'
 /home/nenarokova/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
