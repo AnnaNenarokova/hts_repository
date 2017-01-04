9d434c5aa1738d1c97182ad6354422aa26c73ae8
Anna Nenarokova <a.nenarokova@gmail.com>
2017-01-04 12:02:50 +0100

    ni

--- py_scripts/bioscripts/get_record.py
+++ py_scripts/bioscripts/get_record.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 from Bio import SeqIO
 
-record_id = 'utg000035l|quiver|quiver|quiver'
+record_id = 'NODE_6258_length_1665_cov_2.55155'
 
-fasta_file = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanoplasma/pacbio_consensus_quiver3.fasta'
+fasta_file = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/blasto_kika/scaffolds_triat.fasta'
 
 for record in SeqIO.parse(fasta_file, "fasta"):
     if record_id == record.id:
         result = record
         # result = record[start:end].reverse_complement()
 
-outpath = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanoplasma/kinetoplast.fasta'
+outpath = '/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/blasto_kika/NODE_6258_length_1665_cov_2_55155.fasta'
 
 SeqIO.write(result, outpath, "fasta")
 
--- py_scripts/blast/use_blast2.py
+++ py_scripts/blast/use_blast2.py
@@ -33,10 +33,10 @@ def add_header(blast_csv_path, custom_outfmt):
     write_list_of_lists(blast_hits, blast_csv_path, header=header)
     return blast_csv_path
 
-query_path = "/home/nenarokova/genomes/blasto/blastocrithidia/tomato/GCF_000188115.3_SL2.50_protein.faa.gz"
+query_path = "/home/nenarokova/genomes/diplonema/integration_position.fa"
 custom_outfmt = 'qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send'
 subj_pathes = [
-"/home/nenarokova/genomes/blasto/blastocrithidia/genome/assembly/triat/scaffolds.fasta"
+"/home/nenarokova/genomes/diplonema/diplonema_all_transcripts_trinity.fna"
 ]
 
 for subj_path in subj_pathes:
