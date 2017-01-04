4eeed88b1464fec12ca6a42eab57f03e9e8512c3
Anna Nenarokova <a.nenarokova@gmail.com>
2017-01-04 01:00:04 +0100

    ni

--- bash_scripts/run_spades_triat.sh
+++ bash_scripts/run_spades_triat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
-#PBS -l nodes=1:ppn=64
+#PBS -l nodes=1:ppn=32
 
 pe1_1="/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/triat_trimmed_1.fq"
 pe1_2="/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/triat_trimmed_2.fq"
@@ -9,4 +9,4 @@ outdir="/home/nenarokova/genomes/blasto/blastocrithidia/genome/assembly/triat/"
 
 report=$outdir"spades_report.txt"
 
-/home/nenarokova/tools/SPAdes-3.9.0-Linux/bin/spades.py --pe1-1 $pe1_1 --pe1-2 $pe1_2 --careful -t 64 -o $outdir
+/home/nenarokova/tools/SPAdes-3.9.0-Linux/bin/spades.py --pe1-1 $pe1_1 --pe1-2 $pe1_2 --careful -t 32 -o $outdir
