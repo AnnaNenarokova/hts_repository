f03b391194c0d8f88b16306b2a51b837694c986a
Anna Nenarokova <a.nenarokova@gmail.com>
2017-01-06 14:16:37 +0100

    ni

--- rb_scripts/count_hit_coverage.rb
+++ rb_scripts/count_hit_coverage.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/ruby
 require 'csv'
 
-blast_csv_path='/home/anna/bioinformatics/genomes/wt_novymonas_spades/scaffolds_bl_report.csv'
+blast_csv_path='/home/anna/Dropbox/PhD/bioinformatics/genomes/trypanosomatids/blasto_kika/scaffolds_tr_bl_report_plants.csv'
 
 def merge_ranges(ranges)
   ranges = ranges.sort_by {|r| r.first }
-  outages = ranges.shift
+  *outages = ranges.shift
   ranges.each do |r|
     lastr = outages[-1]
     if lastr.last >= r.first - 1
@@ -22,22 +22,22 @@ all_seqs = {}
 
 blast_csv = blast_csv[1..-1]
 blast_csv.each do |e|
-	all_seqs[e.first] ||= []
-	all_seqs[e.first] << { start: e[10].to_i, end: e[11].to_i }
+    all_seqs[e.first] ||= []
+    all_seqs[e.first] << { start: e[10].to_i, end: e[11].to_i }
 end
 
 result = {}
 
 all_seqs.each do |id, intervals|
-	intervals = intervals.map { |i| i[:start]..i[:end] }
-	intervals = merge_ranges(intervals)
+    intervals = intervals.map { |i| i[:start]..i[:end] }
+    intervals = merge_ranges(intervals)
 
-	result[id] ||= {}
-	result[id][:ranges] = merge_ranges(intervals)
-	result[id][:size] = intervals.map(&:size).inject(0){ |sum,x| sum + x }
+    result[id] ||= {}
+    result[id][:ranges] = merge_ranges(intervals)
+    result[id][:size] = intervals.map(&:size).inject(0){ |sum,x| sum + x }
 end
 
 result = result.sort
 result.each do |key, v|
-	puts "#{key},#{v[:size]}"
+    puts "#{key},#{v[:size]}"
 end
