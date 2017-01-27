a748dab2a690daf77ee5c9a13fc65b6aefad2674
Kika Zahonova <kika.zahonova@gmail.com>
2017-01-27 23:08:26 +0100

    modified augustus script

--- bash_scripts/augustus.sh
+++ bash_scripts/augustus.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd /home/kika/blastocrithidia/augustus
-augustus --species=blastocrithidia Blastocrithidia_for_Augustus.gb.test | tee firsttest.out
+#augustus --species=blastocrithidia Blastocrithidia_for_Augustus.gb.test | tee firsttest.out
+optimize_augustus.pl --species=blastocrithidia Blastocrithidia_for_Augustus.gb.train
