7486b468fb42a2c03b4846a5399d3b1e83ec2959
Anna Nenarokova <a.nenarokova@gmail.com>
2017-02-02 15:08:14 +0100

    ni

--- py_scripts/blast/use_blast.py
+++ py_scripts/blast/use_blast.py
@@ -33,10 +33,10 @@ def add_header(blast_csv_path, custom_outfmt):
     write_list_of_lists(blast_hits, blast_csv_path, header=header)
     return blast_csv_path
 
-query_path = "/home/nenarokova/genomes/euglena/blast_proteome/euglena_all_proteins.fasta"
+query_path = "sftp://nenarokova@147.231.253.13/home/kika/blastocrithidia/datasets/ref_for_novymonas.fa"
 custom_outfmt = 'qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send'
 subj_pathes = [
-"/home/nenarokova/genomes/euglena/EL_ribosomal_proteins_plastid.fa"
+"sftp://nenarokova@147.231.253.13/media/4TB1/blasto/Trinity-GG_p57_6_frames_translated.faa"
 ]
 
 for subj_path in subj_pathes:
