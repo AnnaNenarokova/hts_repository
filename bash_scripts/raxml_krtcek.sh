bff273fd039d69551bbf811c575ea7f1592a9435
Anna Nenarokova <a.nenarokova@gmail.com>
2017-02-03 15:26:06 +0100

    ni

--- bash_scripts/raxml_krtcek.sh
+++ bash_scripts/raxml_krtcek.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=64
+#PBS -o /home/nenarokova/pbs.out
+#PBS -e /home/nenarokova/pbs.err
 
 align_dir = '/home/nenarokova/genomes/novymonas/pandoraea_phylogeny/all_OGs_1prot_short_no_Limno_alignment/'
 raxml='/home/nenarokova/tools/raxml/raxml_sse/standard-RAxML/raxmlHPC-SSE3'
