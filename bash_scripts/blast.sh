f606df5427fdb44faf55aeaff5c8aa12772f5954
Anna Nenarokova <a.nenarokova@gmail.com>
2017-03-20 10:05:18 +0100

    ni

--- bash_scripts/blast.sh
+++ bash_scripts/blast.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
 #PBS -l nodes=krtecek2.local:ppn=30
-
+#PBS -o /home/nenarokova/blast_pbs.out
+#PBS -e /home/nenarokova/blast_pbs.err
 /home/nenarokova/ngs/py_scripts/blast/use_blast.py
