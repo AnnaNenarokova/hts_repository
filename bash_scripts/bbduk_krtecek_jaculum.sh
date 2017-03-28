6810debd0cdbd8161c9fb26dfd6105e8c53b9148
Anna Nenarokova <a.nenarokova@gmail.com>
2017-03-28 15:09:07 +0200

    ni

--- bash_scripts/bbduk_krtecek_jaculum.sh
+++ bash_scripts/bbduk_krtecek_jaculum.sh
@@ -7,4 +7,4 @@ out1="/home/nenarokova/genomes/jaculum/reads/1_1_trimmed.fastq.gz"
 out2="/home/nenarokova/genomes/jaculum/reads/1_2_trimmed.fastq.gz"
 err_out="/home/nenarokova/genomes/jaculum/reads/err.txt"
 adapters="/home/nenarokova/tools/bbmap/resources/adapters.fa"
-/home/nenarokova/tools/bbmap/bbduk.sh overwrite=true in1=$in1 in2=$in2 out1=$out1 out2=$out2 ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=64 qtrim=rl trimq=20
+/home/nenarokova/tools/bbmap/bbduk.sh overwrite=true in1=$in1 in2=$in2 out1=$out1 out2=$out2 ref=$adapters usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=64 qtrim=rl trimq=20 > $err_out
