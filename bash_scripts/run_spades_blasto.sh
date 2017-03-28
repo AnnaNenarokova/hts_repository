446fdb480f571764d2f46f519125c91883610a71
Anna Nenarokova <a.nenarokova@gmail.com>
2017-03-28 15:53:21 +0200

    ni

--- bash_scripts/run_spades_blasto.sh
+++ bash_scripts/run_spades_blasto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
-#PBS -l nodes=1:ppn=64
+#PBS -l nodes=krtecek2.local:ppn=64
 
-pe1_1="/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/p57_trimmed_1.fq"
-pe1_2="/home/nenarokova/genomes/blasto/blastocrithidia/genome/trimmed/p57_trimmed_2.fq"
+pe1_1="/home/nenarokova/genomes/jaculum/reads/1_1_trimmed.fastq.gz"
+pe1_2="/home/nenarokova/genomes/jaculum/reads/1_2_trimmed.fastq.gz"
 
-outdir="/home/nenarokova/genomes/blasto/blastocrithidia/genome/assembly/p57/"
+outdir="/home/nenarokova/genomes/jaculum/assembly/"
 
 report=$outdir"spades_report.txt"
 
