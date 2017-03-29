dd4198693577d7e9623cc0ec87b96d136a7d7a63
Anna Nenarokova <a.nenarokova@gmail.com>
2017-03-29 11:30:47 +0200

    ni

--- py_scripts/blast/use_blast.py
+++ py_scripts/blast/use_blast.py
@@ -44,7 +44,7 @@ subj_paths = [
 
 for query_path in query_paths:
     for subj_path in subj_paths:
-        new_blast = Blast(query_path=query_path, subj_path=subj_path, db_type='prot', threads=30)
+        new_blast = Blast(query_path=query_path, subj_path=subj_path, db_type='nucl', threads=30)
         blast_csv_path = new_blast.blast(
                                          bl_type='blastn',
                                          evalue=0.00001,
