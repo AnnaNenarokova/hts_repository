f17375a54f150fc4c1fd21e8f22674ed1310bca6
Anna Nenarokova <a.nenarokova@gmail.com>
2017-04-10 17:59:10 +0200

    ni

--- bash_scripts/star.sh
+++ bash_scripts/star.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 star="/home/nenarokova/tools/STAR-2.5.3a/bin/Linux_x86_64/STAR"
-genome="/media/4TB1/blasto/star_mapping/p57_scaffolds.fa"
+genome="/media/4TB1/blasto/p57_scaffolds.fa"
 genome_dir="/media/4TB1/blasto/star_mapping"
-r1=""
-r2=""
-$star  [options]... --genomeDir $genome_dir --readFilesIn R1.fq R2.fq
+r1="/media/4TB1/blasto/blastocrithidia/transcriptome/trimmed/p57_trimmed_1.fq.gz"
+r2="/media/4TB1/blasto/blastocrithidia/transcriptome/trimmed/p57_trimmed_2.fq.gz"
+
+$star --runThreadN 30 --genomeSAindexNbases 12 --runMode genomeGenerate --genomeFastaFiles $genome --readFilesCommand zcat--genomeDir $genome_dir --readFilesIn $r1 $r2
