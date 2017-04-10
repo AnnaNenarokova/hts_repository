da5bd6ac374b1eb2e1d905403afaaaed51e9bf22
Anna Nenarokova <a.nenarokova@gmail.com>
2017-04-10 18:17:39 +0200

    ni

--- bash_scripts/sam_to_bam_krtecek.sh
+++ bash_scripts/sam_to_bam_krtecek.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# PBS -l nodes=1:ppn=60
-# PBS -l walltime=10000:00:00
+
 cd /home/nenarokova/tools/samtools/bin/
 
 samfile="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq.sam"
 bamfile="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq.bam"
 sorted="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq_sorted"
 sorted_file=$sorted".bam"
-./samtools view -bS -@ 60 $samfile > $bamfile
-./samtools sort $bamfile $sorted -@ 60
+./samtools view -bS -@ 20 $samfile > $bamfile
+./samtools sort $bamfile $sorted -@ 20
 ./samtools index $sorted_file
 
 samfile="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq.sam"
 bamfile="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq.bam"
 sorted="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq_sorted"
 sorted_file=$sorted".bam"
-./samtools view -bS -@ 60 $samfile > $bamfile
-./samtools sort $bamfile $sorted -@ 60
+./samtools view -bS -@ 20 $samfile > $bamfile
+./samtools sort $bamfile $sorted -@ 20
 ./samtools index $sorted_file
