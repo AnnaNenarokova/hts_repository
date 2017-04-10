5e1c3d408f5db1f83f4502b692874eaeb0075a62
Anna Nenarokova <a.nenarokova@gmail.com>
2017-04-10 17:31:46 +0200

    ni

--- bash_scripts/sam_to_bam_krtecek.sh
+++ bash_scripts/sam_to_bam_krtecek.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # PBS -l nodes=1:ppn=60
 # PBS -l walltime=10000:00:00
-cd /home/nenarokova/tools/samtools/bin/samtools
+cd /home/nenarokova/tools/samtools/bin/
 
 samfile="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq.sam"
 bamfile="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq.bam"
