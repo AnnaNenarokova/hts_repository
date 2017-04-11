edbd4a2115174c9d6534921674dd3f0ce3869ed2
Anna Nenarokova <a.nenarokova@gmail.com>
2017-04-11 10:12:38 +0200

    ni

--- bash_scripts/sort_bam.sh
+++ bash_scripts/sort_bam.sh
@@ -2,15 +2,12 @@
 
 cd /home/nenarokova/tools/samtools/bin/
 
-samfile="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq.sam"
 bam="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq.bam"
 sorted="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq_sorted.bam"
 
-./samtools view -bS -@ 20 $samfile > $bamfile
 ./samtools sort -o $sorted -@ 20 $bam
 ./samtools index $sorted
 
-samfile="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq.sam"
 bam="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq.bam"
 sorted="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq_sorted.bam"
 
