249c89b4defe0d224f789ae7481f8ed686f56729
Anna Nenarokova <a.nenarokova@gmail.com>
2017-04-11 16:05:14 +0200

    ni

--- bash_scripts/fastqc_krtecek.sh
+++ bash_scripts/fastqc_krtecek.sh
@@ -2,15 +2,14 @@
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=1
 
+# f="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq_mapped.1.fq.gz"
+# /home/nenarokova/tools/FastQC/fastqc $f &
 
-f="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq_mapped.1.fq.gz"
+# f="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq_mapped.2.fq.gz"
+# /home/nenarokova/tools/FastQC/fastqc $f &
+
+f="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq_mapped.fq.1.gz"
 /home/nenarokova/tools/FastQC/fastqc $f &
 
-f="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/hiseq_mapped.2.fq.gz"
-/home/nenarokova/tools/FastQC/fastqc $f &
-
-f="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq_mapped.1.fq.gz"
-/home/nenarokova/tools/FastQC/fastqc $f &
-
-f="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq_mapped.2.fq.gz"
+f="/home/nenarokova/genomes/novymonas/assembly/pnov_submission/miseq_mapped.fq.2.gz"
 /home/nenarokova/tools/FastQC/fastqc $f &
