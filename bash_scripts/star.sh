f14635628fc3ac4ce7cf39d368688bd85a1a3731
Anna Nenarokova <a.nenarokova@gmail.com>
2017-04-12 15:35:49 +0200

    ni

--- bash_scripts/star.sh
+++ bash_scripts/star.sh
@@ -5,6 +5,5 @@ genome_dir="/media/4TB1/blasto/star_mapping"
 r1="/media/4TB1/blasto/blastocrithidia/transcriptome/trimmed/p57_trimmed_1.fq.gz"
 r2="/media/4TB1/blasto/blastocrithidia/transcriptome/trimmed/p57_trimmed_2.fq.gz"
 
-$star --runThreadN 30 --genomeSAindexNbases 12 --runMode genomeGenerate --genomeFastaFiles $genome --readFilesCommand zcat --genomeDir $genome_dir --readFilesIn $r1 $r2
-
-$star --runThreadN 30 --genomeSAindexNbases 12 --runMode alignReads --genomeFastaFiles $genome --readFilesCommand zcat --genomeDir $genome_dir --readFilesIn $r1 $r2
+$star --runThreadN 30 --genomeSAindexNbases 12 --runMode genomeGenerate --genomeFastaFiles $genome --readFilesCommand zcat --genomeDir
+$star --runThreadN 30 --alignSJoverhangMin 30 --runMode alignReads --readFilesCommand zcat --genomeDir $genome_dir --outSAMtype BAM SortedByCoordinate --chimSegmentMin 30 --readFilesIn $r1 $r2
