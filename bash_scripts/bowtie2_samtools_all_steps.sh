d0c9bed85cca3bc039a504b15d4393ad59b9901b
Anna Nenarokova <a.nenarokova@gmail.com>
2017-06-09 12:48:07 +0200

    ni

--- bash_scripts/bowtie2_samtools_all_steps.sh
+++ bash_scripts/bowtie2_samtools_all_steps.sh
@@ -2,11 +2,11 @@
 bw2_dir="/home/nenarokova/tools/bowtie2-2.2.9/"
 fasta="/media/4TB1/novymonas/transcriptome/mapping/azi_scaffolds.fasta"
 bt2_base="/media/4TB1/novymonas/transcriptome/mapping/azi_scaffolds"
-$bw2_dir"bowtie2-build" --threads 30 $fasta $bt2_base
+# $bw2_dir"bowtie2-build" --threads 30 $fasta $bt2_base
 
 base_name="/media/4TB1/novymonas/transcriptome/mapping/azi_rna_mapped"
-r1="/media/4TB1/novymonas/transcriptome/trimmed_reads/azi_rna_trimmed_1.fq.gz"
-r2="/media/4TB1/novymonas/transcriptome/trimmed_reads/azi_rna_trimmed_2.fq.gz"
+r1="/media/4TB1/novymonas/transcriptome/reads/trimmed_reads/azi_rna_trimmed_1.fq.gz"
+r2="/media/4TB1/novymonas/transcriptome/reads/trimmed_reads/azi_rna_trimmed_2.fq.gz"
 
 alignment=$base_name".sam"
 report=$base_name".txt"
