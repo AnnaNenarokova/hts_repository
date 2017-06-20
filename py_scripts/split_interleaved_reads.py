f86818f4a7ee65ba04625de832c58fab8492c3ea
Kika Zahonova <kika.zahonova@gmail.com>
2017-06-20 11:32:53 +0200

    changed back to python3

--- py_scripts/split_interleaved_reads.py
+++ py_scripts/split_interleaved_reads.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 import os
 import re
 from Bio import SeqIO
@@ -9,17 +9,17 @@ files = os.listdir()
 for file in files:
 	if '.fq' in file and '.gz' not in file:
 		file_name = file.split('.')[0]
-		print(file_name)
+		print(file)
 		fw = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/' + file_name + '_fw.fq', 'w')
 		rev = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/' + file_name + '_rev.fq', 'w')
 		errors = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/' + file_name + '_errors.fq', 'w')
 		for read in SeqIO.parse(file, 'fastq'):
 			if re.search('SRR\d+.\d+.1 ', read.description):
-				print read.name
+				print(read.name)
 				fw.write(read.format('fastq'))
 			elif re.search('SRR\d+.\d+.2 ', read.description):
-				print read.name
+				print(read.name)
 				rev.write(read.format('fastq'))
 			else:
-				print read.name
+				print(read.name)
 				errors.write(read.format('fastq'))
