b1f40cea72b4e83bb47e17a1ea2d9d6afad964e5
Kika Zahonova <kika.zahonova@gmail.com>
2017-06-21 14:56:40 +0200

    bexlh out_dir changed to contain "trinity"

--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -3,6 +3,6 @@
 read_dir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
 fw=$read_dir'lygus_A1_trimmed_fw.fq',$read_dir'lygus_A2_trimmed_fw.fq',$read_dir'lygus_A3_trimmed_fw.fq',$read_dir'lygus_B1_trimmed_fw.fq',$read_dir'lygus_B2_trimmed_fw.fq',$read_dir'lygus_B3_trimmed_fw.fq',$read_dir'lygus_C1_trimmed_fw.fq',$read_dir'lygus_C2_trimmed_fw.fq',$read_dir'lygus_C3_trimmed_fw.fq',$read_dir'lygus_sraA_trimmed_fw.fq',$read_dir'lygus_sraB_trimmed_fw.fq',$read_dir'lygus_sraC_trimmed_fw.fq'
 rv=$read_dir'lygus_A1_trimmed_rev.fq',$read_dir'lygus_A2_trimmed_rev.fq',$read_dir'lygus_A3_trimmed_rev.fq',$read_dir'lygus_B1_trimmed_rev.fq',$read_dir'lygus_B2_trimmed_rev.fq',$read_dir'lygus_B3_trimmed_rev.fq',$read_dir'lygus_C1_trimmed_rev.fq',$read_dir'lygus_C2_trimmed_rev.fq',$read_dir'lygus_C3_trimmed_rev.fq',$read_dir'lygus_sraA_trimmed_rev.fq',$read_dir'lygus_sraB_trimmed_rev.fq',$read_dir'lygus_sraC_trimmed_rev.fq'
-out_dir='/media/4TB1/blastocrithidia/bexlh/assembly/'
+out_dir='/media/4TB1/blastocrithidia/bexlh/trinity_assembly/'
 
 Trinity --seqType fq --left $fw --right $rv --output $out_dir --max_memory 100G --CPU 30
