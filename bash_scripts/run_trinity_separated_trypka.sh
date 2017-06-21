3c9297d6bd423e1840f609115545bf022edd02ab
Kika Zahonova <kika.zahonova@gmail.com>
2017-06-21 14:53:29 +0200

    bexlh out_dir added

--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
-fw=$wdir'lygus_A1_trimmed_fw.fq',$wdir'lygus_A2_trimmed_fw.fq',$wdir'lygus_A3_trimmed_fw.fq',$wdir'lygus_B1_trimmed_fw.fq',$wdir'lygus_B2_trimmed_fw.fq',$wdir'lygus_B3_trimmed_fw.fq',$wdir'lygus_C1_trimmed_fw.fq',$wdir'lygus_C2_trimmed_fw.fq',$wdir'lygus_C3_trimmed_fw.fq',$wdir'lygus_sraA_trimmed_fw.fq',$wdir'lygus_sraB_trimmed_fw.fq',$wdir'lygus_sraC_trimmed_fw.fq'
-rv=$wdir'lygus_A1_trimmed_rev.fq',$wdir'lygus_A2_trimmed_rev.fq',$wdir'lygus_A3_trimmed_rev.fq',$wdir'lygus_B1_trimmed_rev.fq',$wdir'lygus_B2_trimmed_rev.fq',$wdir'lygus_B3_trimmed_rev.fq',$wdir'lygus_C1_trimmed_rev.fq',$wdir'lygus_C2_trimmed_rev.fq',$wdir'lygus_C3_trimmed_rev.fq',$wdir'lygus_sraA_trimmed_rev.fq',$wdir'lygus_sraB_trimmed_rev.fq',$wdir'lygus_sraC_trimmed_rev.fq'
+read_dir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
+fw=$read_dir'lygus_A1_trimmed_fw.fq',$read_dir'lygus_A2_trimmed_fw.fq',$read_dir'lygus_A3_trimmed_fw.fq',$read_dir'lygus_B1_trimmed_fw.fq',$read_dir'lygus_B2_trimmed_fw.fq',$read_dir'lygus_B3_trimmed_fw.fq',$read_dir'lygus_C1_trimmed_fw.fq',$read_dir'lygus_C2_trimmed_fw.fq',$read_dir'lygus_C3_trimmed_fw.fq',$read_dir'lygus_sraA_trimmed_fw.fq',$read_dir'lygus_sraB_trimmed_fw.fq',$read_dir'lygus_sraC_trimmed_fw.fq'
+rv=$read_dir'lygus_A1_trimmed_rev.fq',$read_dir'lygus_A2_trimmed_rev.fq',$read_dir'lygus_A3_trimmed_rev.fq',$read_dir'lygus_B1_trimmed_rev.fq',$read_dir'lygus_B2_trimmed_rev.fq',$read_dir'lygus_B3_trimmed_rev.fq',$read_dir'lygus_C1_trimmed_rev.fq',$read_dir'lygus_C2_trimmed_rev.fq',$read_dir'lygus_C3_trimmed_rev.fq',$read_dir'lygus_sraA_trimmed_rev.fq',$read_dir'lygus_sraB_trimmed_rev.fq',$read_dir'lygus_sraC_trimmed_rev.fq'
+out_dir='/media/4TB1/blastocrithidia/bexlh/assembly/'
 
-Trinity --seqType fq --left $fw --right $rv --max_memory 100G --CPU 30
+Trinity --seqType fq --left $fw --right $rv --output $out_dir --max_memory 100G --CPU 30
