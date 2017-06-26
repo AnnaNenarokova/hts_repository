625aee5df07768498f81c12b9d4fe3695ff1a5e7
Kika Zahonova <kika.zahonova@gmail.com>
2017-06-26 12:41:25 +0200

    bla

--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-read_dir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
-fw=$read_dir'lygus_sraA_trimmed_fw.fq',$read_dir'lygus_sraB_trimmed_fw.fq',$read_dir'lygus_sraC_trimmed_fw.fq'
-rv=$read_dir'lygus_sraA_trimmed_rev.fq',$read_dir'lygus_sraB_trimmed_rev.fq',$read_dir'lygus_sraC_trimmed_rev.fq'
-out_dir='/media/4TB1/blastocrithidia/bexlh/trinity_assembly_sraABC/'
+read_dir='/media/4TB1/blastocrithidia/bexlh/reads/'
+fw=$read_dir'testing_reads_fw.fq'
+rv=$read_dir'testing_reads_rv.fq'
+out_dir='/media/4TB1/blastocrithidia/bexlh/trinity_test_assembly/'
 
 Trinity --seqType fq --left $fw --right $rv --output $out_dir --max_memory 100G --CPU 30
