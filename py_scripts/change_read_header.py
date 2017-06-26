87bf9648a86e7eeceecfb48acc0a33e94b4d8187
Kika Zahonova <kika.zahonova@gmail.com>
2017-06-26 16:26:03 +0200

    changed folder

--- py_scripts/change_read_header.py
+++ py_scripts/change_read_header.py
@@ -3,14 +3,14 @@ import os
 import re
 from Bio import SeqIO
 
-os.chdir('/media/4TB1/blastocrithidia/bexlh/reads/trimmed')
+os.chdir('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/')
 files = os.listdir()
 
 for file in files:
 	if ('sra' not in file and '_fw.fq' in file) or ('sra' not in file and '_rev.fq' in file):
 		print(file)
 		file_name = file.split('.')[0]
-		out = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed' + file_name + '_upd.fq', 'w')
+		out = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/' + file_name + '_upd.fq', 'w')
 
 		for read in SeqIO.parse(file, 'fastq'):
 			read.description = re.sub(r'(?P<name>SRR\d+.\d+).(?P<num>\d) \d+ (?P<length>.*)', '\g<2> \g<3>', read.description)
