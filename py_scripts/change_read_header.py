c1267dceda887f0a36dda343080f3712f9ac7936
Kika Zahonova <kika.zahonova@gmail.com>
2017-06-27 09:26:44 +0200

    added print statement

--- py_scripts/change_read_header.py
+++ py_scripts/change_read_header.py
@@ -13,6 +13,6 @@ for file in files:
 		out = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/' + file_name + '_upd.fq', 'w')
 
 		for read in SeqIO.parse(file, 'fastq'):
-			print(read)
+			print(read.description)
 			read.description = re.sub(r'(?P<name>SRR\d+.\d+).(?P<num>\d) \d+ (?P<length>.*)', '\g<2> \g<3>', read.description)
 			out.write(read.format('fastq'))
