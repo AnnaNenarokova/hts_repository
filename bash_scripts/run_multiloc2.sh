02e207eeff0b1951b8a1d3a575b86ba89fad6e71
Kika Zahonova <kika.zahonova@gmail.com>
2017-06-28 09:42:07 +0200

    trinity for bexlh_sra

--- bash_scripts/run_multiloc2.sh
+++ bash_scripts/run_multiloc2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-f="/home/kika/MEGAsync/Euglena_longa/2013_Sekvenovanie/Fd+FNR/Sulfite_reductase/EL+EG_SiR.txt"
-r="/home/kika/MEGAsync/Euglena_longa/2013_Sekvenovanie/Fd+FNR/Sulfite_reductase/EL+EG_SiR_multiloc.txt"
+f="/home/kika/Dropbox/blasto_project/blastocrithidia/genes/thiolation/mtu11.fasta"
+r="/home/kika/Dropbox/blasto_project/blastocrithidia/genes/thiolation/mtu11_multiloc.txt"
 
 /usr/bin/python2.7 /home/kika/programs/MultiLoc2-26-10-2009/src/multiloc2_prediction.py -fasta=$f -predictor=LowRes -origin=plant -result=$r -output=simple
--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-read_dir='/media/4TB1/blastocrithidia/bexlh/reads/'
-fw=$read_dir'testing_reads_fw.fq'
-rv=$read_dir'testing_reads_rv.fq'
-out_dir='/media/4TB1/blastocrithidia/bexlh/trinity_test_assembly/'
+/media/4TB1/blastocrithidia/bexlh/reads/trimmed/
+read_dir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
+fw=$read_dir'lygus_sraA_trimmed_fw_upd.fq',$read_dir'lygus_sraB_trimmed_fw_upd.fq',$read_dir'lygus_sraC_trimmed_fw_upd.fq'
+rv=$read_dir'lygus_sraA_trimmed_rev_upd.fq',$read_dir'lygus_sraB_trimmed_rev_upd.fq',$read_dir'lygus_sraC_trimmed_rev_upd.fq'
+out_dir='/media/4TB1/blastocrithidia/bexlh/trinity_assembly_sraABC'
 
 Trinity --seqType fq --left $fw --right $rv --output $out_dir --max_memory 100G --CPU 30
