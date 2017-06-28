23943e6d6fba25db5125d36a3f7d0f98780613de
Kika Zahonova <kika.zahonova@gmail.com>
2017-06-28 21:20:49 +0200

    bla

--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/media/4TB1/blastocrithidia/bexlh/reads/trimmed/
+
 read_dir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
 fw=$read_dir'lygus_sraA_trimmed_fw_upd.fq',$read_dir'lygus_sraB_trimmed_fw_upd.fq',$read_dir'lygus_sraC_trimmed_fw_upd.fq'
 rv=$read_dir'lygus_sraA_trimmed_rev_upd.fq',$read_dir'lygus_sraB_trimmed_rev_upd.fq',$read_dir'lygus_sraC_trimmed_rev_upd.fq'
---
+++ py_scripts/change_read_headers.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import os
+
+os.chdir('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/')
+files = os.listdir()
+
+for file in files:
+	print(file)
+	file_name = file.split('.')[0]
+	out = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/' + file_name + '_upd.fq', 'w')
+	if ('sra' in file and '_fw.fq' in file) or ('sra' in file and '_rev.fq' in file):
+		infile = open(file).read()
+		lines = infile.split('\n')
+
+		for line in lines:
+			if line.startswith('@SRR'):
+				#@SRR2173361.101.1 HWI-ST885:166:H84KHADXX:1:1101:2021:2092 length=100
+				#@SRR2173361.101.2 HWI-ST885:166:H84KHADXX:1:1101:2021:2092 length=100
+				print(line)
+				description = (line.split(' ')[0].split('.')[0] + '.' + line.split(' ')[0].split('.')[1] + ' ' + 
+					line.split(' ')[0].split('.')[2] + ' ' + line.split(' ')[1])
+				out.write(description + '\n')
+			else:
+				out.write(line + '\n')
+
+	if ('sra' not in file and '_fw.fq' in file) or ('sra' not in file and '_rev.fq' in file):
+		infile = open(file).read()
+		lines = infile.split('\n')
+
+		for line in lines:
+			if line.startswith('@SRR'):
+				#@SRR1186295.25.1 25 length=100
+				#@SRR1186295.25.2 25 length=100
+				print(line)
+				description = (line.split(' ')[0].split('.')[0] + '.' + line.split(' ')[0].split('.')[1] + ' ' + 
+					line.split(' ')[0].split('.')[2])
+				out.write(description + '\n')
+			else:
+				out.write(line + '\n')
