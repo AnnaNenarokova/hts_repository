c0871cbdd637aae28cde993b338b86c88bfd5b3b
Anna Nenarokova <a.nenarokova@gmail.com>
2017-08-09 16:36:38 +0200

    ni

--- bash_scripts/bowtie2_samtools_all_steps.sh
+++ bash_scripts/bowtie2_samtools_all_steps.sh
@@ -4,23 +4,19 @@ fasta="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/TriTrypDB-
 bt2_base="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/mapping/lseymouri_genome"
 $bw2_dir"bowtie2-build" --threads 30 $fasta $bt2_base
 
-base_name="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/mapping/lseymouri_23_1_rna"
+file_path="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/mapping/lseymouri_23_1_rna"
 r1="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/reads/L_seymouri_23_1_trimmed_paired_forward.fastq.gz"
 r2="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/reads/L_seymouri_23_1_trimmed_paired_reverse.fastq.gz"
 
-alignment=$base_name".sam"
-report=$base_name".txt"
+alignment=$file_path".sam"
+report=$file_path".txt"
 
 /home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 30 -x $bt2_base -1 $r1 -2 $r2 -S $alignment 2> $report
 
 samfile=$alignment
-bamfile=$base_name"_unsorted.bam"
-sorted_file=$base_name"_sorted.bam"
+bamfile=$file_path"_unsorted.bam"
+sorted_file=$file_path"_sorted.bam"
 
 samtools view -bS -@ 30 $samfile > $bamfile
 samtools sort -o $sorted_file -@ 30 $bamfile
 samtools index -b $sorted_file
-
-base_name="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/mapping/lseymouri_23_2_rna"
-r1="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/reads/L_seymouri_23_2_trimmed_paired_forward.fastq.gz"
-r2="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/reads/L_seymouri_23_2_trimmed_paired_reverse.fastq.gz"
