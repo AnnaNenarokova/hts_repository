29d446f31315cdd052eccc930a0a60848da705de
Anna Nenarokova <a.nenarokova@gmail.com>
2017-08-09 16:38:15 +0200

    ni

--- bash_scripts/bowtie2_samtools_all_steps.sh
+++ bash_scripts/bowtie2_samtools_all_steps.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+threads=30
+
 bw2_dir="/home/nenarokova/tools/bowtie2-2.2.9/"
-fasta="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/TriTrypDB-33_LseymouriATCC30220_Genome.fasta"
+fasta="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/TriTrypDB-33_LseymouriATCC$threads220_Genome.fasta"
 bt2_base="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/mapping/lseymouri_genome"
-$bw2_dir"bowtie2-build" --threads 30 $fasta $bt2_base
+$bw2_dir"bowtie2-build" --threads $threads $fasta $bt2_base
 
 file_path="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/mapping/lseymouri_23_1_rna"
 r1="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/reads/L_seymouri_23_1_trimmed_paired_forward.fastq.gz"
@@ -11,12 +14,12 @@ r2="/media/4TB1/blastocrithidia/UTR_analyisis/references/lseymouri/reads/L_seymo
 alignment=$file_path".sam"
 report=$file_path".txt"
 
-/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p 30 -x $bt2_base -1 $r1 -2 $r2 -S $alignment 2> $report
+/home/nenarokova/tools/bowtie2-2.2.9/bowtie2 --very-sensitive -p $threads -x $bt2_base -1 $r1 -2 $r2 -S $alignment 2> $report
 
 samfile=$alignment
 bamfile=$file_path"_unsorted.bam"
 sorted_file=$file_path"_sorted.bam"
 
-samtools view -bS -@ 30 $samfile > $bamfile
-samtools sort -o $sorted_file -@ 30 $bamfile
+samtools view -bS -@ $threads $samfile > $bamfile
+samtools sort -o $sorted_file -@ $threads $bamfile
 samtools index -b $sorted_file
