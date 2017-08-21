408461b60358e42870588419148c028907861913
Kika Zahonova <kika.zahonova@gmail.com>
2017-08-21 15:52:12 +0200

    bla

---
+++ bash_scripts/get_sra_fastq.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+directory="/media/4TB1/blastocrithidia/bexlh/reads/raw/"
+
+fastq-dump --split-files --gzip -O $directory SRR2173361
+fastq-dump --split-files --gzip -O $directory SRR2170117
