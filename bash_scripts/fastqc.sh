38d2ccc3872a5e8f2a529bc955cbf519f2b4c73d
Kika Zahonova <kika.zahonova@gmail.com>
2017-08-22 15:39:27 +0200

    bla

---
+++ bash_scripts/fastqc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/ /media/4TB1/blastocrithidia/bexlh/reads/trimmed/SRR2170108_trimmed_1.fq.gz
+/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/ /media/4TB1/blastocrithidia/bexlh/reads/trimmed/SRR2170108_trimmed_2.fq.gz
+/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/ /media/4TB1/blastocrithidia/bexlh/reads/trimmed/SRR2170117_trimmed_1.fq.gz
+/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/ /media/4TB1/blastocrithidia/bexlh/reads/trimmed/SRR2170117_trimmed_2.fq.gz
+/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/ /media/4TB1/blastocrithidia/bexlh/reads/trimmed/SRR2173361_trimmed_1.fq.gz
+/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/ /media/4TB1/blastocrithidia/bexlh/reads/trimmed/SRR2173361_trimmed_2.fq.gz
--- bash_scripts/get_sra_fastq.sh
+++ bash_scripts/get_sra_fastq.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-directory="/media/4TB1/blastocrithidia/bexlh/reads/raw/"
+cold_dir="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/"
+fastq-dump --split-files --gzip -O $cold_dir SRR1186295
+fastq-dump --split-files --gzip -O $cold_dir SRR1186315
+fastq-dump --split-files --gzip -O $cold_dir SRR1186516
 
-fastq-dump --split-files --gzip -O $directory SRR2170108
-fastq-dump --split-files --gzip -O $directory SRR2170117
+non_dir="/media/4TB1/blastocrithidia/bexlh/reads/raw/non/"
+fastq-dump --split-files --gzip -O $non_dir SRR1186658
+fastq-dump --split-files --gzip -O $non_dir SRR1186749
+fastq-dump --split-files --gzip -O $non_dir SRR1186752
+
+heat_dir="/media/4TB1/blastocrithidia/bexlh/reads/raw/heat/"
+fastq-dump --split-files --gzip -O $heat_dir SRR1186787
+fastq-dump --split-files --gzip -O $heat_dir SRR1186788
+fastq-dump --split-files --gzip -O $heat_dir SRR1186789
