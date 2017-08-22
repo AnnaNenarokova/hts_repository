c09d212e5e283ab2b716fed46fc1cb3871f79910
Kika Zahonova <kika.zahonova@gmail.com>
2017-08-22 09:58:41 +0200

    bla

--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -3,6 +3,6 @@
 read_dir='/media/4TB1/blastocrithidia/jac_rna_reads/trimmed/'
 fw=$read_dir'jac_trimmed_1.fq.gz'
 rv=$read_dir'jac_trimmed_2.fq.gz'
-out_dir='/media/4TB1/blastocrithidia/transcriptome_assembly/trinity_denovo/jac_default/'
+out_dir='/media/4TB1/blastocrithidia/transcriptome_assembly/trinity_denovo/jac_trinity/'
 
 Trinity --seqType fq --left $fw --right $rv --output $out_dir --max_memory 100G --CPU 30
