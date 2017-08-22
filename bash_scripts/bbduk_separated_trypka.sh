2401b52a1dfaaafbe0a7ed7c8af0b26cdff92cf4
Kika Zahonova <kika.zahonova@gmail.com>
2017-08-22 15:06:59 +0200

    bla

--- bash_scripts/bbduk_separated_trypka.sh
+++ bash_scripts/bbduk_separated_trypka.sh
@@ -2,10 +2,30 @@
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=60
 
-fw="/media/4TB1/blastocrithidia/jac_rna_reads/jac_1.fastq.gz"
-rv="/media/4TB1/blastocrithidia/jac_rna_reads/jac_2.fastq.gz"
-trimdir='/media/4TB1/blastocrithidia/jac_rna_reads/trimmed/'
-name='jac'
+fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/SRR2170108_1.fastq.gz"
+rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/SRR2170108_2.fastq.gz"
+trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
+name='SRR2170108'
+trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
+trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
+report=$trimdir$name"_report.txt"
+adapt='/home/kika/tools/bbmap/resources/adapters.fa'
+/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
+
+fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/SRR2170117_1.fastq.gz"
+rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/SRR2170117_2.fastq.gz"
+trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
+name='SRR2170117'
+trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
+trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
+report=$trimdir$name"_report.txt"
+adapt='/home/kika/tools/bbmap/resources/adapters.fa'
+/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
+
+fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/SRR2173361_1.fastq.gz"
+rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/SRR2173361_2.fastq.gz"
+trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
+name='SRR2173361'
 trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
 trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
 report=$trimdir$name"_report.txt"
--- bash_scripts/run_quast.sh
+++ bash_scripts/run_quast.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-f="/home/kika/MEGAsync/Data/Chlamydomonas_amazonie/2015_DNA-seq/cam_spades_2.fasta"
-output="/home/kika/MEGAsync/Data/Chlamydomonas_amazonie/2015_DNA-seq/quast_results_2/"
+f="/home/kika/blasto_project/jaculum/transcriptome/jac_trinity.fasta"
+output="/home/kika/blasto_project/jaculum/transcriptome/quast/"
 
 /usr/bin/python3.5 /home/kika/programs/quast-4.4/quast.py $f -o $output -t 4
