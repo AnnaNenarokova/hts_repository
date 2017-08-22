5d8b08b64cddf23e88cd571021fd7bbe6d9100e8
Kika Zahonova <kika.zahonova@gmail.com>
2017-08-22 09:56:18 +0200

    bla

--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-read_dir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
-fw=$read_dir'lygus_sraA_trimmed_fw_upd.fq',$read_dir'lygus_sraB_trimmed_fw_upd.fq',$read_dir'lygus_sraC_trimmed_fw_upd.fq'
-rv=$read_dir'lygus_sraA_trimmed_rev_upd.fq',$read_dir'lygus_sraB_trimmed_rev_upd.fq',$read_dir'lygus_sraC_trimmed_rev_upd.fq'
-out_dir='/media/4TB1/blastocrithidia/bexlh/trinity_assembly_sraABC'
+read_dir='/media/4TB1/blastocrithidia/jac_rna_reads/trimmed/'
+fw=$read_dir'jac_trimmed_1.fq.gz'
+rv=$read_dir'jac_trimmed_2.fq.gz'
+out_dir='/media/4TB1/blastocrithidia/transcriptome_assembly/trinity_denovo/jac_default/'
 
 Trinity --seqType fq --left $fw --right $rv --output $out_dir --max_memory 100G --CPU 30
