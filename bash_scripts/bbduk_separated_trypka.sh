638ac75efd2d7eef40fe114df1e8b8909a0aeb3b
Kika Zahonova <kika.zahonova@gmail.com>
2017-08-27 19:36:32 +0200

    bla

--- bash_scripts/bbduk_separated_trypka.sh
+++ bash_scripts/bbduk_separated_trypka.sh
@@ -2,25 +2,25 @@
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=60
 
-# fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_1.fastq.gz"
-# rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_2.fastq.gz"
-# trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/cold/'
-# name='SRR1186295'
-# trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
-# trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
-# report=$trimdir$name"_report.txt"
-# adapt='/home/kika/tools/bbmap/resources/adapters.fa'
-# /home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
+fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_1.fastq.gz"
+rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_2.fastq.gz"
+trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/cold/'
+name='SRR1186295'
+trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
+trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
+report=$trimdir$name"_report.txt"
+adapt='/home/kika/tools/bbmap/resources/adapters.fa'
+/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
 
-# fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186315_1.fastq.gz"
-# rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186315_2.fastq.gz"
-# trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/cold/'
-# name='SRR1186315'
-# trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
-# trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
-# report=$trimdir$name"_report.txt"
-# adapt='/home/kika/tools/bbmap/resources/adapters.fa'
-# /home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
+fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186315_1.fastq.gz"
+rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186315_2.fastq.gz"
+trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/cold/'
+name='SRR1186315'
+trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
+trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
+report=$trimdir$name"_report.txt"
+adapt='/home/kika/tools/bbmap/resources/adapters.fa'
+/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
 
 fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186516_1.fastq.gz"
 rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186516_2.fastq.gz"
--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-read_dir='/media/4TB1/blastocrithidia/jac_rna_reads/trimmed/'
-fw=$read_dir'jac_trimmed_1.fq.gz'
-rv=$read_dir'jac_trimmed_2.fq.gz'
-out_dir='/media/4TB1/blastocrithidia/transcriptome_assembly/trinity_denovo/jac_trinity/'
+read_dir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/'
+fw=$read_dir'SRR2170108_trimmed_1.fq.gz',$read_dir'SRR2170117_trimmed_1.fq.gz',$read_dir'SRR2173361_trimmed_1.fq.gz'
+rv=$read_dir'SRR2170108_trimmed_2.fq.gz',$read_dir'SRR2170117_trimmed_2.fq.gz',$read_dir'SRR2173361_trimmed_2.fq.gz'
+out_dir='/media/4TB1/blastocrithidia/bexlh/transcriptome_trinity/'
 
 Trinity --seqType fq --left $fw --right $rv --output $out_dir --max_memory 100G --CPU 30
