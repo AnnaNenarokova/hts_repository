419b20c6438d8f929da1ef09906b0af1b6aa140f
Kika Zahonova <kika.zahonova@gmail.com>
2017-08-27 18:40:02 +0200

    bla

--- bash_scripts/bbduk_separated_trypka.sh
+++ bash_scripts/bbduk_separated_trypka.sh
@@ -2,37 +2,6 @@
 #PBS -l walltime=100:00:00
 #PBS -l nodes=1:ppn=60
 
-fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186658_1.fastq.gz"
-rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186658_2.fastq.gz"
-trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/non/'
-name='SRR1186658'
-trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
-trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
-report=$trimdir$name"_report.txt"
-adapt='/home/kika/tools/bbmap/resources/adapters.fa'
-/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
-
-fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186749_1.fastq.gz"
-rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186749_2.fastq.gz"
-trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/non/'
-name='SRR1186749'
-trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
-trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
-report=$trimdir$name"_report.txt"
-adapt='/home/kika/tools/bbmap/resources/adapters.fa'
-/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
-
-fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186752_1.fastq.gz"
-rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186752_2.fastq.gz"
-trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/non/'
-name='SRR1186752'
-trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
-trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
-report=$trimdir$name"_report.txt"
-adapt='/home/kika/tools/bbmap/resources/adapters.fa'
-/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
-
-
 fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_1.fastq.gz"
 rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_2.fastq.gz"
 trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/cold/'
@@ -57,37 +26,6 @@ fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186516_1.fastq.gz"
 rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186516_2.fastq.gz"
 trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/cold/'
 name='SRR1186516'
-trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
-trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
-report=$trimdir$name"_report.txt"
-adapt='/home/kika/tools/bbmap/resources/adapters.fa'
-/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
-
-
-fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186787_1.fastq.gz"
-rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186787_2.fastq.gz"
-trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/heat/'
-name='SRR1186787'
-trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
-trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
-report=$trimdir$name"_report.txt"
-adapt='/home/kika/tools/bbmap/resources/adapters.fa'
-/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
-
-fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186788_1.fastq.gz"
-rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186788_2.fastq.gz"
-trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/heat/'
-name='SRR1186788'
-trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
-trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
-report=$trimdir$name"_report.txt"
-adapt='/home/kika/tools/bbmap/resources/adapters.fa'
-/home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t ktrim=r k=22 mink=11 hdist=2 tpe tbo t=32 qtrim=rl trimq=20
-
-fw="/media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186789_1.fastq.gz"
-rv="/media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186789_2.fastq.gz"
-trimdir='/media/4TB1/blastocrithidia/bexlh/reads/trimmed/heat/'
-name='SRR1186789'
 trimmed_fw=$trimdir$name'_trimmed_1.fq.gz'
 trimmed_rv=$trimdir$name'_trimmed_2.fq.gz'
 report=$trimdir$name"_report.txt"
