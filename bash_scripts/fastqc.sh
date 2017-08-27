5dee5e3daedf2acd3d7f38d50ff5a7b38a28c84f
Kika Zahonova <kika.zahonova@gmail.com>
2017-08-27 19:19:01 +0200

    bla

--- bash_scripts/fastqc.sh
+++ bash_scripts/fastqc.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186658_1.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186658_2.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186749_1.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186749_2.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186752_1.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186752_2.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186658_1.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186658_2.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186749_1.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186749_2.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186752_1.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/non/ /media/4TB1/blastocrithidia/bexlh/reads/raw/non/SRR1186752_2.fastq.gz
 
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_1.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_2.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186315_1.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186315_2.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_1.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186295_2.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186315_1.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186315_2.fastq.gz
 /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186516_1.fastq.gz
 /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/cold/ /media/4TB1/blastocrithidia/bexlh/reads/raw/cold/SRR1186516_2.fastq.gz
 
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186787_1.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186787_2.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186788_1.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186788_2.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186789_1.fastq.gz
-/home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186789_2.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186787_1.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186787_2.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186788_1.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186788_2.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186789_1.fastq.gz
+# /home/kika/tools/FastQC/fastqc -o /media/4TB1/blastocrithidia/bexlh/reads/fastqc/heat/ /media/4TB1/blastocrithidia/bexlh/reads/raw/heat/SRR1186789_2.fastq.gz
