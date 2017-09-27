e436cfe90875785239ab144ea2ec463b9918d1a7
Anna Nenarokova <a.nenarokova@gmail.com>
2017-09-27 14:31:54 +0200

    ni

--- bash_scripts/blast_many.sh
+++ bash_scripts/blast_many.sh
@@ -3,5 +3,6 @@ cd /media/4TB1/blastocrithidia/blast_searches/datasets/all_kinetoplastid_referen
 
 for f in *filtered.faa
 do
+    echo $f
     /home/nenarokova/ngs/py_scripts/blast/use_blast.py $f
 done
