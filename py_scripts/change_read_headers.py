5b0af5ba27f7c3c202a1ab21c8c5abb7a16908cd
Kika Zahonova <kika.zahonova@gmail.com>
2017-10-11 22:32:27 +0200

    bla

--- py_scripts/change_read_headers.py
+++ py_scripts/change_read_headers.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
 import os
 
-os.chdir('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/')
+os.chdir('/home/kika/MEGAsync/Data/brevimastigomonas_motovehiculus/')
 files = os.listdir()
 
 for file in files:
-	if ('sra' in file and '_fw.fq' in file) or ('sra' in file and '_rev.fq' in file):
+	if '.fq' in file:
 		print(file)
-		file_name = file.split('.')[0]
-		out = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/' + file_name + '_upd.fq', 'w')
+		SRR = file.split('_')[0]
+		pair = file.split('_')[2].split('.')[0]
 		infile = open(file).read()
 		lines = infile.split('\n')
 		for line in lines:
-			if line.startswith('@SRR'):
-				#@SRR2173361.101.1 HWI-ST885:166:H84KHADXX:1:1101:2021:2092 length=100
-				#@SRR2173361.101.2 HWI-ST885:166:H84KHADXX:1:1101:2021:2092 length=100
+			out = open(SRR + '_trimmed_renamed_' + pair + '.fq', 'a')
+			if line.startswith('@HWI'):
+				# @HWI-ST942:114:C11YJACXX:3:1101:3080:2460_forward/1
+				# @HWI-ST942:114:C11YJACXX:3:1101:3080:2460_reverse/2
 				print(line)
-				description = (line.split(' ')[0].split('.')[0] + '.' + line.split(' ')[0].split('.')[1] + ' ' + 
-					line.split(' ')[0].split('.')[2] + ' ' + line.split(' ')[1])
+				description = line.split('_')[0] + '/' + line.split('_')[1].split('/')[1]
+				out.write(description + '\n')
+			elif line.startswith('@GWZ'):
+				# @GWZHISEQ01:84:D0V9TACXX:2:1101:7691:1834_forward/1
+				# @GWZHISEQ01:84:D0V9TACXX:2:1101:7691:1834_reverse/2
+				print(line)
+				description = line.split('_')[0] + '/' + line.split('_')[1].split('/')[1]
 				out.write(description + '\n')
 			else:
 				out.write(line + '\n')
-
-	if ('sra' not in file and '_fw.fq' in file) or ('sra' not in file and '_rev.fq' in file):
-		print(file)
-		file_name = file.split('.')[0]
-		out = open('/media/4TB1/blastocrithidia/bexlh/reads/trimmed/' + file_name + '_upd.fq', 'w')
-		infile = open(file).read()
-		lines = infile.split('\n')
-		for line in lines:
-			if line.startswith('@SRR'):
-				#@SRR1186295.25.1 25 length=100
-				#@SRR1186295.25.2 25 length=100
-				print(line)
-				description = (line.split(' ')[0].split('.')[0] + '.' + line.split(' ')[0].split('.')[1] + ' ' + 
-					line.split(' ')[0].split('.')[2])
-				out.write(description + '\n')
-			else:
-				out.write(line + '\n')
+			out.close()
