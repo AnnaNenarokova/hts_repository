cf7a99c2da59c21d8c6de1c0793c6f0f610f953e
Kika Zahonova <kika.zahonova@gmail.com>
2017-10-12 09:11:55 +0200

    bla

--- bash_scripts/run_trinity_separated_trypka.sh
+++ bash_scripts/run_trinity_separated_trypka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 read_dir='/media/4TB1/blastocrithidia/kika_workdir/reads/'
-fw=$read_dir'SRR4017973_trimmed_1.fq.gz',$read_dir'SRR4017993_trimmed_1.fq.gz'
-rv=$read_dir'SRR4017973_trimmed_2.fq.gz',$read_dir'SRR4017993_trimmed_2.fq.gz'
+fw=$read_dir'SRR4017973_trimmed_renamed_1.fq',$read_dir'SRR4017993_trimmed_renamed_1.fq'
+rv=$read_dir'SRR4017973_trimmed_renamed_2.fq',$read_dir'SRR4017993_trimmed_renamed_2.fq'
 out_dir='/media/4TB1/blastocrithidia/kika_workdir/trinity_assembly/'
 
 Trinity --seqType fq --left $fw --right $rv --output $out_dir --max_memory 100G --CPU 16
