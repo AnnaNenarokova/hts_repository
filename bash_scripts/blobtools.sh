2f2b5781b88eb83344a1867791069577c5709078
Kika Zahonova <kika.zahonova@gmail.com>
2017-11-27 15:05:34 +0100

    bla

--- bash_scripts/blobtools.sh
+++ bash_scripts/blobtools.sh
@@ -3,12 +3,9 @@
 c_in='/home/kika/tools/blobtools/example/assembly.fna'
 bam='/home/kika/tools/blobtools/example/mapping_1.bam'
 blast='/home/kika/tools/blobtools/example/blast.out'
-out='/home/kika/tools/blobtools/example/my_first_blobplot/'
-
-# /home/kika/tools/blobtools/blobtools create -i $c_in -b $bam -o $out
-# -t $blast 
-
+out='/home/kika/tools/blobtools/example/my_second_blobplot_blast/'
 v_in=$out'blobDB.json'
-# /home/kika/tools/blobtools/blobtools view -i $v_in -o $out
 
+/home/kika/tools/blobtools/blobtools create -i $c_in -b $bam -t $blast -o $out
+/home/kika/tools/blobtools/blobtools view -i $v_in -o $out
 /home/kika/tools/blobtools/blobtools plot -i $v_in -o $out
