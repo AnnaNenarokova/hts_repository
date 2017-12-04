32188268ef5850bc5306297da8581f6e138b5abb
Anna Nenarokova <a.nenarokova@gmail.com>
2017-12-04 11:33:50 +0100

    ni

--- bash_scripts/blast_many.sh
+++ bash_scripts/blast_many.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd /media/4TB1/blastocrithidia/blast_searches/datasets/all_filtered_proteins_20_11
-
-for f in *.faa
+for f in /media/4TB1/blastocrithidia/blast_searches/datasets/all_filtered_proteins_20_11/*.faa
 do
     echo $f
     /home/nenarokova/ngs/py_scripts/blast/use_blast.py $f
