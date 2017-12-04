e29215895ede80eb87ca2a3b42353a2f26392ada
Kika Zahonova <kika.zahonova@gmail.com>
2017-12-04 09:38:02 +0100

    bla

--- bash_scripts/blobtools.sh
+++ bash_scripts/blobtools.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 blob='/home/kika/tools/blobtools/blobtools'
-c_in='/media/4TB1/blastocrithidia/bexlh/lhes1_PRJNA238835_trinity/Trinity.fasta'
-bam='/media/4TB1/blastocrithidia/mapping/lhes1_bowtie2_RNA/lhes1_bw2_sorted.bam'
-blast='/media/4TB1/blastocrithidia/kika_workdir/lhes1.diamond_out'
-out='/media/4TB1/blastocrithidia/kika_workdir/lhes1_blobtools/'
-v_in=$out'blobDB.json'
+fasta='/media/4TB1/blastocrithidia/bexlh/lhes2_PRJNA284294_trinity/Trinity.fasta'
+bam='/media/4TB1/blastocrithidia/mapping/lhes2_bowtie2_RNA/lhes2_bw2_sorted.bam'
+blast='/media/4TB1/blastocrithidia/kika_workdir/lhes2.diamond_out'
+out='/media/4TB1/blastocrithidia/kika_workdir/lhes2_blobtools/'
+input=$out'blobDB.json'
 
-$blob create -i $c_in -b $bam -t $blast -o $out
-$blob view -i $v_in -o $out
-$blob plot -i $v_in -o $out
+$blob create -i $fasta -b $bam -t $blast -o $out
+$blob view -i $input -o $out
+$blob plot -i $input -o $out
