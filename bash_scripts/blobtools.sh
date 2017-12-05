bb524d99c0141a3dc0d6e039ad96b7bf5852eed7
Kika Zahonova <kika.zahonova@gmail.com>
2017-12-05 09:21:18 +0100

    bla

--- bash_scripts/blobtools.sh
+++ bash_scripts/blobtools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 blob='/home/kika/tools/blobtools/blobtools'
-fasta='/media/4TB1/blastocrithidia/bexlh/lhes1_PRJNA238835_trinity/Trinity.fasta'
-bam='/media/4TB1/blastocrithidia/mapping/lhes1_bowtie2_RNA/lhes1_bw2_sorted.bam'
-blast='/media/4TB1/blastocrithidia/kika_workdir/lhes1_new.diamond_out'
-out='/media/4TB1/blastocrithidia/kika_workdir/lhes1_new/'
+fasta='/media/4TB1/blastocrithidia/bexlh/lhes2_PRJNA284294_trinity/Trinity.fasta'
+bam='/media/4TB1/blastocrithidia/mapping/lhes2_bowtie2_RNA/lhes2_bw2_sorted.bam'
+blast='/media/4TB1/blastocrithidia/kika_workdir/lhes2_new.diamond_out'
+out='/media/4TB1/blastocrithidia/kika_workdir/lhes2_new/'
 input=$out'blobDB.json'
 
 $blob create -i $fasta -b $bam -t $blast -o $out
