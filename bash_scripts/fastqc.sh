5150ffc2a608b6d149010df95abde240368f17a8
Kika Zahonova <kika.zahonova@gmail.com>
2018-01-19 17:30:46 +0100

    bla

--- bash_scripts/fastqc.sh
+++ bash_scripts/fastqc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 out_dir='/home/kika/diplonema/reads/raw/fastqc/'
-read_dir='/home/kika/diplonema/reads/raw'
+read_dir='/home/kika/diplonema/reads/raw/'
 
 /home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'MI.M03555_0253.001.Index_13.YPF1604_R1.fastq.gz'
