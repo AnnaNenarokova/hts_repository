6f5a63dce751edccf5c2677205f3d78b2b4fc9c6
Kika Zahonova <kika.zahonova@gmail.com>
2018-01-23 14:01:44 +0100

    bla

--- bash_scripts/fastqc.sh
+++ bash_scripts/fastqc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-read_dir='/home/kika/diplonema/reads/merged_qtrimmed_vstrict/'
-out_dir='/home/kika/diplonema/reads/merged_qtrimmed_vstrict/fastqc/'
+read_dir='/home/kika/diplonema/reads/merged_qtrimmed_ustrict/'
+out_dir='/home/kika/diplonema/reads/merged_qtrimmed_ustrict/fastqc/'
 
 /home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'YPF1604_adapter_trimmed_merged.fq'
 /home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'YPF1604_adapter_trimmed_unmerged_1.fq'
