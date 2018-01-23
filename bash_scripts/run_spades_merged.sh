f6170faa070a3bd02a188d0bfab6f620ee4ff57f
Kika Zahonova <kika.zahonova@gmail.com>
2018-01-23 14:51:53 +0100

    bla

--- bash_scripts/run_spades_merged.sh
+++ bash_scripts/run_spades_merged.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-read_dir='/home/kika/diplonema/reads/merged/'
+read_dir='/home/kika/diplonema/reads/merged_qtrimmed_strict/'
 merged=$read_dir'YPF1604_adapter_trimmed_merged.fq'
-fw=$read_dir'YPF1604_adapter_trimmed_unmerged_1.fq'
-rv=$read_dir'YPF1604_adapter_trimmed_unmerged_2.fq'
+fw=$read_dir'YPF1604_unmerged_fully_trimmed_1.fq'
+rv=$read_dir'YPF1604_unmerged_fully_trimmed_2.fq'
 
 outdir='/home/kika/diplonema/genome_assembly/1604/'
 report=$outdir"spades_report.txt"
