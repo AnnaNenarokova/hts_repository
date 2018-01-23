58a5b36e716a913a0c9dce9b4beabf0845af135b
Kika Zahonova <kika.zahonova@gmail.com>
2018-01-23 20:17:59 +0100

    bla

--- bash_scripts/bbduk_separated_trypka.sh
+++ bash_scripts/bbduk_separated_trypka.sh
@@ -13,3 +13,4 @@ report=$trimdir$name"_report.txt"
 adapt='/home/kika/tools/bbmap/resources/adapters.fa'
 
 /home/kika/tools/bbmap/bbduk.sh overwrite=true in1=$fw in2=$rv out1=$trimmed_fw out2=$trimmed_rv ref=$adapt usejni=t qtrim=r trimq=20 ktrim=r k=22 mink=11 hdist=2 tpe tbo t=30
+# 
--- bash_scripts/fastqc.sh
+++ bash_scripts/fastqc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-read_dir='/home/kika/diplonema/reads/merged_qtrimmed_vstrict/'
-out_dir='/home/kika/diplonema/reads/merged_qtrimmed_vstrict/fastqc/'
+read_dir='/home/kika/diplonema/reads/trimmed/'
+out_dir='/home/kika/diplonema/reads/trimmed/fastqc/'
 
 # /home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'YPF1604_adapter_trimmed_merged.fq'
-/home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'YPF1604_unmerged_fully_trimmed_1.fq'
-/home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'YPF1604_unmerged_fully_trimmed_2.fq'
+/home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'YPF1604_trimmed_1.fq.gz'
+/home/kika/tools/FastQC/fastqc -o $out_dir $read_dir'YPF1604_trimmed_2.fq.gz'
