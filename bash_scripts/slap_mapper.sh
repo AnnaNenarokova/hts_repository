05fd5b539c9f64638f086bafbabbbaf276e5ef63
Anna Nenarokova <a.nenarokova@gmail.com>
2018-10-02 16:24:45 +0200

    ni

--- bash_scripts/slap_mapper.sh
+++ bash_scripts/slap_mapper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 slap_mapper="/home/nenarokova/tools/SLaPMapper/SLaPMapper.pl"
 genome="/media/4TB1/blastocrithidia/genome_assembly/p57_scaffolds.fa"
-reads1="/media/4TB1/blastocrithidia/reads/transcriptome/raw/Blastoc_spP57_1.fastq"
-reads2="/media/4TB1/blastocrithidia/reads/transcriptome/raw/Blastoc_spP57_2.fastq"
-gff="/media/4TB1/blastocrithidia/slap_mapping/empty_annotation.gff"
-work_dir="/media/4TB1/blastocrithidia/slap_mapping"
+reads1="/media/4TB1/blastocrithidia/reads/transcriptome/raw/p57_both_RNA_1.fastq"
+reads2="/media/4TB1/blastocrithidia/reads/transcriptome/raw/p57_both_RNA_2.fastq"
+gff="/media/4TB1/blastocrithidia/mapping/slap_mapping/p57_empty_annotation.gff"
+work_dir="/media/4TB1/blastocrithidia/mapping/slap_mapping/p57_both_rna"
 
 SL="AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG"
 min_length="6"
