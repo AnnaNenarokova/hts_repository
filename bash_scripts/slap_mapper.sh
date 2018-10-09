28c2799ee62be479661647333d7b0430132575e9
Anna Nenarokova <a.nenarokova@gmail.com>
2018-10-09 14:51:43 +0200

    ni

--- bash_scripts/slap_mapper.sh
+++ bash_scripts/slap_mapper.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 slap_mapper="/home/nenarokova/tools/SLaPMapper/SLaPMapper.pl"
-genome="/media/4TB1/blastocrithidia/genome_assembly/p57_scaffolds.fa"
-reads1="/media/4TB1/blastocrithidia/reads/transcriptome/raw/p57_both_RNA_1.fastq"
-reads2="/media/4TB1/blastocrithidia/reads/transcriptome/raw/p57_both_RNA_2.fastq"
-gff="/media/4TB1/blastocrithidia/mapping/slap_mapping/p57_empty_annotation.gff"
-work_dir="/media/4TB1/blastocrithidia/mapping/slap_mapping/p57_both_rna_2"
+genome="/media/4TB1/novymonas/genome/annotation/companion_no_pand_lbraz/scafs.fasta"
+reads1="/media/4TB1/novymonas/transcriptome/reads/raw_reads/No_WT1_1.fastq.gz"
+reads2="/media/4TB1/novymonas/transcriptome/reads/raw_reads/No_WT1_2.fastq.gz"
+gff="/media/4TB1/novymonas/genome/annotation/companion_no_pand_lbraz/scaffold.out.gff3"
+work_dir="/media/4TB1/novymonas/slap_mapping/"
 
-SL="AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG"
-min_length="2"
+#SL="AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG" #p57
+SL="ATCAGTTTCTGTACTTTATTG" #Novymonas
+
+min_length="6"
 
 cd $work_dir
 ln -s $reads1 reads1.fastq
