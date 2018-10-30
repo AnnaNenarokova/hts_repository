56b4d5978031c10ddfc2ee454a37e8a009f6e5bf
Anna Nenarokova <a.nenarokova@gmail.com>
2018-10-30 11:50:07 +0100

    ni

--- bash_scripts/slap_mapper.sh
+++ bash_scripts/slap_mapper.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 slap_mapper="/home/nenarokova/tools/SLaPMapper/SLaPMapper.pl"
-genome="/media/4TB1/novymonas/genome/annotation/companion_no_pand_lbraz/scafs.fasta"
-reads1="/media/4TB1/novymonas/transcriptome/reads/raw_reads/No_WT1_1.fastq"
-reads2="/media/4TB1/novymonas/transcriptome/reads/raw_reads/No_WT1_2.fastq"
-gff="/media/4TB1/novymonas/genome/annotation/companion_no_pand_lbraz/scaffold.out.gff3"
-work_dir="/media/4TB1/novymonas/slap_mapping/"
+genome="/media/4TB1/blastocrithidia/UTR_analysis/references/blechomonas/TriTrypDB-33_BayalaiB08-376_Genome.fasta"
+reads1="/media/4TB1/blastocrithidia/UTR_analysis/references/blechomonas/reads/Blechomonas_forward_reads.fastq"
+reads2="/media/4TB1/blastocrithidia/UTR_analysis/references/blechomonas/reads/Blechomonas_reverse_reads.fastq"
+gff="/media/4TB1/blastocrithidia/UTR_analysis/references/blechomonas/TriTrypDB-33_BayalaiB08-376.gff"
+work_dir="/media/4TB1/blastocrithidia/mapping/slap_mapping/blechomonas"
 
 #SL="AACGCATTTTTTGTTACAGTTTCTGTACTTTATTG" #p57
-SL="ATCAGTTTCTGTACTTTATTG" #Novymonas
-
+#SL="ATCAGTTTCTGTACTTTATTG" #Novymonas
+SL="AACTAACGCTATTATTGATACAGTTTCTGTACTATATTG" #b_ayalai
 min_length="6"
 
 cd $work_dir
--- py_scripts/blast/use_blast.py
+++ py_scripts/blast/use_blast.py
@@ -36,23 +36,23 @@ def add_header(blast_csv_path, custom_outfmt):
 custom_outfmt = 'qseqid qlen sseqid slen length evalue pident bitscore mismatch gaps qstart qend sstart send'
 
 query_paths= [
-    "/home/anna/bioinformatics/blasto_local/tbrucei_tubulins.fasta"
+    "/home/anna/bioinformatics/novymonas/primers_23_10.fa"
     ]
 
 subj_paths = [
-    "/home/anna/bioinformatics/blasto_local/p57_scaffolds.fasta"
+    "/home/anna/bioinformatics/novymonas/novymonas_no_pand_scaffolds/blast_db/novymonas_no_pand_scaffolds.fasta.db"
     ]
 
 for query_path in query_paths:
     for subj_path in subj_paths:
-        new_blast = Blast(query_path=query_path,subj_path=subj_path, db_type='nucl', threads=4)
+        new_blast = Blast(query_path=query_path,db_path=subj_path, db_type='nucl', threads=4)
         blast_csv_path = new_blast.blast(
-                                         bl_type='tblastn',
-                                         evalue=0.1,
-                                         outfmt='comma_values',
-                                         # outfmt='pairwise',
+                                         bl_type='blastn',
+                                         evalue=1,
+                                         # outfmt='comma_values',
+                                         outfmt='pairwise',
                                          custom_outfmt=custom_outfmt,
-                                         word_size=2
+                                         word_size=7
                                          )
         add_header(best_hits(blast_csv_path), custom_outfmt)
         add_header(blast_csv_path, custom_outfmt)
