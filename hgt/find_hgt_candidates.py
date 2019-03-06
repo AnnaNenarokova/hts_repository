5ff6483d770050f27a832044199533ac157e5d59
AnnaNenarokova <a.nenarokova@gmail.com>
2019-03-06 12:16:46 +0000

    ni

--- hgt/find_hgt_candidates.py
+++ hgt/find_hgt_candidates.py
@@ -8,6 +8,11 @@ diamond_result_path = "/home/vl18625/bioinformatics/diplonema/dpapi_genome_diamo
 infasta_path = "/home/vl18625/bioinformatics/diplonema/Dp_PB-MI_190104_dedup_cut_l100.faa"
 outfasta_path = "/home/vl18625/bioinformatics/diplonema/dpapi_hgt_candidates.faa"
 
+outfmt_opts="qseqid qlen sseqid slen staxids length evalue bitscore"
+
+outfmt_opt_list = outfmt_opts.split(" ")
+
+
 evalue_cutoff = 1
 bacteria_taxid = 2
 
@@ -23,16 +28,16 @@ candidates = []
 with open diamond_result_path as diamond_f:
     for line in diamond_f:
         line_split = line.rstrip().split("\t")
-        id = line_split[0]
-        taxid = line_split[1]
-        evalue = float(line_split[2])
+        id = line_split[outfmt_opt_list.index("qseqid")]
+        taxid = line_split[outfmt_opt_list.index("staxids")]
+        evalue = float(line_split[outfmt_opt_list.index("evalue")])
         if taxid == "0":
             is_bacteria = False
         else:
             is_bacteria = True if bacteria_taxid in ncbi_taxa.get_lineage(taxid) else False
         if is_bacteria and evalue < evalue_cutoff:
             candidates.append(all_records[id])
-            
+
 SeqIO.write(candidates, outfasta_path, "fasta")
  
 
--- hgt/prepare_seqs.py
+++ hgt/prepare_seqs.py
@@ -1,19 +1,36 @@
 #!python3
 from Bio import SeqIO
 from Bio import Entrez
-
 Entrez.email = "a.nenarokova@gmail.com"
+
 diamond_result_path = "/projects/Diplonema_genome_evolution/hgt/hgt_nr_diamond_0e.tsv"
-taxid_into_path = "/projects/Diplonema_genome_evolution/hgt/hgt_nr_diamond_0e_taxids.tsv"
+infasta_path = "/projects/Diplonema_genome_evolution/hgt/dpapi_hgt_candidates.faa"
+outfmt_opts="qseqid qlen sseqid slen staxids length evalue bitscore"
+outdir="/projects/Diplonema_genome_evolution/hgt/results/fasta/"
 
+dpapi_seqs = {}
+for record in SeqIO.parse(infasta_path, "fasta"):
+	dpapi_seqs[record.id] = record
 
-with open diamond_result_path as diamond_f, open taxid_into_path as taxid_f:
+with open diamond_result_path as diamond_f:
+	current_qseqid = None
+	current_f = None
+
     for line in diamond_f:
         line_split = line.rstrip().split("\t")
-        query_id = line_split[0]
-        subject_id = line_split[0]
-        taxid_f.
-
-
-handle = Entrez.efetch(db="protein", id="WP_014997480.1", rettype="fasta") 
-
+        qseqid = line_split[outfmt_opt_list.index("qseqid")]
+        sseqid = line_split[outfmt_opt_list.index("sseqid")]
+        taxid = line_split[outfmt_opt_list.index("staxids")]
+        evalue = float(line_split[outfmt_opt_list.index("evalue")])
+        
+        if qseqid != current_qseqid:
+        	if current_f: current_f.close()
+        	current_qseqid = qseqid
+        	current_path = outdir + current_qseqid + ".faa"
+        	dpapi_record = [dpapi_seqs[qseqid]]
+        	SeqIO.write(dpapi_record, current_path, "fasta")
+        	current_f = current_path.open("a")
+		ncbi_record = Entrez.efetch(db="protein", id=sseqid, rettype="fasta")
+		current_f.write(ncbi_record)
+		# handle = Entrez.efetch(db="protein", id="WP_014997480.1", rettype="fasta")
+	current_f.close()
