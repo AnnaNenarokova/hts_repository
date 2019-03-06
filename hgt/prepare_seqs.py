1acb4b04ef2377aecc18691c025e636fbe19cb28
AnnaNenarokova <a.nenarokova@gmail.com>
2019-03-06 12:58:23 +0000

    ni

--- hgt/prepare_seqs.py
+++ hgt/prepare_seqs.py
@@ -5,32 +5,43 @@ Entrez.email = "a.nenarokova@gmail.com"
 
 diamond_result_path = "/projects/Diplonema_genome_evolution/hgt/hgt_nr_diamond_0e.tsv"
 infasta_path = "/projects/Diplonema_genome_evolution/hgt/dpapi_hgt_candidates.faa"
-outfmt_opts="qseqid qlen sseqid slen staxids length evalue bitscore"
 outdir="/projects/Diplonema_genome_evolution/hgt/results/fasta/"
+
+# diamond_result_path = "/home/vl18625/bioinformatics/diplonema/hgt/hgt_nr_diamond_0e.tsv"
+# infasta_path = "/home/vl18625/bioinformatics/diplonema/hgt/dpapi_hgt_candidates.faa"
+# outdir="/home/vl18625/bioinformatics/diplonema/hgt/results/fasta/"
+
+outfmt_opts="qseqid qlen sseqid slen staxids length evalue bitscore"
+outfmt_opt_list = outfmt_opts.split(" ")
 
 dpapi_seqs = {}
 for record in SeqIO.parse(infasta_path, "fasta"):
-	dpapi_seqs[record.id] = record
+    dpapi_seqs[record.id] = record
 
 with open(diamond_result_path) as diamond_f:
-	current_qseqid = None
-	current_f = None
-
+    current_qseqid = None
+    current_f = None
+    i = 0
     for line in diamond_f:
+        i += 1
+        print (i)
         line_split = line.rstrip().split("\t")
         qseqid = line_split[outfmt_opt_list.index("qseqid")]
         sseqid = line_split[outfmt_opt_list.index("sseqid")]
         taxid = line_split[outfmt_opt_list.index("staxids")]
         evalue = float(line_split[outfmt_opt_list.index("evalue")])
         
+        print (current_qseqid)
+
+
         if qseqid != current_qseqid:
-        	if current_f: current_f.close()
-        	current_qseqid = qseqid
-        	current_path = outdir + current_qseqid + ".faa"
-        	dpapi_record = [dpapi_seqs[qseqid]]
-        	SeqIO.write(dpapi_record, current_path, "fasta")
-        	current_f = current_path.open("a")
-		ncbi_record = Entrez.efetch(db="protein", id=sseqid, rettype="fasta")
-		current_f.write(ncbi_record)
-		# handle = Entrez.efetch(db="protein", id="WP_014997480.1", rettype="fasta")
-	current_f.close()
+            if current_f: current_f.close()
+            current_qseqid = qseqid
+            current_path = outdir + current_qseqid + ".faa"
+            dpapi_record = [dpapi_seqs[qseqid]]
+            SeqIO.write(dpapi_record, current_path, "fasta")
+            current_f = open(current_path, "a")
+            print (current_path)
+        ncbi_record = Entrez.efetch(db="protein", id=sseqid, rettype="fasta").read()[:-1]
+        current_f.write(ncbi_record)
+    current_f.close()
