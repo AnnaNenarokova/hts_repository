63009c6e59634ef31d0ba2a304b3a6670bf76a61
AnnaNenarokova <a.nenarokova@gmail.com>
2019-04-18 10:56:31 +0100

    ni

--- hgt/check_hgt_trees.py
+++ hgt/check_hgt_trees.py
@@ -5,7 +5,7 @@ import sys
 from os import listdir
 
 treedir_path="/projects/Diplonema_genome_evolution/hgt/results/trees/"
-taxid_path="/projects/Diplonema_genome_evolution/hgt/wholedataset_1_taxid.csv"
+taxid_path="/projects/Diplonema_genome_evolution/hgt/whole_set_taxids.csv"
 bootstrap_threshold = 70.0
 
 def parse_taxids(taxid_path, delimeter=","):
@@ -34,8 +34,6 @@ def get_tags_leaves(tree, taxid_dict):
         if "DIPPA" in seqid:
             leaf_tags[seqid] = "dpapi"
         else:
-            print (seqid)
-            print (taxid_dict[seqid])
             taxid = int(taxid_dict[seqid])
             if taxid == dpapi_taxid:
                 leaf_tags[seqid] = "dpapi"
