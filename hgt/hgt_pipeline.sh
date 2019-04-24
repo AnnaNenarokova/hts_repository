82486a7f8e7dd0fbc833af923514003b53c96d8d
AnnaNenarokova <a.nenarokova@gmail.com>
2019-04-24 14:24:18 +0100

    ni

--- hgt/hgt_pipeline.sh
+++ hgt/hgt_pipeline.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-threads="5"
+threads="30"
 
 infasta=""
-outdir="/projects/Diplonema_genome_evolution/hgt/results"
+outdir="/projects/Diplonema_genome_evolution/hgt/results/"
 
 # bmge="/home/vl18625/bioinformatics/tools/BMGE-1.12/BMGE.jar"
 bmge="/home/vl18625/tools/BMGE-1.12/BMGE.jar"
 
 # creating directories
-fasta_dir=$outdir"/fasta/"
-msa_dir=$outdir"/msa/"
-trimmed_msa_dir=$outdir"/trimmed_msa/"
-iqtree_dir="$outdir/iqtree_files/"
+fasta_dir=$outdir"fasta/"
+msa_dir=$outdir"msa/"
+trimmed_msa_dir=$outdir"trimmed_msa/"
+iqtree_dir=$outdir"iqtree_files/"
 tree_dir=$outdir"trees"
 
 rm -r $msa_dir
@@ -24,22 +24,25 @@ rm -r $tree_dir
 mkdir $tree_dir
 
 #Diamond
-# nr_diamond_db_path="/projects/Diplonema_genome_evolution/databases/nr/nr_diamond.dmnd"
-# taxonmap="/projects/Diplonema_genome_evolution/databases/nr/prot.accession2taxid.gz"
-# taxonnodes="/projects/Diplonema_genome_evolution/databases/nr/taxdmp/nodes.dmp"
+nr_db_path="/projects/Diplonema_genome_evolution/databases/nr/nr_diamond.dmnd"
+refset_db_path="/projects/Diplonema_genome_evolution/refdataset_diplonema/no_dpapi_refdataset.dmnd"
 
-# diamond_tax_out=$outdir"tax_nr_diamond.tsv"
+taxonmap="/projects/Diplonema_genome_evolution/databases/nr/prot.accession2taxid.gz"
+taxonnodes="/projects/Diplonema_genome_evolution/databases/nr/taxdmp/nodes.dmp"
 
-# diamond_hgt_out=$outdir"hgt_nr_diamond.tsv"
+diamond_nr_out=$outdir"hgt_nr_diamond.tsv"
+diamond_refset_out=$outdir"hgt_refset_diamond.tsv"
 
-# candidates_path="/projects/Diplonema_genome_evolution/hgt/dpapi_hgt_candidates.faa"
+candidates_path="/projects/Diplonema_genome_evolution/hgt/dpapi_hgt_candidates.faa"
 
-# max_hits="25"
-# e_cutoff="0.001"
+max_hits="25"
+e_cutoff="0.001"
 
-# outfmt_opts="qseqid qlen sseqid slen staxids length evalue bitscore"
-# diamond blastp -q $candidates_path -d $nr_diamond_db_path -o $diamond_hgt_out -p $threads --max-hsps 1 --taxonmap $taxonmap -f 6 $outfmt_opts #-evalue e_cutoff -k $max_hits tab-separated output, staxids for subject taxids
+outfmt_opts="qseqid qlen sseqid slen staxids length evalue bitscore"
+diamond blastp -q $candidates_path -d $nr_db_path -o $diamond_nr_out -p $threads --max-hsps 1 --taxonmap $taxonmap -f 6 $outfmt_opts #-evalue e_cutoff -k $max_hits -f 6 tab-separated output, staxids for subject taxids
 
+outfmt_opts="qseqid qlen sseqid slen length evalue bitscore"
+diamond blastp -q $candidates_path -d $refset_db_path -o $diamond_refset_out -p $threads --max-hsps 1 -f 6 $outfmt_opts #-evalue e_cutoff -k $max_hits -f 6 tab-separated output
 
 cd $fasta_dir
 
