eba24a2fa80653f2aa122dd2fc95dcd7b32d6e50
Anna Nenarokova <a.nenarokova@gmail.com>
2022-01-27 11:31:00 +0000

    ni

--- projects/euk/gene_trees_pipeline_bp_array.sh
+++ projects/euk/gene_trees_pipeline_bp_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load apps/bmge/1.12 apps/iqtree/1.6.12 apps/mafft/7.429
+module load apps/bmge/1.12 apps/iqtree/1.6.12 apps/mafft/7.429 lang/java/15.0.1
 
 bmge="/sw/apps/BMGE-1.12/BMGE.jar"
 
@@ -9,9 +9,6 @@ msa_dir="/user/home/vl18625/euk/ed_markers/aligned/"
 trimmed_msa_dir="/user/home/vl18625/euk/ed_markers/aligned_trimmed/"
 tree_dir="/user/home/vl18625/euk/ed_markers/trees/"
 
-mkdir $msa_dir
-mkdir $trimmed_msa_dir
-mkdir $tree_dir
 
 cd $fasta_dir
 fasta=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
@@ -26,4 +23,4 @@ java -jar $bmge -i $msa -t "AA" -m BLOSUM30 -of $trimmed_msa
 
 cd $tree_dir
 
-iqtree -s $f -m MFP -bb 1000 -nt AUTOp
+iqtree -s $f -m MFP -bb 1000 -nt AUTO
