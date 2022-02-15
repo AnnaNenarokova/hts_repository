65f7dff71020cb3d5a77b3ec5852c89f45d55518
Anna Nenarokova <anna@it035271.users.bris.ac.uk>
2022-02-15 13:21:22 +0000

    ni

--- projects/euk/array_iqtrees_bp.sh
+++ projects/euk/array_iqtrees_bp.sh
@@ -4,9 +4,9 @@
 #SBATCH --partition=taw
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --time=14-00:0:0
-#SBATCH --mem=10G
+#SBATCH --mem=100G
 #SBATCH --array=1-11
 
 cd /user/home/vl18625/code/ngs/projects/euk/
--- projects/euk/iqtree_array.sh
+++ projects/euk/iqtree_array.sh
@@ -9,4 +9,4 @@ file=$(ls *.fasta | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
 echo $file
 
-iqtree2 -s $file -mset LG -madd "LG+C20,LG+C20+F" --score-diff all -bb 1000 -nt 4
+iqtree2 -s $file -mset LG -madd "LG+C20,LG+C20+F" --score-diff all -bb 1000 -nt 8
