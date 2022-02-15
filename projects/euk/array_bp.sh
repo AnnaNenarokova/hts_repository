1066c922b78a47a3c9f6e7eda667cb80d83608b3
Anna Nenarokova <anna@it035271.users.bris.ac.uk>
2022-02-15 12:28:36 +0000

    ni

--- projects/euk/array_bp.sh
+++ projects/euk/array_bp.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --time=14-00:0:0
 #SBATCH --mem=1000M
-#SBATCH --array=1-27
+#SBATCH --array=1-5
 
 cd /user/home/vl18625/code/ngs/projects/euk/
 srun gene_trees_pipeline_bp_array.sh
---
+++ projects/euk/iqtree_loop_bp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=iqtree_loop
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=14-00:00:00
+#SBATCH --mem=1000M
+
+threads=40
+
+module load apps/iqtree/2.1.3
+
+workdir="/user/home/vl18625/euk/ed_markers/trees_refitted/"
+
+cd $workdir
+for f in *.faa
+do
+    echo $f
+    iqtree -s $f iqtree2 -s alignment.aln -mset LG -madd "LG+C20,LG+C20+F" -score-diff ALL -bb 1000 -nt AUTO
+done
