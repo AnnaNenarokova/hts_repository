280f3037811289c164dd88e5f7ff03af275cef16
Anna Nenarokova <anna@it035271.users.bris.ac.uk>
2022-03-16 12:00:00 +0000

    ni

--- projects/euk/constrain_trees.py
+++ projects/euk/constrain_trees.py
@@ -14,8 +14,12 @@ def make_constrained_tree(full_seqset, monophyletic_set):
 	constrained_tree = "(" + ",".join(full_tree_set) + ");"
 	return constrained_tree
 
-def write_constrained_tree_alphabac(alignment, euk_list, alpha_list, outtree):
+def write_constrained_tree_mono(alignment, list1, list2, outtree_path):
 	seqlist = SeqIO.index(alignment, "fasta")
 	seqset = set(seqlist)
-	monophyletic_set = set.intersection(set(euk_list + alpha_list), seqset)
-	constrained_tree = make_constrained_tree(full_seqset, monophyletic_set
+	monophyletic_set = set.intersection(set(list1 + list2), seqset)
+	constrained_tree = make_constrained_tree(full_seqset, monophyletic_set)
+	return outtree_path
+
+def constrain_trees(msa_dir, list1_path, list2_path, outdir):
+	return 0
---
+++ projects/euk/mafft_hmmbuild_bp_array.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+module load apps/mafft/7.429 apps/hmmer/3.3.2
+
+bmge="/sw/apps/BMGE-1.12/BMGE.jar"
+
+fasta_dir="/user/work/vl18625/euk/markers_nina/all_markers/faa/"
+msa_dir="/user/work/vl18625/euk/markers_nina/all_markers/msa/"
+hmm_dir="/user/work/vl18625/euk/markers_nina/all_markers/hmm_profiles/"
+
+cd $fasta_dir
+fasta=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
+
+echo $fasta
+
+msa=$msa_dir$fasta
+hmm_file=$hmm_dir$fasta
+
+mafft $fasta > $msa
+hmmbuild $hmm_file $msa
