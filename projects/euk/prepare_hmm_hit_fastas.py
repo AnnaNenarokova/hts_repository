8299068c54c5205b0159e6b77c8b7d90b4ba410f
Anna Nenarokova <a.nenarokova@gmail.com>
2022-03-23 19:22:10 +0000

    ni

--- projects/euk/prepare_hmm_hit_fastas.py
+++ projects/euk/prepare_hmm_hit_fastas.py
@@ -38,7 +38,7 @@ def prepare_hmm_dict(hmm_report_dir, n_best=5, max_evalue=0.0001):
 	for hmm_report in listdir_nohidden(hmm_report_dir):
 		hmm_report_name_split = hmm_report.split(".fasta")
 		proteome_file = hmm_report_name_split[0] + ".fasta"
-		cog_file = hmm_report_name_split[1].split(".faa")[0]
+		cog_file = hmm_report_name_split[1].split(".txt")[0]
 		if proteome_file not in hmm_dict:
 			hmm_dict[proteome_file] = {}
 		hmm_dict[proteome_file][cog_file] = {}
@@ -79,7 +79,7 @@ def prepare_fastas(hmm_report_dir, proteome_dir, cog_dir, result_dir, n_best=5, 
 		SeqIO.write(out_records, outpath, "fasta")
 	return 0
 
-def prepare_fastas_keep_list(keep_list_path, hmm_report_dir, proteome_dir, cog_dir, result_dir, n_best=3, max_evalue=0.00001):
+def prepare_fastas_keep_list(keep_list_path, hmm_report_dir, proteome_dir, cog_dir, result_dir, n_best=10, max_evalue=0.00001):
 	keep_list = read_list(keep_list_path)
 	print("Parcing hmm_reports")
 	hmm_dict = prepare_hmm_dict(hmm_report_dir, n_best=n_best, max_evalue=max_evalue)
@@ -93,25 +93,24 @@ def prepare_fastas_keep_list(keep_list_path, hmm_report_dir, proteome_dir, cog_d
 			proteome_path = proteome_dir + proteome
 			for record in SeqIO.parse(proteome_path, "fasta"):
 				sseqid = record.id
-				for cog in proteome_dict:
-					if record.id in proteome_dict[cog]:
-						hmm_dict[proteome][cog][sseqid] = record
+				for cog_file in proteome_dict:
+					if record.id in proteome_dict[cog_file]:
+						hmm_dict[proteome][cog_file][sseqid] = record
 	print("Writing down sequences")
 	for cog_file in listdir_nohidden(cog_dir):
-		cog = cog_file.split(".faa")[0]
 		out_records = []
 		cog_path = cog_dir + cog_file
 		outpath = result_dir + cog_file
 		for record in SeqIO.parse(cog_path, "fasta"):
 			out_records.append(record)
 		for proteome in proteome_set:
-			proteome_cog_dict = hmm_dict[proteome][cog]
+			proteome_cog_dict = hmm_dict[proteome][cog_file]
 			for sseqid in proteome_cog_dict:
 				out_records.append(proteome_cog_dict[sseqid])
 		SeqIO.write(out_records, outpath, "fasta")
 	return 0
 
-hmm_report_dir = "/user/work/vl18625/euk/nina_markers/all_markers/hmmresults_eukprot/"
+hmm_report_dir = "/user/work/vl18625/euk/nina_markers/all_markers/test/"
 proteome_dir = "/user/work/vl18625/euk/eukprot/proteins/"
 cog_dir = "/user/work/vl18625/euk/nina_markers/all_markers/faa/"
 result_dir = "/user/work/vl18625/euk/nina_markers/all_markers/faa_with_euks/"
