90e5e9fae809802ab1a26530df08385eed0da979
Anna Nenarokova <a.nenarokova@gmail.com>
2022-03-25 15:31:39 +0000

    ni

--- projects/euk/mafft_bmge_bp_array.sh
+++ projects/euk/mafft_bmge_bp_array.sh
@@ -4,9 +4,9 @@ module load apps/bmge/1.12 apps/mafft/7.429
 
 bmge="/sw/apps/BMGE-1.12/BMGE.jar"
 
-fasta_dir="/user/work/vl18625/euk/nina_markers/all_markers/faa_with_euks/"
-msa_dir="/user/work/vl18625/euk/nina_markers/all_markers/msa_w_euks/"
-trimmed_msa_dir="/user/work/vl18625/euk/nina_markers/all_markers/msa_w_euks_trimmed/"
+fasta_dir="/user/work/vl18625/euk/ed_markers/faa_with_euks/"
+msa_dir="/user/work/vl18625/euk/ed_markers/msa_with_euks/"
+trimmed_msa_dir="/user/work/vl18625/euk/ed_markers/msa_w_euks_trimmed/"
 
 cd $fasta_dir
 fasta=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
