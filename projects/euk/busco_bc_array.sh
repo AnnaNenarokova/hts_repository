7cf9900b18556199e1718c723e8e9e3e2ee422c4
Anna Nenarokova <anna@it035271.users.bris.ac.uk>
2022-03-31 16:12:45 +0100

    ni

--- projects/euk/busco_bc_array.sh
+++ projects/euk/busco_bc_array.sh
@@ -4,11 +4,14 @@ module add languages/anaconda3/3.7
 source activate busco
 
 indir="/user/work/vl18625/eukprot/eukprot2_proteins/"
-outdir="/user/work/vl18625/eukprot/eukprot2_proteins_busco/"
 cd $indir
-
 fasta=$(ls *.fasta | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
 echo $fasta
 
-busco -i $fasta -l eukaryota_odb10 -o $outdir -m proteins -f
+fasta_path=$indir$fasta
+
+outdir="/user/work/vl18625/eukprot/eukprot2_proteins_busco/"
+
+cd $outdir
+busco -i $fasta_path -l eukaryota_odb10 -o $fasta -m proteins -f
