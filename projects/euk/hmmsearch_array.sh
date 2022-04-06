a1f361071828c6cdb7cf91d32d54be2b4a865cb0
Anna Nenarokova <anna@it035271.users.bris.ac.uk>
2022-04-06 12:50:06 +0100

    ni

--- projects/euk/hmmsearch_array.sh
+++ projects/euk/hmmsearch_array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 module load apps/hmmer/3.3.2
-cog_hmm_dir="/user/work/vl18625/euk/ed_markers/hmm_filtered/"
-prot_dir="/user/work/vl18625/euk/eukprot/proteins/"
-out_dir="/user/work/vl18625/euk/ed_markers/hmm_results_all_euks/"
+cog_hmm_dir="/user/work/vl18625/euk/nina_markers/all_markers/hmm_profiles/"
+prot_dir="/user/work/vl18625/euk/eukprot/anna_eukprot3_proteome_dataset/"
+out_dir="/user/work/vl18625/euk/nina_markers/anna_set_results/hmm_results/"
 e_threshold="0.00001"
 
 cd $prot_dir
