542f97864739961afe663ea948105eb0dfa7f10c
Anna Nenarokova <a.nenarokova@gmail.com>
2022-04-07 13:33:16 +0100

    ni

--- projects/euk/array_iqtrees_bp.sh
+++ projects/euk/array_iqtrees_bp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=iqtree_array
+#SBATCH --job-name=iqtree
 #SBATCH --partition=taw
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=14-00:0:0
-#SBATCH --mem=100G
-#SBATCH --array=1-11
+#SBATCH --mem=20G
+#SBATCH --array=1-25
 
 cd /user/home/vl18625/code/ngs/projects/euk/
-srun iqtree_array.sh
+srun iqtree_MFP_array.sh
