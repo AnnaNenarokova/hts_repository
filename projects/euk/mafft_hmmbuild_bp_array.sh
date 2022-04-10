441c8794bd49738536ab06b2b284e6ea673e4074
Anna Nenarokova <a.nenarokova@gmail.com>
2022-04-10 23:13:08 +0100

    ni

--- projects/euk/mafft_hmmbuild_bp_array.sh
+++ projects/euk/mafft_hmmbuild_bp_array.sh
@@ -4,9 +4,9 @@ module load apps/mafft/7.429 apps/hmmer/3.3.2
 
 bmge="/sw/apps/BMGE-1.12/BMGE.jar"
 
-fasta_dir="/user/work/vl18625/euk/nina_markers/faa/"
-msa_dir="/user/work/vl18625/euk/nina_markers/msa/"
-hmm_dir="/user/work/vl18625/euk/nina_markers/hmm_profiles/"
+fasta_dir="/user/work/vl18625/euk/ed_markers/anna_set_results/monobranch_results/fasta/"
+msa_dir="/user/work/vl18625/euk/ed_markers/anna_set_results/monobranch_results/msa/"
+hmm_dir="/user/work/vl18625/euk/ed_markers/anna_set_results/monobranch_results/hmm_profiles/"
 
 cd $fasta_dir
 fasta=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
