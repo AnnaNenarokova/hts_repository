53a095ac474940aa495fbb4be63dffd1450c2560
Anna Nenarokova <a.nenarokova@gmail.com>
2022-05-13 12:19:54 +0100

    ni

--- projects/euk/iqtree_concat_mellifera.sh
+++ projects/euk/iqtree_concat_mellifera.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=40
-#SBATCH --mem=100G
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=50G
 
 fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/nina_markers/ae_sets_only_euks/iqtree_linsi_bmge_trimal75_concatenated/ae_only_euk_linsi_bmge_trimal_75_concatenated.faa"
-iqtree2 -s $fasta -m LG+G+F -B 1000 -nt 40
+iqtree2 -s $fasta -m LG+G+F -B 1000 -nt 20
