03b5f83ccb0b7d96592cf960e66ab16ab11b6830
Anna Nenarokova <anna@it035271.users.bris.ac.uk>
2022-05-16 14:55:23 +0100

    ni

--- projects/euk/mafft_hmmsearch_many_mellifera.sh
+++ projects/euk/mafft_hmmsearch_many_mellifera.sh
@@ -36,7 +36,7 @@ e_threshold="0.00001"
 cd $subject_dir
 for subject in *.fasta
 do
-	subject_path=subject_dir$subject
+	subject_path=$subject_dir$subject
 	result=$hmm_results_dir$fasta$subject".txt"
 	echo $result
 	hmmsearch -E $e_threshold --tblout $result $hmm_file $subject_path
