774762c4e5c0693a2278eabd1f078c7f64ecad15
Anna Nenarokova <anna@it035271.users.bris.ac.uk>
2022-05-16 14:44:23 +0100

    ni

---
+++ projects/euk/mafft_hmmsearch_many_mellifera.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name=hmmsearch
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/hmmsearch_%A_%a.out
+#SBATCH --partition=high
+#SBATCH --time=7-12:00:00
+#SBATCH --array=1-119
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1GB
+##SBATCH --nodes=1
+## --cpu_bind=v,threads
+
+hmmbuild="/mnt/alvarium2pool/scratch/nenarokova/tools/hmmer-3.3.2/bin/hmmbuild"
+hmmsearch="/mnt/alvarium2pool/scratch/nenarokova/tools/hmmer-3.3.2/bin/hmmsearch"
+
+fasta_dir="/scratch/nenarokova/euk/markers/bacteria/alphaproteo_markers/faa/"
+msa_dir="/scratch/nenarokova/euk/markers/bacteria/alphaproteo_markers/msa/"
+hmm_dir="/scratch/nenarokova/euk/markers/bacteria/alphaproteo_markers/hmm/"
+
+subject_dir="/scratch/nenarokova/euk/proteomes/anna_set_16_05_22/"
+hmm_results_dir="/scratch/nenarokova/euk/markers/be_mono_results/alpha/hmm_results/"
+
+cd $fasta_dir
+fasta=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
+
+echo $fasta
+
+msa=$msa_dir$fasta
+hmm_file=$hmm_dir$fasta".hmm"
+
+mafft --anysymbol $fasta > $msa
+$hmmbuild $hmm_file $msa
+
+e_threshold="0.00001"
+
+cd $subject_dir
+for subject in *.fasta
+	result=$hmm_result_dir$fasta$subject".txt"
+	echo $result
+	hmmsearch -E $e_threshold --tblout $result $hmm_file $subject_path
---
+++ projects/euk/replace_sp_delimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import os
+from Bio import SeqIO
+
+def listdir_nohidden(path):
+	for f in listdir(path):
+		if not f.startswith('.'):
+			yield f
+
+def replace_species_delimeter(infasta, outfasta, old_delimeter="_", new_delimeter="-"):
+	out_records = []
+	for record in SeqIO.parse(infasta, "fasta"):
+		old_id_split = record.id.split(old_delimeter)
+		new_id = new_delimeter.join(old_id_split)
+		record.id = new_id
+		out_records.append(record)
+	SeqIO.write(out_records, outfasta, "fasta")
+	return 0
+
+def replace_species_delimeter_dir(indir, outdir):
+	for fasta in listdir_nohidden(indir):
+		infasta = indir + fasta
+		outfasta = outdir + fasta
+		print (fasta)
+		replace_species_delimeter(infasta, outfasta)
+	return 0
+
+indir="/Users/anna/work/euk_local/eukprot/anna_eukprot3_set/"
+outdir="/Users/anna/work/euk_local/eukprot/anna_set_16_05_22/"
+replace_species_delimeter_dir(indir, outdir)
