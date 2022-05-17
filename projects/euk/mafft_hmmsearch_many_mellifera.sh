a5ea811905a4a0052d0c3be3f73d5f0ea48a5c8c
Anna Nenarokova <anna@it035271.users.bris.ac.uk>
2022-05-17 10:55:32 +0100

    ni

--- projects/euk/mafft_hmmsearch_many_mellifera.sh
+++ projects/euk/mafft_hmmsearch_many_mellifera.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/scratch/nenarokova/code/slurm_out/hmmsearch_%A_%a.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
-#SBATCH --array=1-119
+#SBATCH --array=1-102
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1GB
@@ -13,12 +13,12 @@
 hmmbuild="/mnt/alvarium2pool/scratch/nenarokova/tools/hmmer-3.3.2/bin/hmmbuild"
 hmmsearch="/mnt/alvarium2pool/scratch/nenarokova/tools/hmmer-3.3.2/bin/hmmsearch"
 
-fasta_dir="/scratch/nenarokova/euk/markers/bacteria/alphaproteo_markers/faa/"
-msa_dir="/scratch/nenarokova/euk/markers/bacteria/alphaproteo_markers/msa/"
-hmm_dir="/scratch/nenarokova/euk/markers/bacteria/alphaproteo_markers/hmm/"
+fasta_dir="/scratch/nenarokova/euk/markers/bacteria/cyano_markers/faa/"
+msa_dir="/scratch/nenarokova/euk/markers/bacteria/cyano_markers/msa/"
+hmm_dir="/scratch/nenarokova/euk/markers/bacteria/cyano_markers/hmm/"
 
 subject_dir="/scratch/nenarokova/euk/proteomes/anna_set_16_05_22/"
-hmm_results_dir="/scratch/nenarokova/euk/markers/be_mono_results/alpha/hmm_results/"
+hmm_results_dir="/scratch/nenarokova/euk/markers/be_mono_results/cyano/hmm_results/"
 
 cd $fasta_dir
 fasta=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
