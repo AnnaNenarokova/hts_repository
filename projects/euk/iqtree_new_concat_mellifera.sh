71230fcdb72b4934e56078974c2551a8f0619d71
Anna Nenarokova <a.nenarokova@gmail.com>
2022-05-19 17:57:11 +0100

    ni

--- projects/euk/iqtree_new_concat_mellifera.sh
+++ projects/euk/iqtree_new_concat_mellifera.sh
@@ -9,4 +9,4 @@
 #SBATCH --mem=20G
 
 fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/archaea/ae_set_concat/65_markers_filtered_LG_G/ae_65_markers_concat.fasta"
-iqtree2 -s $fasta -m LG+G -B 100 -nt 20
+iqtree2 -s $fasta -m LG+G -nt 20
