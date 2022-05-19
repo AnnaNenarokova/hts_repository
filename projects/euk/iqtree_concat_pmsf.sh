70ba12af53e1bb90d5f9dca376996f43e3efde15
Anna Nenarokova <a.nenarokova@gmail.com>
2022-05-19 21:47:42 +0100

    ni

--- projects/euk/iqtree_concat_pmsf.sh
+++ projects/euk/iqtree_concat_pmsf.sh
@@ -11,4 +11,4 @@
 msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/archaea/ae_set_concat/65_markers_filtered_LG_C60_PMSF/ae_65_markers_concat.fasta"
 lg_tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/archaea/ae_set_concat/65_markers_filtered_LG_G/ae_65_markers_concat.fasta.treefile"
 
-iqtree2 -s $fasta -m LG+C60+F+G -ft lg_tree -B 1000 -nt 40
+iqtree2 -s $msa -m LG+C60+F+G -ft lg_tree -B 1000 -nt 40
