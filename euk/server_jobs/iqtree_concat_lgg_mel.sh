37a331952c3f8adf69e8d40b9a4ef9ac028f9763
Anna Nenarokova <a.nenarokova@gmail.com>
2022-05-23 18:19:05 +0100

    ni

--- euk/server_jobs/iqtree_concat_lgg_mel.sh
+++ euk/server_jobs/iqtree_concat_lgg_mel.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=10G
 
-fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/ae_set_concat/68_all_filtered_trimal85_LG_G/final_68ae_all_filtered_trimal85_concat.fasta"
-iqtree2 -s $fasta -m LG+G -B 1000 -nt 40
+fasta="/scratch/nenarokova/euk/markers/ae/ae_set_concat/only_euks/68final_only_euks_lgg/"
+iqtree2 -s $fasta -m LG+G -B 1000 -nt 20
--- euk/tools/catfasta2phyml.sh
+++ euk/tools/catfasta2phyml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 catfasta2phyml=/Users/vl18625/work/tools/catfasta2phyml/catfasta2phyml.pl
-workdir="/Users/vl18625/work/euk/markers_euks/nina_markers/ae/final_ae_sets/final_68ae_all_filtered/linsi_bmge_trimal85_for_concat/"
-outfile="/Users/vl18625/work/euk/markers_euks/nina_markers/ae/final_ae_sets/final_68ae_all_filtered/final_68ae_all_filtered_trimal85_concat.fasta"
+workdir="/Users/vl18625/work/euk/markers_euks/nina_markers/ae/final_ae_sets/final_68ae_all_filtered/only_euks/linsi_bmge_for_concat/"
+outfile="/Users/vl18625/work/euk/markers_euks/nina_markers/ae/final_ae_sets/final_68ae_all_filtered/final_68ae_only_euks_concat.fasta"
 cd $workdir
 
 $catfasta2phyml --concatenate --verbose -f *.faa > $outfile
