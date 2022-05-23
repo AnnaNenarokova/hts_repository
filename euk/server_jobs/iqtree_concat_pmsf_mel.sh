bdf3069a6271bbbe1df20509d3f7779a778bcfcf
Anna Nenarokova <a.nenarokova@gmail.com>
2022-05-23 08:14:05 +0100

    ni

---
+++ euk/server_jobs/iqtree_concat_pmsf_mel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=concat_ae_tree
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/concat_ae_tree_%A.out
+#SBATCH --partition=high
+#SBATCH --time=7-12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=100G
+
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/ae_set_concat/68_final/68_all_filtered_trimal85_c60_pmsf/final_68ae_all_filtered_trimal85_concat.fasta"
+tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/ae_set_concat/68_final/68_all_filtered_trimal85_LG_G/final_68ae_all_filtered_trimal85_concat.fasta.treefile"
+iqtree2 -s $msa -m LG+C60+F+G -ft $tree -b 1000 -nt 40
