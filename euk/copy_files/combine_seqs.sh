afa70521ab73ba15470c786b0b3d496a35c77494
Anna Nenarokova <a.nenarokova@gmail.com>
2022-07-10 21:11:26 +0100

    ni

--- euk/copy_files/combine_seqs.sh
+++ euk/copy_files/combine_seqs.sh
@@ -3,7 +3,6 @@
 outdir="/Users/anna/work/euk_local/nina_markers/ABE/5_markers/faa/euk_all/"
 for f in *.faa
 do
-<<<<<<< HEAD
 	echo $f
 	outfile=$outdir$f
 	cat $f >> $outfile
@@ -17,9 +16,4 @@ do
  infile=$indir$f
  outfile=$outdir$f
  cat $infile >> $outfile
-=======
- echo $f
- outfile=$outdir$f
- cat $f >> $outfile
->>>>>>> d332b1b8576d7c006cd73c2a738af8f8b3d2ea5b
 done
