42c391f8c89aa95a26ce521ffb1e8338cff9422e
Anna Nenarokova <a.nenarokova@gmail.com>
2022-08-25 22:55:52 +0100

    ni

--- euk/server_jobs/iqtree_concat_lgg_mel.sh
+++ euk/server_jobs/iqtree_concat_lgg_mel.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=40
 #SBATCH --mem=10G
 
 fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/be/alpha/concat/be_c20_filtered_58_markers_concat.fasta"
