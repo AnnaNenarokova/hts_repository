19126537410667d480d789f4434a757b940946e6
Anna Nenarokova <a.nenarokova@gmail.com>
2022-08-27 08:25:23 +0100

    ni

--- euk/server_jobs/iqtree_concat_pmsf_mel.sh
+++ euk/server_jobs/iqtree_concat_pmsf_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=concat_be_alpha_tree
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/concat_AE_tree_c60_pmsf_%A.out
+#SBATCH --job-name=concat_BE_alpha_tree
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/concat_BE_tree_c60_pmsf_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
@@ -8,6 +8,6 @@
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=250G
 
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/65_ae_markers_euk_filtered_23_08_22/c60_lg_f_g_pmsf/euk_65_markers_all_filtered_concat.fasta"
-tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/65_ae_markers_euk_filtered_23_08_22/lgg/euk_65_markers_all_filtered_concat.fasta.treefile"
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/be/alpha/concat/c20_initial_58/c60_pmsf_lgg/be_c20_filtered_58_markers_concat.fasta"
+tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/be/alpha/concat/c20_initial_58/lgg/be_c20_filtered_58_markers_concat.fasta.treefile"
 iqtree2 -s $msa -m LG+C60+F+G -ft $tree -b 1000 -nt 40
