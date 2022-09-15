4beb93243ef56664b140af231c4b9a341e6a4fbf
Anna Nenarokova <a.nenarokova@gmail.com>
2022-09-15 09:10:42 +0100

    ni

--- euk/server_jobs/iqtree_concat_pmsf_mel.sh
+++ euk/server_jobs/iqtree_concat_pmsf_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=c60_lgg_AE_pmsf
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/c60_lgg_pmsf_AE_concat_tree_%A.out
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/c60_lgg_AUTO_pmsf_AE_concat_tree_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
@@ -10,4 +10,4 @@
 
 msa="/scratch/nenarokova/euk/markers/ae/65_ae_markers_euk_filtered_23_08_22/c60_lgg_pmsf/euk_65_markers_all_filtered_concat.fasta"
 tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/65_ae_markers_euk_filtered_23_08_22/lgg/euk_65_markers_all_filtered_concat.fasta.treefile"
-iqtree2 -s $msa -m LG+C60+G -ft $tree -B 1000 -T 1 -T AUTO --threads-max 48
+iqtree2 -s $msa -m LG+C60+G -ft $tree -B 1000 -nt AUTO -ntmax 48
