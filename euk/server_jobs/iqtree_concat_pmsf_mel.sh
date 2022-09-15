68f098a441dd72141e69a3991a8ec62f71d7f4a4
Anna Nenarokova <a.nenarokova@gmail.com>
2022-09-15 09:14:58 +0100

    ni

--- euk/server_jobs/iqtree_concat_pmsf_mel.sh
+++ euk/server_jobs/iqtree_concat_pmsf_mel.sh
@@ -10,4 +10,4 @@
 
 msa="/scratch/nenarokova/euk/markers/ae/65_ae_markers_euk_filtered_23_08_22/c60_lgg_pmsf/euk_65_markers_all_filtered_concat.fasta"
 tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/65_ae_markers_euk_filtered_23_08_22/lgg/euk_65_markers_all_filtered_concat.fasta.treefile"
-iqtree2 -s $msa -m LG+C60+G -ft $tree -B 1000 -nt AUTO -ntmax 20
+iqtree2 -s $msa -m LG+C60+G -ft $tree -B 1000 -nt 20
