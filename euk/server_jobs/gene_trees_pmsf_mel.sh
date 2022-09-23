fe0381186b464cec69cc21cf3427b8c0813db47c
Anna Nenarokova <a.nenarokova@gmail.com>
2022-09-23 17:52:30 +0100

    ni

--- euk/server_jobs/gene_trees_pmsf_mel.sh
+++ euk/server_jobs/gene_trees_pmsf_mel.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=pmsf_ae_sgt
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/pmsf_ae_sgt_%A_%a.out
+#SBATCH --job-name=abe_pmsf_sgt
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/abe_pmsf_sgt_%A_%a.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
-#SBATCH --array=1-115
+#SBATCH --array=1-26
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=5GB
 ##SBATCH --nodes=1
 ## --cpu_bind=v,threads
 
-workdir="/scratch/nenarokova/euk/markers/ae/final_68ae_all_filtered/"
+workdir="/scratch/nenarokova/euk/markers/abe/26_markers/"
 
 msa_dir=$workdir"linsi_bmge/"
-lgg_dir=$workdir"iqtree_lgg/"
+lgg_dir=$workdir"lgg_trees/"
 
 cd $msa_dir
 name=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
@@ -22,4 +22,4 @@ echo $name
 msa=$msa_dir$name
 tree=$lgg_dir$name".treefile"
 
-iqtree2 -s $msa -m LG+C20+F+G -ft $tree -B 1000 -nt 1
+iqtree2 -s $msa -m LG+C20+G -ft $tree -B 1000 -nt 1
