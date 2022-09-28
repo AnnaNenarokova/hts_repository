955c9780f336f7d4030a5001e71ecc4d5e3cbea0
Anna Nenarokova <a.nenarokova@gmail.com>
2022-09-28 11:09:41 +0100

    ni

--- euk/server_jobs/iqtree_test_models3_mel.sh
+++ euk/server_jobs/iqtree_test_models3_mel.sh
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=250G
 
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/65_ae_markers_euk_filtered_23_08_22/model_testing_3/euk_65_markers_all_filtered_concat.fasta"
-models="LG+R+F,LG+R,LG+G+F,LG+C60,LG+C60+G,LG+C60+F,LG+C60+G+F,LG+C60+R,LG+C60+R+F"
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/65_ae_markers_euk_filtered_23_08_22/model_testing/model_testing_4/euk_65_markers_all_filtered_concat.fasta"
+models="LG+R+F,LG+R,LG+G+F,LG+C60,LG+C60+G,LG+C60+F,LG+C60+G+F"
 
 iqtree2 -s $msa  -T 8 -m MF -mset $models --score-diff all -mem 250G
 
