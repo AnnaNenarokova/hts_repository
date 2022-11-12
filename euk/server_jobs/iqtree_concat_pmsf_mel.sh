49edf4fa2b60999a83936b9d487725486addb916
Anna Nenarokova <a.nenarokova@gmail.com>
2022-11-12 13:51:41 +0000

    ni

--- euk/server_jobs/iqtree_concat_pmsf_mel.sh
+++ euk/server_jobs/iqtree_concat_pmsf_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=abce_c60_lgg_pmsf
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/abce_c60_lgg_pmsf_tree_%A.out
+#SBATCH --job-name=abce_c20_lgg_pmsf
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/abce_c20_lgg_pmsf_tree_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
@@ -8,6 +8,6 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=250G
 
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/abce_94_markers_concat/c60_pmsf/abce_94_markers_concat.fasta"
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/abce_94_markers_concat/c20_pmsf/abce_94_markers_concat.fasta"
 tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/abce_94_markers_concat/lgg/abce_94_markers_concat.fasta.treefile"
-iqtree2 -s $msa -m LG+C60+G -ft $tree -B 1000 -nt 20
+iqtree2 -s $msa -m LG+C20+G -ft $tree -B 1000 -nt 20
