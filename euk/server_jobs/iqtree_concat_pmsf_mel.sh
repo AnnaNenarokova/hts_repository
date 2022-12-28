50b710976990f53901aabcbb13d35d20e8d39b8d
Anna Nenarokova <a.nenarokova@gmail.com>
2022-12-28 21:55:34 +0000

    ni

--- euk/server_jobs/iqtree_concat_pmsf_mel.sh
+++ euk/server_jobs/iqtree_concat_pmsf_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=brett_16_markers_lgg_pmsf
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/brett_16_markers_lgg_pmsf_%A.out
+#SBATCH --job-name=34_ae_asgard_only_c60_pmsf
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/34_ae_asgard_only_c60_pmsf_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
@@ -8,6 +8,6 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=85G
 
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/be/alpha/brett_16markers/concat_16_markers/no_anaerobs/lgg_c60_pmsf/16_markers_brett_concat_no_anaerobs.fasta"
-tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/be/alpha/brett_16markers/concat_16_markers/no_anaerobs/lgg/16_markers_brett_concat_no_anaerobs.fasta.treefile"
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/only_asgard/34_ae_asgard_concat/c60_pmsf/34_ae_asgard_only_concat.faa"
+tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/only_asgard/34_ae_asgard_concat/lgg/34_ae_asgard_only_concat.faa.treefile"
 iqtree2 -s $msa -m LG+C60+G -ft $tree -B 1000 -nt 20
