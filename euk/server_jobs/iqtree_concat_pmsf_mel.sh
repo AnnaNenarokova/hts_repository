14e809b55f21a3f94a78a9532512082429111451
Anna Nenarokova <a.nenarokova@gmail.com>
2023-01-09 13:36:16 +0000

    ni

--- euk/server_jobs/iqtree_concat_pmsf_mel.sh
+++ euk/server_jobs/iqtree_concat_pmsf_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=abce_94_no_kor_c30_pmsf
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/abce_94_no_kor_c30_pmsf_%A.out
+#SBATCH --job-name=abce_94_no_kor_c20_pmsf
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/abce_94_no_kor_c20_pmsf_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
@@ -10,4 +10,4 @@
 
 msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/abce_94_markers_concat/filtered/lgg_c30_pmsf/abce_94_markers_no_kor_filtered.fasta"
 tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/abce_94_markers_concat/filtered/lgg/abce_94_markers_no_kor_filtered.fasta.treefile"
-iqtree2 -s $msa -m LG+C30+G -ft $tree -B 1000 -nt 20
+iqtree2 -s $msa -m LG+C20+G -ft $tree -B 1000 -nt 20
