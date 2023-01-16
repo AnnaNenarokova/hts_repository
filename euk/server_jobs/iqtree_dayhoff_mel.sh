709eebcb272622b3518ac3da96ae1515f0c216b0
Anna Nenarokova <a.nenarokova@gmail.com>
2023-01-16 15:21:36 +0000

    ni

--- euk/server_jobs/iqtree_dayhoff_mel.sh
+++ euk/server_jobs/iqtree_dayhoff_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=recoded_abce_94_markers
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/recoded_abce_94_markers_c60_%A.out
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/recoded_abce_94_markers_gtr_g_%A.out
 #SBATCH --partition=high
 #SBATCH --time=99-12:00:00
 #SBATCH --nodes=1
@@ -8,6 +8,6 @@
 #SBATCH --cpus-per-task=48
 #SBATCH --mem=250G
 
-msa=""
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/abce_94_markers_concat/recoded_msa/dayhoff4/gtr_g/abce_94_markers_16_01_dayhoff4_recoded.fasta"
 
 iqtree2 -s $fasta -m GTR+G -B 1000 -T AUTO --threads-max 48
