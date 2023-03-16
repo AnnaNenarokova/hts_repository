3c73fd43bd6a37b64ed133c8c89b11a03f9dc380
Anna Nenarokova <a.nenarokova@gmail.com>
2023-03-16 12:55:55 +0000

    ni

--- euk/server_jobs/iqtree_concat_c60_mel.sh
+++ euk/server_jobs/iqtree_concat_c60_mel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=104sp_no_pmsf_c60
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/104sp_no_pmsf_c60_%A.out
+#SBATCH --job-name=68ae_c60_no_pmsf
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/68ae_c60_no_pmsf_%A.out
 #SBATCH --partition=high
 #SBATCH --time=99-99:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=48
-#SBATCH --mem=150G
+#SBATCH --mem=250G
 
-fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/abce_94m_104_species_concat/c60_no_pmsf/104species_94markers_abce.fasta"
+fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/final_68ae_filtered_concat/lgg_c60/68_final_ae.fasta"
 iqtree2 -s $fasta -m LG+C60+G -B 1000 --threads-max 48 -T AUTO
