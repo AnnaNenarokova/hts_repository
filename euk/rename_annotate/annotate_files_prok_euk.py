6cb6d04e97f9bd1000d28bc35c6babd77b58e90f
Anna Nenarokova <a.nenarokova@gmail.com>
2023-04-06 11:55:20 +0100

    ni

--- euk/rename_annotate/annotate_files_prok_euk.py
+++ euk/rename_annotate/annotate_files_prok_euk.py
@@ -396,8 +396,8 @@ out_tree_path="/Users/vl18625/work/euk/concat_trees/abce/annotated_trees/104spec
 alignment_path="/Users/vl18625/work/euk/concat_trees/alignments/104_species_abce_concat.fasta"
 # write_tree_tax_info(in_tree_path,out_tree_path,tax_info_path,key_name="taxonomy",abce=True,alignment_path=alignment_path)
 
-in_tree_path="/Users/vl18625/work/euk/concat_trees/other_trees/trees/68_final_ae_с60.treefile"
-out_tree_path="/Users/vl18625/work/euk/concat_trees/other_trees/annotated_trees/68_final_ae_с60.treefile"
+in_tree_path="/Users/vl18625/work/euk/concat_trees/other_trees/trees/68_final_ae_heimdall_constraint.tree"
+out_tree_path="/Users/vl18625/work/euk/concat_trees/other_trees/annotated_trees/68_final_ae_heimdall_constraint.tree"
 alignment_path="/Users/vl18625/work/euk/concat_trees/alignments/68_final_ae.fasta"
 write_tree_tax_info(in_tree_path,out_tree_path,tax_info_path,key_name="taxonomy",abce=False,alignment_path=alignment_path)
 
