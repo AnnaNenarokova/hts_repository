6e29199867fffb9a8e543560c1ad3e6159ec1c59
Anna Nenarokova <a.nenarokova@gmail.com>
2023-04-06 13:07:16 +0100

    ni

--- euk/server_jobs/au_test_iqtree.sh
+++ euk/server_jobs/au_test_iqtree.sh
@@ -8,6 +8,6 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=50G
 
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/one_hit/final_68ae_filtered_concat/au_test_constraint_heimdall_lgg/68_final_ae.fasta"
-trees_file="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/one_hit/final_68ae_filtered_concat/au_test_constraint_heimdall_lgg/68_final_ae.trees"
-iqtree2 -s $msa -m LG+G -z $trees_file -T 10 -n 0 -zb 1000 -au
+msa="/scratch/nenarokova/euk/markers/ae/one_hit/final_68ae_filtered_concat/au_tests_constraint_hemidall/lgg_20000/68_final_ae.fasta"
+trees_file="/scratch/nenarokova/euk/markers/ae/one_hit/final_68ae_filtered_concat/au_tests_constraint_hemidall/lgg_20000/68_final_ae.trees"
+iqtree2 -s $msa -m LG+G -z $trees_file -T 10 -n 0 -zb 20000 -au
