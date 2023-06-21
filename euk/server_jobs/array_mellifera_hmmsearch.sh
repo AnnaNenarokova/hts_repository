1d94f5149f17f3bd2e96fe65b94b898e96346f93
Anna Nenarokova <a.nenarokova@gmail.com>
2023-06-21 09:29:44 +0100

    ni

--- euk/server_jobs/array_mellifera_hmmsearch.sh
+++ euk/server_jobs/array_mellifera_hmmsearch.sh
@@ -13,7 +13,6 @@
 hmmsearch="/mnt/alvarium2pool/scratch/nenarokova/tools/hmmer-3.3.2/bin/hmmsearch"
 
 hmm_dir="/scratch/nenarokova/euk/markers/archaea/hmm/"
-
 subject_dir="/scratch/nenarokova/euk/proteomes/anna_eukprot3_v3_21_06_23/"
 hmm_results_dir="/scratch/nenarokova/euk/hmm_results/eukprot3_v3_21_06_23_abc/archaea/"
 
