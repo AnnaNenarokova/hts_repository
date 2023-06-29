578ac7271ffef1585053e7969e3959f83d2bcf9c
Anna Nenarokova <a.nenarokova@gmail.com>
2023-06-29 08:56:41 +0100

    ni

--- euk/rename_annotate/rename_fastas_to_concat.py
+++ euk/rename_annotate/rename_fastas_to_concat.py
@@ -31,6 +31,6 @@ def rename_fastas_to_concat(indir, outdir):
 		rename_fasta_to_concat(infasta, outfasta)
 	return outdir
 
-indir="/Users/vl18625/work/euk/markers_euks/nina_markers/ae/68_final_ae_markers/linsi_bmge/"
-outdir="/Users/vl18625/work/euk/markers_euks/nina_markers/ae/68_final_ae_markers/faa_renamed_concat/"
+indir="/Users/vl18625/work/euk/markers_euks/nina_markers/abe/87_markers_29_06_23/linsi_bmge_filtered_ABaE/"
+outdir="/Users/vl18625/work/euk/markers_euks/nina_markers/abe/87_markers_29_06_23/linsi_bmge_filtered_ABaE_for_concat/"
 rename_fastas_to_concat(indir, outdir)
--- euk/server_jobs/iqtree_concat_lgg_mel.sh
+++ euk/server_jobs/iqtree_concat_lgg_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=68_final_ae_lgg
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/68_final_ae_lgg_%A.out
+#SBATCH --job-name=ABaE_lgg
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/ABaE_lgg_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
@@ -8,5 +8,5 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=50G
 
-fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/final_68ae_filtered_concat/lgg/68_final_ae.fasta"
+fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/abAe/87_markers_concat/lgg/87_markers_filtered_ABaE_concat.fasta"
 iqtree2 -s $fasta -m LG+G -B 1000 -T AUTO --threads-max 20
