0be1f0794995c6e44d2f24fd24d142544ec36c3a
Anna Nenarokova <a.nenarokova@gmail.com>
2023-07-01 08:58:57 +0100

    ni

--- euk/server_jobs/array_mellifera_hmmsearch.sh
+++ euk/server_jobs/array_mellifera_hmmsearch.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/scratch/nenarokova/code/slurm_out/alpha_hmm_%A_%a.out
 #SBATCH --partition=med
 #SBATCH --time=7-12:00:00
-#SBATCH --array=1-114
+#SBATCH --array=1-102
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1GB
@@ -13,8 +13,8 @@
 hmmsearch="/mnt/alvarium2pool/scratch/nenarokova/tools/hmmer-3.3.2/bin/hmmsearch"
 
 subject_dir="/scratch/nenarokova/euk/proteomes/anna_eukprot3_v2_21_06_23/"
-hmm_dir="/scratch/nenarokova/euk/markers/bacteria/alphaproteo_markers/hmm/"
-hmm_results_dir="/scratch/nenarokova/euk/hmm_results/eukprot3_v3_21_06_23_abc/alpha/"
+hmm_dir="/scratch/nenarokova/euk/markers/bacteria/cyano_markers/hmm/"
+hmm_results_dir="/scratch/nenarokova/euk/hmm_results/eukprot3_v3_21_06_23_abc/cyano/"
 
 cd $hmm_dir
 hmm_file=$(ls *.hmm | sed -n ${SLURM_ARRAY_TASK_ID}p)
