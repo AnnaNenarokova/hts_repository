90c3a1c1395a36d78596ac2a08b87b71c10be9de
Anna Nenarokova <a.nenarokova@gmail.com>
2023-07-02 09:35:22 +0100

    ni

--- euk/server_jobs/prepare_hmm_hit_fasta_job_mel.sh
+++ euk/server_jobs/prepare_hmm_hit_fasta_job_mel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=archaea_fastas
+#SBATCH --job-name=AE_fastas
 #SBATCH --partition=high
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=14-00:00:00
 #SBATCH --mem=50G
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/archaea_fastas_%A_%a.out
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/AE_fastas_%A_%a.out
 
 cd /scratch/nenarokova/code/ngs/euk/hmm_results_scripts
 
