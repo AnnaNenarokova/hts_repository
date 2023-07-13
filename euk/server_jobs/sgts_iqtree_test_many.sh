35b73bd7eb14c7b5b749a364d0fe126e002d5796
Anna Nenarokova <a.nenarokova@gmail.com>
2023-07-13 09:45:03 +0100

    ni

--- euk/server_jobs/sgts_iqtree_test_many.sh
+++ euk/server_jobs/sgts_iqtree_test_many.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=be_sgts_test_c60
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/be_sgts_test_c60_%A_%a.out
+#SBATCH --job-name=ce_sgts_test_c60
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/ce_sgts_test_c60_%A_%a.out
 #SBATCH --partition=low
 #SBATCH --time=99-99:00:00
-#SBATCH --array=1-119
+#SBATCH --array=1-102
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=10GB
 ##SBATCH --nodes=1
 ## --cpu_bind=v,threads
 
-workdir="/scratch/nenarokova/euk/markers/be/one_hit/01_07_23/alpha/linsi_bmge/"
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/be/alpha/brett_16markers/concat_16_markers/model_testing/16_markers_brett_concat.fasta"
+workdir="/scratch/nenarokova/euk/markers/be/one_hit/01_07_23/cyano/linsi_bmge/"
 models="LG+C10+G,LG+C20+G,LG+C30+G,LG+C40+G,LG+C50+G,LG+C60+G"
 
 cd $workdir
