2aec2da1127bb34b086e3b549fe1e3cd0fb1de7a
Anna Nenarokova <a.nenarokova@gmail.com>
2023-08-07 11:26:07 +0100

    ni

--- euk/server_jobs/sgts_iqtree_best_model_bb.sh
+++ euk/server_jobs/sgts_iqtree_best_model_bb.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=ae_sgts_test_c60
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/ae_sgts_test_c60_%A_%a.out
+#SBATCH --job-name=be_sgts_best_model_bb
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/be_sgts_best_model_bb_%A_%a.out
 #SBATCH --partition=low
 #SBATCH --time=99-99:00:00
-#SBATCH --array=1-85
+#SBATCH --array=1-119
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=10GB
 ##SBATCH --nodes=1
 ## --cpu_bind=v,threads
 
-workdir="/scratch/nenarokova/euk/markers/ae/one_hit/01_07_23/no_idun/linsi_bmge/"
+workdir="/scratch/nenarokova/euk/markers/be/one_hit/01_07_23/alpha/linsi_bmge/"
 models="LG+C10+G,LG+C20+G,LG+C30+G,LG+C40+G,LG+C50+G,LG+C60+G"
 
 cd $workdir
 
 msa=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
-iqtree2 -s $msa -m MFP -madd $models --score-diff all
+iqtree2 -s $msa -m MFP -madd $models --score-diff all -B 1000
