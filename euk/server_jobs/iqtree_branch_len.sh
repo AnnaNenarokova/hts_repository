cd477ef65a3cc2af30334e155bcd29d602eed1dd
Anna Nenarokova <a.nenarokova@gmail.com>
2023-09-12 22:56:39 +0100

    ni

--- euk/server_jobs/iqtree_branch_len.sh
+++ euk/server_jobs/iqtree_branch_len.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=100G
+#SBATCH --mem=150G
 
 msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/one_hit/old_data/only_euks/117euks_132markers_concat/c60_lgg_only_len/117euks_132markers_concat.fasta"
 tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/one_hit/old_data/only_euks/117euks_132markers_concat/c60_lgg_only_len/only_euks_manual_only_ids.tree"
