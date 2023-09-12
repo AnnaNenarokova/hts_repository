4a4f05c5b4e1ea3fa6ec4619b686a3abdce65733
Anna Nenarokova <a.nenarokova@gmail.com>
2023-09-12 18:05:26 +0100

    ni

--- euk/server_jobs/iqtree_branch_len.sh
+++ euk/server_jobs/iqtree_branch_len.sh
@@ -6,8 +6,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=50G
+#SBATCH --mem=100G
 
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/toyset_benoit/short_names_2cyanos.fasta"
-tree="/mnt/alvarium2pool/scratch/nenarokova/euk/toyset_benoit/short_names_tree_2cyanos_calibrated.tree"
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/one_hit/old_data/only_euks/117euks_132markers_concat/c60_lgg_only_len/117euks_132markers_concat.fasta"
+tree="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/abe/one_hit/old_data/only_euks/117euks_132markers_concat/c60_lgg_only_len/only_euks_manual_only_ids.tree"
 iqtree2 -s $msa -te $tree -m C60+LG+G -T AUTO --threads-max 20
