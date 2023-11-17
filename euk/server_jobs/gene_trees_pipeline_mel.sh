406d6f010915929ed96b900943e3e3518df30ed9
Anna Nenarokova <a.nenarokova@gmail.com>
2023-11-17 14:43:01 +0000

    ni

--- euk/server_jobs/gene_trees_pipeline_mel.sh
+++ euk/server_jobs/gene_trees_pipeline_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=ab_idunn_sgts_new
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/ab_idunn_sgts_new_%A_%a.out
+#SBATCH --job-name=archaea_sgt
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/archaea_sgt_%A_%a.out
 #SBATCH --partition=high
 #SBATCH --time=99-99:00:00
 #SBATCH --array=1-94
@@ -10,11 +10,12 @@
 ##SBATCH --nodes=1
 ## --cpu_bind=v,threads
 
-workdir="/scratch/nenarokova/euk/markers/ab/ab_94markers/with_idunn/"
+workdir=""
 fasta_dir=$workdir"faa/"
 
 linsi_dir=$workdir"linsi/"
 trimmed_linsi_dir=$workdir"linsi_bmge/"
+tree_dir=$workdir"treefiles/"
 
 cd $fasta_dir
 
@@ -24,10 +25,12 @@ echo $fasta
 
 msa=$linsi_dir$fasta
 trimmed_msa=$trimmed_linsi_dir$fasta
+copy_trimmed_msa=$tree_dir$fasta
 
 linsi --anysymbol $fasta > $msa
 BMGE -i $msa -t "AA" -m BLOSUM30 -of $trimmed_msa
 
-cd $trimmed_linsi_dir
+cp $trimmed_msa $copy_trimmed_msa
+cd $tree_dir
 
 iqtree2 -s $fasta -m LG+G -B 1000 -nt 1 -redo
--- euk/tools/catfasta2phyml.sh
+++ euk/tools/catfasta2phyml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-catfasta2phyml=/Users/vl18625/work/tools/catfasta2phyml/catfasta2phyml.pl
-
+# catfasta2phyml='/Users/vl18625/work/tools/catfasta2phyml/catfasta2phyml.pl'
+catfasta2phyml='/mnt/alvarium2pool/scratch/nenarokova/tools/catfasta2phyml/catfasta2phyml.pl'
 workdir='/Users/vl18625/work/euk/markers_euks/nina_markers/ab_94_markers/no_idunn/linsi_bmge_renamed/'
 outfile='/Users/vl18625/work/euk/markers_euks/nina_markers/ab_94_markers/no_idunn/ab_94_markers_no_idunn_concat.fasta'
 cd $workdir
