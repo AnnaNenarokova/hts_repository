6cc7a995c3e4bdd823a1f991314ecb8708d378a7
Anna Nenarokova <a.nenarokova@gmail.com>
2023-11-17 23:01:37 +0000

    ni

--- euk/server_jobs/iqtree_concat_c60_mel.sh
+++ euk/server_jobs/iqtree_concat_c60_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=94ab_c60_no_pmsf
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/94ab_c60_no_pmsf_%A.out
+#SBATCH --job-name=85m_c60_Archaea
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/85m_c60_Archaea_%A.out
 #SBATCH --partition=high
 #SBATCH --time=99-99:00:00
 #SBATCH --nodes=1
@@ -8,5 +8,5 @@
 #SBATCH --cpus-per-task=48
 #SBATCH --mem=250G
 
-fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ab/ab_94markers/no_idunn_concat/c60_lgg/ab_94_markers_no_idunn_concat.fasta"
+fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/archaea/concat/lgg_c60/archaea_85_markers_concat.fasta"
 iqtree2 -s $fasta -m LG+C60+G -B 1000 --threads-max 48 -T AUTO
