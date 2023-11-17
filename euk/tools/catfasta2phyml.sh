3d16145ce726fe3ce041d0e736bb75ed4e5f61d9
Anna Nenarokova <a.nenarokova@gmail.com>
2023-11-17 22:51:19 +0000

    ni

--- euk/tools/catfasta2phyml.sh
+++ euk/tools/catfasta2phyml.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # catfasta2phyml='/Users/vl18625/work/tools/catfasta2phyml/catfasta2phyml.pl'
 catfasta2phyml='/mnt/alvarium2pool/scratch/nenarokova/tools/catfasta2phyml/catfasta2phyml.pl'
-workdir='/Users/vl18625/work/euk/markers_euks/nina_markers/ab_94_markers/no_idunn/linsi_bmge_renamed/'
-outfile='/Users/vl18625/work/euk/markers_euks/nina_markers/ab_94_markers/no_idunn/ab_94_markers_no_idunn_concat.fasta'
+workdir='/scratch/nenarokova/euk/markers/archaea/linsi_bmge/'
+outfile='/scratch/nenarokova/euk/markers/archaea/concat/archaea_85_markers_concat.fasta'
 cd $workdir
 
 $catfasta2phyml --concatenate --verbose -f *.faa > $outfile
