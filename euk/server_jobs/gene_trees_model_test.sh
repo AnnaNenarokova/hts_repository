a5bdb7932702dd9a79643b8ba6f63a86b9ca9679
Anna Nenarokova <a.nenarokova@gmail.com>
2023-11-20 13:52:03 +0000

    ni

--- euk/server_jobs/gene_trees_model_test.sh
+++ euk/server_jobs/gene_trees_model_test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=A_sgt_mtest
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/A_sgt_mtest_%A_%a.out
+#SBATCH --job-name=B_sgt_mtest
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/B_sgt_mtest_%A_%a.out
 #SBATCH --partition=high
 #SBATCH --time=99-99:00:00
-#SBATCH --array=1-85
+#SBATCH --array=1-115
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=10GB
 ##SBATCH --nodes=1
 ## --cpu_bind=v,threads
 
-workdir="/scratch/nenarokova/euk/markers/archaea/"
+workdir="/scratch/nenarokova/euk/markers/bacteria/all_bacteria_markers/"
 
 msa_dir=$workdir"linsi_bmge/"
 tree_dir=$workdir"model_test/"
