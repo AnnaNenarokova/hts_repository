c7407c57305818af899f4dc4beec3466bfbc3846
Anna Nenarokova <a.nenarokova@gmail.com>
2023-11-20 13:44:29 +0000

    ni

---
+++ euk/server_jobs/gene_trees_model_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=A_sgt_mtest
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/A_sgt_mtest_%A_%a.out
+#SBATCH --partition=high
+#SBATCH --time=99-99:00:00
+#SBATCH --array=1-85
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10GB
+##SBATCH --nodes=1
+## --cpu_bind=v,threads
+
+workdir="/scratch/nenarokova/euk/markers/archaea/"
+
+msa_dir=$workdir"linsi_bmge/"
+tree_dir=$workdir"model_test/"
+
+name=$(ls *.faa | sed -n ${SLURM_ARRAY_TASK_ID}p)
+
+echo $name
+msa=$msa_dir$name
+msa_copy=$tree_dir$name
+
+cp $msa $msa_copy
+
+models="LG+C10+G,LG+C20+G,LG+C30+G,LG+C40+G,LG+C50+G,LG+C60+G"
+
+iqtree2 -s $msa_copy -B 1000 -nt 1 -m MFP -madd $models --score-diff all
