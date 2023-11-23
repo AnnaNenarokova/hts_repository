f40d3f84be981713c80f8bf99c024dab0276de46
Anna Nenarokova <a.nenarokova@gmail.com>
2023-11-23 12:32:32 +0000

    ni

---
+++ euk/server_jobs/iqtree_concat_c30_mel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=c30_Bacteria
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/c30_Bacteria_%A.out
+#SBATCH --partition=high
+#SBATCH --time=99-99:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=250G
+
+fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/bacteria/all_bacteria_markers/concat/c30_lgg/bacteria_115_markers_concat.fasta"
+iqtree2 -s $fasta -m LG+C30+G -B 1000 --threads-max 20 -T AUTO
