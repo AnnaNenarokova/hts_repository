edc63dc543574706fb1cc0e40956abe05bda42ad
Anna Nenarokova <a.nenarokova@gmail.com>
2023-12-09 20:09:00 +0000

    ni

--- euk/server_jobs/iqtree_concat_lgg_mel.sh
+++ euk/server_jobs/iqtree_concat_lgg_mel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=CE_lgg
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/CE_lgg_%A.out
+#SBATCH --job-name=CE_lgg_filtered
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/CE_lgg_filtered_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
@@ -8,5 +8,5 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=10G
 
-fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/be/one_hit/01_07_23/cyano/concat/lgg/CE_102_markers_concat.fasta"
+fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/be/one_hit/01_07_23/ce_filtered/concat/lgg/CE_81_markers_concat.fasta"
 iqtree2 -s $fasta -m LG+G -B 1000 -T AUTO --threads-max 10
