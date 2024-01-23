519b1e278af5a85c9d7f0852fce81b23553153a3
Anna Nenarokova <a.nenarokova@gmail.com>
2024-01-23 16:44:42 +0000

    ni

--- euk/server_jobs/iqtree_branch_len.sh
+++ euk/server_jobs/iqtree_branch_len.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=250G
+#SBATCH --mem=15G
 msa="/scratch/nenarokova/euk/molecular_clock_datasets/aebece_1211_tips/lgg4f_23_01_24/abce_94_markers_concat_filtered.fasta"
 tree="/scratch/nenarokova/euk/molecular_clock_datasets/aebece_1211_tips/lgg4f_23_01_24/ABAEBECE_manual_only_ids_no_tabs_23_01_24.nwk"
 iqtree2 -s $msa -te $tree -m LG+F+G4 -T AUTO --threads-max 10
