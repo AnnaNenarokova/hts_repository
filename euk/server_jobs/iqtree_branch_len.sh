b1c79183a57d01059ba3d0ac42b90876ef181005
Anna Nenarokova <a.nenarokova@gmail.com>
2024-02-22 16:03:13 +0000

    ni

--- euk/server_jobs/iqtree_branch_len.sh
+++ euk/server_jobs/iqtree_branch_len.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=baldauf_alt_lengths
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/baldauf_alt_len_%A.out
+#SBATCH --job-name=frank_bfixed
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/frank_bfixed_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=15G
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/molecular_clock_datasets/aebece_1211_tips/alt_topologies_19_02_24/baldauf_root_alt/abce_94_markers_concat_filtered.fasta"
-tree="/mnt/alvarium2pool/scratch/nenarokova/euk/molecular_clock_datasets/aebece_1211_tips/alt_topologies_19_02_24/baldauf_root_alt/baldauf_alt.tree"
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/molecular_clock_datasets/aebece_1211_tips/22_02_2024/frankenstein/abce_94_markers_concat_filtered.fasta"
+tree="/mnt/alvarium2pool/scratch/nenarokova/euk/molecular_clock_datasets/aebece_1211_tips/22_02_2024/frankenstein/fixed_bacteria_frankenstein.tree"
 iqtree2 -s $msa -te $tree -m LG+F+G4 -T AUTO --threads-max 10
