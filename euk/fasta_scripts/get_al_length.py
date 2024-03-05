6a73085e402c935ff1f910a24b90d89503879f21
Anna Nenarokova <a.nenarokova@gmail.com>
2024-03-05 15:46:40 +0000

    ni

--- euk/fasta_scripts/get_al_length.py
+++ euk/fasta_scripts/get_al_length.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from Bio import SeqIO
+import sys
+sys.path.insert(0, "/Users/anna/work/code/ngs/")
+sys.path.insert(0, "/user/home/vl18625/code/ngs")
+sys.path.insert(0, "/Users/vl18625/work/code/ngs/")
+from py_scripts.helpers.parse_csv import *
 
 def get_msa_len_dict(fasta_path):
 	msa_len_dict = {}
 	for record in SeqIO.parse(fasta_path, "fasta"):
-		alignment_no_gaps = record.seq.replace("-","")
+		alignment_no_gaps = str(record.seq).replace("-","")
 		alignment_len = len(alignment_no_gaps)
 		msa_len_dict[record.id] = alignment_len
 
@@ -12,9 +17,12 @@ def get_msa_len_dict(fasta_path):
 	msa_percent_dict = {}
 	for seqid, al_len in msa_len_dict.items():
 		msa_percent = 100*al_len/float(max_length)
-		msa_percent = '{:.2f}%'.format(msa_percent)
+		msa_percent = '{:.2f}'.format(msa_percent)
 		msa_percent_dict[seqid] = msa_percent
 	return msa_percent_dict
 
-fasta_path = "/Users/vl18625/work/euk/markers_euks/nina_markers/abe/only_euks/euks_split_markers/only_euks_132_markers_concat.fasta"
+fasta_path="/Users/vl18625/work/euk/molecular_clock/22_02_24/abce_94_markers_concat.fasta"
+outpath="/Users/vl18625/work/euk/molecular_clock/22_02_24/abce_94_markers_msa_len.csv"
 msa_len_dict = get_msa_len_dict(fasta_path)
+
+write_dict(msa_len_dict, outpath, delimiter=",")
--- euk/server_jobs/iqtree_branch_len.sh
+++ euk/server_jobs/iqtree_branch_len.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=frank_bfixed
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/frank_bfixed_%A.out
+#SBATCH --job-name=frank_580
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/frank_580_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=15G
-msa="/mnt/alvarium2pool/scratch/nenarokova/euk/molecular_clock_datasets/aebece_1211_tips/22_02_2024/frankenstein/abce_94_markers_concat_filtered.fasta"
-tree="/mnt/alvarium2pool/scratch/nenarokova/euk/molecular_clock_datasets/aebece_1211_tips/22_02_2024/frankenstein/fixed_bacteria_frankenstein.tree"
+#SBATCH --mem=10G
+
+msa="/mnt/alvarium2pool/scratch/nenarokova/euk/molecular_clock_datasets/aebece_580_tips/05_03_24_alternative_topologies/frank/abce_94_markers_580_tips.fasta"
+tree="/mnt/alvarium2pool/scratch/nenarokova/euk/molecular_clock_datasets/aebece_580_tips/05_03_24_alternative_topologies/frank/frank_580.tree"
 iqtree2 -s $msa -te $tree -m LG+F+G4 -T AUTO --threads-max 10
