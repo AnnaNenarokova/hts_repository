3314fc3705973f366b1f66f936a6cdcaf2730a93
Anna Nenarokova <a.nenarokova@gmail.com>
2024-03-12 10:56:42 +0000

    ni

--- euk/server_jobs/iqtree_concat_lgg_mel.sh
+++ euk/server_jobs/iqtree_concat_lgg_mel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=CE_lgg_filtered
-#SBATCH --output=/scratch/nenarokova/code/slurm_out/CE_lgg_filtered_%A.out
+#SBATCH --job-name=ae_lgg_no_meta
+#SBATCH --output=/scratch/nenarokova/code/slurm_out/ae_lgg_no_meta_%A.out
 #SBATCH --partition=high
 #SBATCH --time=7-12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=10G
+#SBATCH --mem=20G
 
-fasta="/scratch/nenarokova/euk/markers/be/one_hit/01_07_23/ce_filtered/concat/lgg/CE_81_markers_concat.fasta"
+fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/one_hit/ae_81_markers_11_03_24_all/ae_no_meta_81_markers_11_03_24/concat/lgg/ae_81_markers_no_meta.fasta"
 iqtree2 -s $fasta -m LG+G -B 1000 -T AUTO --threads-max 10
--- euk/tools/catfasta2phyml.sh
+++ euk/tools/catfasta2phyml.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # catfasta2phyml='/Users/vl18625/work/tools/catfasta2phyml/catfasta2phyml.pl'
 catfasta2phyml='/mnt/alvarium2pool/scratch/nenarokova/tools/catfasta2phyml/catfasta2phyml.pl'
-workdir='/scratch/nenarokova/euk/markers/be/one_hit/01_07_23/ce_filtered/linsi_bmge_renamed/'
-outfile='/scratch/nenarokova/euk/markers/be/one_hit/01_07_23/ce_filtered/concat/CE_81_markers_concat.fasta'
+workdir='/scratch/nenarokova/euk/markers/ae/one_hit/ae_81_markers_11_03_24_all/ae_no_meta_81_markers_11_03_24/linsi_bmge_renamed/'
+outfile='/scratch/nenarokova/euk/markers/ae/one_hit/ae_81_markers_11_03_24_all/ae_no_meta_81_markers_11_03_24/concat/ae_81_markers_no_meta.fasta'
 cd $workdir
 
 $catfasta2phyml --concatenate --verbose -f *.faa > $outfile
