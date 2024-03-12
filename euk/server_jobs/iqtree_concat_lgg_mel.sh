f37c7314da5f0531f66de3db08dcfaa19b027c63
Anna Nenarokova <a.nenarokova@gmail.com>
2024-03-12 10:59:38 +0000

    ni

--- euk/server_jobs/iqtree_concat_lgg_mel.sh
+++ euk/server_jobs/iqtree_concat_lgg_mel.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=20G
+#SBATCH --mem=5G
 
 fasta="/mnt/alvarium2pool/scratch/nenarokova/euk/markers/ae/one_hit/ae_81_markers_11_03_24_all/ae_no_meta_81_markers_11_03_24/concat/lgg/ae_81_markers_no_meta.fasta"
 iqtree2 -s $fasta -m LG+G -B 1000 -T AUTO --threads-max 10
